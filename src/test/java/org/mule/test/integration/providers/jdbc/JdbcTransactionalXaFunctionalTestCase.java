e0640acd0f04e21733c47576aa3be6a6a4dc6408
Ross Mason <ross@mulesoft.com>
2005-03-05 11:58:46 +0000

    Moved Jms and Jdbc functional tests to the integration sub-project
    git-svn-id: https://svn.codehaus.org/mule/trunk@149 bf997673-6b11-0410-b953-e057580c5b09

---
+++ src/test/java/org/mule/test/integration/providers/jdbc/JdbcTransactionalXaFunctionalTestCase.java
@@ -0,0 +1,61 @@
+/*
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * Copyright (c) Cubis Limited. All rights reserved.
+ * http://www.cubis.co.uk
+ *
+ * The software in this package is published under the terms of the BSD
+ * style license a copy of which has been included with this distribution in
+ * the LICENSE.txt file.
+ */
+package org.mule.test.integration.providers.jdbc;
+
+import javax.sql.DataSource;
+import javax.transaction.TransactionManager;
+
+import org.enhydra.jdbc.standard.StandardXADataSource;
+import org.mule.providers.jdbc.xa.DataSourceWrapper;
+import org.mule.transaction.XaTransactionFactory;
+import org.mule.umo.UMOTransactionFactory;
+import org.objectweb.jotm.Current;
+import org.objectweb.jotm.Jotm;
+
+/**
+ * @author Guillaume Nodet
+ * @version $Revision$
+ */
+public class JdbcTransactionalXaFunctionalTestCase extends AbstractJdbcTransactionalFunctionalTestCase {
+
+	private TransactionManager txManager;
+	
+    protected void setUp() throws Exception {
+		// check for already active JOTM instance
+    	txManager = Current.getCurrent();
+		// if none found, create new local JOTM instance
+		if (txManager == null) {
+			new Jotm(true, false);
+			txManager = Current.getCurrent();
+		}
+    	super.setUp();
+    	manager.setTransactionManager(txManager);
+    }
+
+    protected UMOTransactionFactory getTransactionFactory() {
+        return new XaTransactionFactory();
+    }
+    
+	protected DataSource createDataSource() throws Exception {
+		StandardXADataSource ds = new StandardXADataSource();
+		ds.setDriverName("org.hsqldb.jdbcDriver");
+        ds.setUrl("jdbc:hsqldb:mem:.");
+        ds.setUser("sa");
+        ds.setPassword("");
+        ds.setTransactionManager(txManager);
+        return new DataSourceWrapper(ds, txManager);
+	}
+	
+	
+}
