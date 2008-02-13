cbe63933655a6d34244da6fe41d95a677147d564
Daniel Feist <dfeist@gmail.com>
2008-02-13 00:07:51 +0000

    Merged revisions 10555-10589,10591,10593-10594,10596,10598,10600-10769,10771-10773,10776-10781,10783-10786,10788,10791-10793 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.0.x
    ........
      r10761 | rossmason | 2008-02-10 19:43:57 -0200 (Sun, 10 Feb 2008) | 1 line
      
      MULE-2978 - Support for non-string config locations. I have abstracted a string location into a ConfigResource object. This did require a minor change to the MuleConfiguration API, but nothing that will impact the user
    ........
      r10762 | rossmason | 2008-02-10 20:40:01 -0200 (Sun, 10 Feb 2008) | 1 line
      
      Use execption handler
    ........
      r10763 | rossmason | 2008-02-10 20:40:20 -0200 (Sun, 10 Feb 2008) | 1 line
      
      Explicitly declare this dependency
    ........
      r10764 | rossmason | 2008-02-10 20:41:10 -0200 (Sun, 10 Feb 2008) | 1 line
      
      A minore change to the ConfigResource API, JavaDoc fixes
    ........
      r10776 | rossmason | 2008-02-11 16:15:42 -0200 (Mon, 11 Feb 2008) | 1 line
      
      Fix for MULE-2881. Moved all MuleAdminAgent code to the client module.  The RemoteDispatcher also now does a Handshake with the server to agree the wire format. Renamed MuleAdminAgent to RemoteDispatcherAgent.
    ........
      r10791 | dfeist | 2008-02-12 21:03:47 -0200 (Tue, 12 Feb 2008) | 1 line
      
      Recover missing classes and fix issues with r10776 (MULE-2881) after the merge of the org.mule.extras.* -> org.mule.module.* changes (lost r10776 history for some files, sorry)
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@10795 bf997673-6b11-0410-b953-e057580c5b09

---
+++ src/test/java/org/mule/test/integration/client/RemoteDispatcherAgentConfigTestCase.java
@@ -0,0 +1,32 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.integration.client;
+
+import org.mule.module.client.remoting.RemoteDispatcherAgent;
+import org.mule.tck.FunctionalTestCase;
+import org.mule.transformer.wire.TransformerPairWireFormat;
+
+public class RemoteDispatcherAgentConfigTestCase extends FunctionalTestCase
+{
+    protected String getConfigResources()
+    {
+        return "org/mule/test/integration/client/mule-admin-agent.xml";
+    }
+
+    public void testNonEmptyProperties() throws Exception
+    {
+        RemoteDispatcherAgent agent = (RemoteDispatcherAgent) muleContext.getRegistry().lookupAgent("remoteDispatcher");
+        assertNotNull(agent.getEndpoint());
+        assertEquals("test://12345",agent.getEndpoint().getEndpointURI().toString());
+        assertNotNull(agent.getWireFormat());
+        assertTrue(agent.getWireFormat() instanceof TransformerPairWireFormat);
+    }
+    
+}
