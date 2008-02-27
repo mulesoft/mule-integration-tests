fbc59ac1d2ff5c434bd0b71f48f973ee2e8995f8
tcarlson <tcarlson>
2008-02-27 15:52:01 +0000

    Merged revisions 10944-10970 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.0.x
    ........
      r10944 | acooke | 2008-02-22 13:08:35 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-1742: Link to sub-elements.
    ........
      r10945 | dirk.olmes | 2008-02-22 13:11:10 -0200 (Fri, 22 Feb 2008) | 2 lines
      
      MULE-3008: added test
    ........
      r10946 | acooke | 2008-02-22 13:24:09 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-1742: Improve placeholder text.
    ........
      r10947 | acooke | 2008-02-22 13:57:28 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-1742: Include attribute groups in normalized schema.
    ........
      r10948 | holger | 2008-02-22 14:18:47 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-2928: oops, missed this one in the previous commit
    ........
      r10949 | acooke | 2008-02-22 15:40:35 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-1742: Clean up, restrict to known links.
    ........
      r10950 | acooke | 2008-02-22 15:46:30 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-1742: Replace NMTOKEN.
    ........
      r10951 | acooke | 2008-02-22 15:50:02 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-1742: Smaller name width (sorry - I need to commit to see changes in site).
    ........
      r10952 | acooke | 2008-02-22 16:06:15 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-1742: Further small fixes.
    ........
      r10953 | dirk.olmes | 2008-02-22 16:10:00 -0200 (Fri, 22 Feb 2008) | 2 lines
      
      MULE-3008 (Cannot set HTTP cookies on HTTP/HTTPS connector - ClassCastException): implemented the functionality
    ........
      r10954 | acooke | 2008-02-22 16:27:16 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-1742: Trying to make links consistent with Confluence.
    ........
      r10955 | acooke | 2008-02-22 16:29:56 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-1742: Trying to make links consistent with Confluence.
    ........
      r10956 | acooke | 2008-02-22 16:32:37 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-1742: Trying to make links consistent with Confluence.
    ........
      r10957 | holger | 2008-02-22 16:45:32 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-2423: Move XML tests out of VM transport
    ........
      r10958 | holger | 2008-02-22 16:47:00 -0200 (Fri, 22 Feb 2008) | 1 line
      
      clean empty dir
    ........
      r10959 | acooke | 2008-02-22 16:52:59 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-1742: Is replace() function causing errors?
    ........
      r10960 | acooke | 2008-02-22 16:58:10 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-1742: translate(replace,replace,translate)
    ........
      r10961 | dfeist | 2008-02-22 17:01:02 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-2292 Introduce inbound/outbound endpoint interfaces/roles and update API to use them
    ........
      r10962 | acooke | 2008-02-22 17:02:23 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-1742: More fiddling with anchors.
    ........
      r10963 | dirk.olmes | 2008-02-22 17:18:44 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-3008: fix the failing CI server
    ........
      r10964 | dfeist | 2008-02-22 18:04:03 -0200 (Fri, 22 Feb 2008) | 1 line
      
      Ooops! Nothing is going to compile without these two guys. (MULE-2292)
    ........
      r10965 | acooke | 2008-02-22 18:26:22 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-1742: More links.
    ........
      r10966 | acooke | 2008-02-22 18:28:30 -0200 (Fri, 22 Feb 2008) | 1 line
      
      MULE-1742: More links.
    ........
      r10967 | acooke | 2008-02-22 18:40:21 -0200 (Fri, 22 Feb 2008) | 1 line
      
      Fix build?  Sorted out anon function hack.
    ........
      r10968 | acooke | 2008-02-22 18:47:08 -0200 (Fri, 22 Feb 2008) | 1 line
      
      Trying again.
    ........
      r10969 | holger | 2008-02-22 19:57:26 -0200 (Fri, 22 Feb 2008) | 1 line
      
      got rickrolled again by inconsistent resource paths..
    ........
      r10970 | dfeist | 2008-02-22 19:59:28 -0200 (Fri, 22 Feb 2008) | 1 line
      
      Move missing test config (MULE-2423: Move XML tests out of VM transport)
    ........
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.x@11079 bf997673-6b11-0410-b953-e057580c5b09

---
+++ src/test/java/org/mule/issues/MulticastRouterMule2136TestCase.java
@@ -0,0 +1,120 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.issues;
+
+import org.mule.api.MuleException;
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.module.xml.functional.AbstractXmlFunctionalTestCase;
+
+
+/**
+ * This is a simplified version of {@link org.mule.module.xml.functional.XmlTransformerFunctionalTestCase}
+ * The {@link #testObjectXmlOut()} method hangs intermittently.
+ */
+public class MulticastRouterMule2136TestCase extends AbstractXmlFunctionalTestCase
+{
+
+    public static final int TEST_COUNT = 10000;
+    public static final String SERIALIZED = "<org.mule.issues.MulticastRouterMule2136TestCase_-Parent>\n" +
+                    "  <child/>\n" +
+                    "</org.mule.issues.MulticastRouterMule2136TestCase_-Parent>";
+
+    protected String getConfigResources()
+    {
+        return "org/mule/issues/multicast-router-mule-2136-test.xml";
+    }
+
+    protected MuleClient sendObject() throws MuleException
+    {
+        MuleClient client = new MuleClient();
+        client.dispatch("object-in", new Parent(new Child()), null);
+        return client;
+    }
+
+    public void testObjectOut() throws MuleException, InterruptedException
+    {
+        request(sendObject(), "object-out", Parent.class);
+        // wait a while, otherwise we pull down everything while it is still running
+        Thread.sleep(3000);
+    }
+
+    public void testObjectXmlOut() throws MuleException
+    {
+        String xml = (String) request(sendObject(), "object-xml-out", String.class);
+        assertEquals(SERIALIZED, xml);
+    }
+
+    public void testXmlObjectOut() throws MuleException
+    {
+        request(sendObject(), "xml-object-out", Parent.class);
+    }
+
+    public void testStress() throws MuleException
+    {
+        int tenth = TEST_COUNT / 10;
+        for (int i = 0; i < TEST_COUNT; i++)
+        {
+            testObjectXmlOut();
+            
+            // Pull result from "xml-object-out" endpoint as queuing is enabled and otherwise we get
+            // OutOfMemoryExceptions during stress tests when these results build up in queue.
+            request(new MuleClient(), "xml-object-out", Parent.class);
+            
+            if (i % tenth == 0)
+            {
+                logger.info("Iteration " + i);
+            }
+        }
+    }
+
+
+    protected Object request(MuleClient client, String endpoint, Class clazz) throws MuleException
+    {
+        MuleMessage message = client.request(endpoint, TIMEOUT);
+        assertNotNull(message);
+        assertNotNull(message.getPayload());
+        assertTrue(message.getPayload().getClass().getName(), clazz.isAssignableFrom(message.getPayload().getClass()));
+        return message.getPayload();
+    }
+
+
+    public static class Parent
+    {
+        private Child child;
+
+        public Parent()
+        {
+            this(null);
+        }
+
+        public Parent(Child child)
+        {
+            setChild(child);
+        }
+
+        public Child getChild()
+        {
+            return child;
+        }
+
+        public void setChild(Child child)
+        {
+            this.child = child;
+        }
+    }
+
+    public static class Child
+    {
+        // nothing here
+    }
+
+}
