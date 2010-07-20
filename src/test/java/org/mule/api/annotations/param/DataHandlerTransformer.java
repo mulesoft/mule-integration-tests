115ec26d109ee49753c814481bed4f7750021182
Ross Mason <ross@mulesoft.com>
2010-07-20 20:05:47 +0000

    move functional tests to the integration test tree since annotations cannot depend on anything since spring-config depends on it
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.x@18397 bf997673-6b11-0410-b953-e057580c5b09

---
+++ src/test/java/org/mule/api/annotations/param/DataHandlerTransformer.java
@@ -0,0 +1,28 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.api.annotations.param;
+
+import org.mule.api.annotations.Transformer;
+
+import java.io.IOException;
+
+import javax.activation.DataHandler;
+
+/**
+ * TODO
+ */
+public class DataHandlerTransformer
+{
+    @Transformer
+    public String dataHandlerToObject(DataHandler source) throws IOException
+    {
+        return (String)source.getContent();
+    }
+}
---
+++ src/test/java/org/mule/api/annotations/param/OutboundAttachmentsAnnotationTestCase.java
@@ -0,0 +1,68 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.api.annotations.param;
+
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+
+import java.util.Map;
+
+import javax.activation.DataHandler;
+
+public class OutboundAttachmentsAnnotationTestCase extends FunctionalTestCase
+{
+    public OutboundAttachmentsAnnotationTestCase()
+    {
+        setDisposeManagerPerSuite(true);
+    }
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/test/annotations/outbound-attachments-annotation.xml";
+    }
+
+    public void testProcessAttachment() throws Exception
+    {
+        MuleClient client = new MuleClient(muleContext);
+        MuleMessage message = client.send("vm://attachment", null, null);
+        assertNotNull("return message from MuleClient.send() should not be null", message);
+        assertTrue("Message payload should be a Map", message.getPayload() instanceof Map);
+        Map<String, DataHandler> result = getMapPayload(message);
+        assertEquals("barValue", result.get("bar").getContent());
+    }
+
+    public void testProcessAttachmentWithExistingOutAttachments() throws Exception
+    {
+        MuleClient client = new MuleClient(muleContext);
+        MuleMessage message = client.send("vm://attachment2", null, null);
+        assertNotNull("return message from MuleClient.send() should not be null", message);
+        assertTrue("Message payload should be a Map", message.getPayload() instanceof Map);
+        Map<String, DataHandler> result = getMapPayload(message);
+        assertEquals("barValue", result.get("bar").getContent());
+        assertEquals("fooValue", result.get("foo").getContent());
+    }
+
+    public void testInvalidParamType() throws Exception
+    {
+        MuleClient client = new MuleClient(muleContext);
+        MuleMessage message = client.send("vm://invalid", null, null);
+        assertNotNull("return message from MuleClient.send() should not be null", message);
+        assertNotNull(message.getExceptionPayload());
+        assertTrue(message.getExceptionPayload().getRootException() instanceof IllegalArgumentException);
+    }
+    
+    @SuppressWarnings("unchecked")
+    private Map<String, DataHandler> getMapPayload(MuleMessage message)
+    {
+        return (Map<String, DataHandler>) message.getPayload();
+    }
+}
