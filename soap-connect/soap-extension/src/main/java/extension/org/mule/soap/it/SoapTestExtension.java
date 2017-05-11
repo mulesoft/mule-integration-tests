ec99d9ecb24b499c6cd2b795aba3addf877e2635
Juan Desimoni <juan.desimoni@mulesoft.com>
2017-05-11 17:10:42 -0300

    MULE-12111: Soap Connect integration tests (#18)

---
+++ soap-connect/soap-extension/src/main/java/extension/org/mule/soap/it/SoapTestExtension.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package extension.org.mule.soap.it;
+
+import org.mule.runtime.extension.api.annotation.Extension;
+import org.mule.runtime.extension.api.soap.HttpMessageDispatcherProvider;
+import org.mule.runtime.extension.api.soap.annotation.Soap;
+import org.mule.runtime.extension.api.soap.annotation.SoapMessageDispatcherProviders;
+
+@Soap(TestServiceProvider.class)
+@SoapMessageDispatcherProviders(HttpMessageDispatcherProvider.class)
+@Extension(name = "soap")
+public class SoapTestExtension {
+
+}
---
+++ soap-connect/soap-extension/src/main/java/extension/org/mule/soap/it/TestServiceProvider.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package extension.org.mule.soap.it;
+
+import static java.util.Collections.singletonList;
+import static org.mule.runtime.extension.api.soap.WebServiceDefinition.builder;
+import org.mule.runtime.extension.api.annotation.param.Parameter;
+import org.mule.runtime.extension.api.soap.SoapServiceProvider;
+import org.mule.runtime.extension.api.soap.WebServiceDefinition;
+
+import java.util.List;
+
+public class TestServiceProvider implements SoapServiceProvider {
+
+  @Parameter
+  private String url;
+
+  @Parameter
+  private String service;
+
+  @Parameter
+  private String port;
+
+  public List<WebServiceDefinition> getWebServiceDefinitions() {
+    return singletonList(builder().withId("1").withWsdlUrl(url).withService(service).withPort(port).build());
+  }
+}
