ce7a333d7bf3b30a227c7f2e1960166cf5c1c760
Gustavo Adolfo Rodriguez <gustavo.libreros@mulesoft.com>
2017-05-16 12:50:18 -0500

    MULE-6619: flow initialState should support a property placeholder
    * Updating mule XSD for allowing a property placeholder on the initialState property of the flowType elemen

---
+++ integration/src/test/java/org/mule/test/components/InvalidFlowStatePlaceholderTestCase.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.components;
+
+import org.mule.test.tck.AbstractConfigurationErrorTestCase;
+
+import org.junit.Test;
+
+public class InvalidFlowStatePlaceholderTestCase extends AbstractConfigurationErrorTestCase {
+
+  @Override
+  protected String getConfigFile() {
+    return "org/mule/test/components/invalid-flow-initial-state.xml";
+  }
+
+  @Test
+  public void invalidInitialFlowStatePlaceholder() throws Exception {
+    assertConfigurationError("'${state}]' is not a valid value of union type '#AnonType_initialStateflowType'");
+  }
+
+}
---
+++ integration/src/test/java/org/mule/test/tck/AbstractConfigurationErrorTestCase.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.tck;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.slf4j.LoggerFactory.getLogger;
+import org.mule.runtime.core.api.MuleContext;
+import org.mule.test.AbstractIntegrationTestCase;
+
+import org.slf4j.Logger;
+
+public class AbstractConfigurationErrorTestCase extends AbstractIntegrationTestCase {
+
+  private static final Logger LOGGER = getLogger(AbstractConfigurationErrorTestCase.class);
+  private Exception exception;
+
+  public AbstractConfigurationErrorTestCase() {
+    setStartContext(false);
+  }
+
+  @Override
+  protected MuleContext createMuleContext() throws Exception {
+    try {
+      return super.createMuleContext();
+    } catch (Exception e) {
+      LOGGER.error("Configuration error detected", e);
+      exception = e;
+      return null;
+    }
+  }
+
+  protected final void assertConfigurationError(String assertionMessage) {
+    assertTrue(assertionMessage, exception != null);
+  }
+
+  protected final void assertConfigurationError(String assertionMessage, String expectedError) {
+    assertConfigurationError(assertionMessage);
+    assertThat(exception.getMessage(), containsString(expectedError));
+  }
+}
