5742cb268af33aae52b473e7a180796fb29553ba
Rodrigo <elrodro83@users.noreply.github.com>
2017-06-05 19:44:27 +0200

    MULE-12461: Interception API does not resolve connection params (#84)

--- integration/src/test/java/org/mule/test/integration/interception/ProcessorInterceptorFactoryTestCase.java
+++ integration/src/test/java/org/mule/test/integration/interception/ProcessorInterceptorFactoryTestCase.java
@@ -6,17 +6,24 @@
  */
 package org.mule.test.integration.interception;
 
+import static java.lang.Math.random;
+import static java.util.Arrays.asList;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import static org.mule.test.allure.AllureConstants.InterceptonApi.INTERCEPTION_API;
 import static org.mule.test.allure.AllureConstants.InterceptonApi.ComponentInterceptionStory.COMPONENT_INTERCEPTION_STORY;
+import static org.mule.test.heisenberg.extension.HeisenbergConnectionProvider.getConnects;
+import static org.mule.test.heisenberg.extension.HeisenbergConnectionProvider.getDisconnects;
 
 import org.mule.runtime.api.component.location.ComponentLocation;
 import org.mule.runtime.api.el.MuleExpressionLanguage;
+import org.mule.runtime.api.interception.InterceptionAction;
 import org.mule.runtime.api.interception.InterceptionEvent;
 import org.mule.runtime.api.interception.ProcessorInterceptor;
 import org.mule.runtime.api.interception.ProcessorInterceptorFactory;
@@ -26,18 +33,25 @@ import org.mule.runtime.core.api.config.ConfigurationBuilder;
 import org.mule.runtime.core.api.config.ConfigurationException;
 import org.mule.runtime.http.api.HttpService;
 import org.mule.test.AbstractIntegrationTestCase;
+import org.mule.test.heisenberg.extension.HeisenbergConnection;
 import org.mule.test.heisenberg.extension.HeisenbergExtension;
 import org.mule.test.heisenberg.extension.model.KillParameters;
+import org.mule.test.runner.RunnerDelegateTo;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 
 import javax.inject.Inject;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import ru.yandex.qatools.allure.annotations.Description;
 import ru.yandex.qatools.allure.annotations.Features;
@@ -45,7 +59,19 @@ import ru.yandex.qatools.allure.annotations.Stories;
 
 @Features(INTERCEPTION_API)
 @Stories(COMPONENT_INTERCEPTION_STORY)
+@RunnerDelegateTo(Parameterized.class)
 public class ProcessorInterceptorFactoryTestCase extends AbstractIntegrationTestCase {
+
+  private final boolean mutateEventBefore;
+
+  public ProcessorInterceptorFactoryTestCase(boolean mutateEventBefore) {
+    this.mutateEventBefore = mutateEventBefore;
+  }
+
+  @Parameters(name = "{0}")
+  public static Collection<Object> data() {
+    return asList(new Object[] {true, false});
+  }
 
   @Override
   protected String getConfigFile() {
@@ -65,13 +91,15 @@ public class ProcessorInterceptorFactoryTestCase extends AbstractIntegrationTest
 
       @Override
       public void configure(MuleContext muleContext) throws ConfigurationException {
-        muleContext.getProcessorInterceptorManager().addInterceptorFactory(new HasInjectedAttributesInterceptorFactory());
+        muleContext.getProcessorInterceptorManager()
+            .addInterceptorFactory(new HasInjectedAttributesInterceptorFactory(mutateEventBefore));
       }
     });
   }
 
   @Before
   public void before() {
+    HasInjectedAttributesInterceptor.actioner = action -> action.proceed();
     HasInjectedAttributesInterceptor.interceptionParameters.clear();
   }
 
@@ -80,8 +108,9 @@ public class ProcessorInterceptorFactoryTestCase extends AbstractIntegrationTest
   public void injection() throws Exception {
     List<Object> payload = new ArrayList<>();
     flowRunner("injectionInterceptionTest").withPayload(payload).run();
-    assertThat(HasInjectedAttributesInterceptor.interceptionParameters.size(), is(3));
+
     List<InterceptionParameters> interceptionParameters = HasInjectedAttributesInterceptor.interceptionParameters;
+    assertThat(interceptionParameters, hasSize(3));
 
     InterceptionParameters loggerInterceptionParameter = interceptionParameters.get(0);
     InterceptionParameters flowRefInterceptionParameter = interceptionParameters.get(1);
@@ -96,8 +125,8 @@ public class ProcessorInterceptorFactoryTestCase extends AbstractIntegrationTest
   public void operationParameters() throws Exception {
     flowRunner("killFromPayload").withPayload("T-1000").withVariable("goodbye", "Hasta la vista, baby").run();
 
-    assertThat(HasInjectedAttributesInterceptor.interceptionParameters.size(), is(1));
     List<InterceptionParameters> interceptionParameters = HasInjectedAttributesInterceptor.interceptionParameters;
+    assertThat(interceptionParameters, hasSize(1));
 
     InterceptionParameters killInterceptionParameter = interceptionParameters.get(0);
 
@@ -128,8 +157,8 @@ public class ProcessorInterceptorFactoryTestCase extends AbstractIntegrationTest
   public void resolvedComplexParametersOperationParameters() throws Exception {
     flowRunner("killWithCustomMessage").withVariable("goodbye", "Hasta la vista, baby").run();
 
-    assertThat(HasInjectedAttributesInterceptor.interceptionParameters.size(), is(1));
     List<InterceptionParameters> interceptionParameters = HasInjectedAttributesInterceptor.interceptionParameters;
+    assertThat(interceptionParameters, hasSize(1));
 
     InterceptionParameters killInterceptionParameter = interceptionParameters.get(0);
 
@@ -137,6 +166,78 @@ public class ProcessorInterceptorFactoryTestCase extends AbstractIntegrationTest
     assertThat(killInterceptionParameter.getParameters().get("victim"), is("T-1000"));
     assertThat(killInterceptionParameter.getParameters().get("goodbyeMessage"), is("Hasta la vista, baby"));
     assertThat(killInterceptionParameter.getParameters().get("killParameters"), is(instanceOf(KillParameters.class)));
+  }
+
+  @Test
+  @Description("The connection was fetched on the interceptor, and the operation uses the connection obtained there rather then fetching it again")
+  public void resolvedConnectionParam() throws Exception {
+    int connectsBefore = getConnects();
+    int disconnectsBefore = getDisconnects();
+
+    flowRunner("callSaul").run();
+
+    List<InterceptionParameters> interceptionParameters = HasInjectedAttributesInterceptor.interceptionParameters;
+    assertThat(interceptionParameters, hasSize(1));
+
+    InterceptionParameters killInterceptionParameter = interceptionParameters.get(0);
+
+    assertThat(killInterceptionParameter.getParameters().keySet(), contains("config-ref", "connection"));
+    assertThat(killInterceptionParameter.getParameters().get("config-ref"), is("heisenberg"));
+    assertThat(killInterceptionParameter.getParameters().get("connection"), is(instanceOf(HeisenbergConnection.class)));
+
+    assertThat(getConnects() - connectsBefore, is(mutateEventBefore ? 2 : 1));
+    assertThat(getDisconnects() - disconnectsBefore, is(mutateEventBefore ? 2 : 1));
+  }
+
+  @Test
+  @Description("The connection was fetched on the interceptor, and released by the interceptor")
+  public void resolvedConnectionParamSkips() throws Exception {
+    int connectsBefore = getConnects();
+    int disconnectsBefore = getDisconnects();
+
+    HasInjectedAttributesInterceptor.actioner = action -> action.skip();
+
+    flowRunner("callSaul").run();
+
+    List<InterceptionParameters> interceptionParameters = HasInjectedAttributesInterceptor.interceptionParameters;
+    assertThat(interceptionParameters, hasSize(1));
+
+    InterceptionParameters killInterceptionParameter = interceptionParameters.get(0);
+
+    assertThat(killInterceptionParameter.getParameters().keySet(), contains("config-ref", "connection"));
+    assertThat(killInterceptionParameter.getParameters().get("config-ref"), is("heisenberg"));
+    assertThat(killInterceptionParameter.getParameters().get("connection"), is(instanceOf(HeisenbergConnection.class)));
+
+    assertThat(getConnects() - connectsBefore, is(mutateEventBefore ? 2 : 1));
+    assertThat(getDisconnects() - disconnectsBefore, is(mutateEventBefore ? 2 : 1));
+  }
+
+  @Test
+  @Description("The connection was fetched on the interceptor, and released by the interceptor")
+  public void resolvedConnectionParamFails() throws Exception {
+    int connectsBefore = getConnects();
+    int disconnectsBefore = getDisconnects();
+
+    HasInjectedAttributesInterceptor.actioner = action -> action.fail(new RuntimeException());
+
+    try {
+      flowRunner("callSaul").run();
+      fail("Expected an exception. Refer to ReactiveInterceptorAdapterTestCase");
+    } catch (Exception e) {
+      // expected
+    } finally {
+      List<InterceptionParameters> interceptionParameters = HasInjectedAttributesInterceptor.interceptionParameters;
+      assertThat(interceptionParameters, hasSize(1));
+
+      InterceptionParameters killInterceptionParameter = interceptionParameters.get(0);
+
+      assertThat(killInterceptionParameter.getParameters().keySet(), contains("config-ref", "connection"));
+      assertThat(killInterceptionParameter.getParameters().get("config-ref"), is("heisenberg"));
+      assertThat(killInterceptionParameter.getParameters().get("connection"), is(instanceOf(HeisenbergConnection.class)));
+
+      assertThat(getConnects() - connectsBefore, is(mutateEventBefore ? 2 : 1));
+      assertThat(getDisconnects() - disconnectsBefore, is(mutateEventBefore ? 2 : 1));
+    }
   }
 
   public static class HasInjectedAttributesInterceptorFactory implements ProcessorInterceptorFactory {
@@ -150,25 +251,35 @@ public class ProcessorInterceptorFactoryTestCase extends AbstractIntegrationTest
     @Inject
     private HttpService httpService;
 
+    private boolean mutateEventBefore;
+
+    public HasInjectedAttributesInterceptorFactory(boolean mutateEventBefore) {
+      this.mutateEventBefore = mutateEventBefore;
+    }
+
     @Override
     public ProcessorInterceptor get() {
-      return new HasInjectedAttributesInterceptor(expressionEvaluator, lockFactory, httpService);
+      return new HasInjectedAttributesInterceptor(expressionEvaluator, lockFactory, httpService, mutateEventBefore);
     }
   }
 
   public static class HasInjectedAttributesInterceptor implements ProcessorInterceptor {
 
+    private static Function<InterceptionAction, CompletableFuture<InterceptionEvent>> actioner = action -> action.proceed();
     private static final List<InterceptionParameters> interceptionParameters = new LinkedList<>();
 
     private MuleExpressionLanguage expressionEvaluator;
     private LockFactory lockFactory;
     private HttpService httpService;
 
+    private boolean mutateEventBefore;
+
     public HasInjectedAttributesInterceptor(MuleExpressionLanguage expressionEvaluator, LockFactory lockFactory,
-                                            HttpService httpService) {
+                                            HttpService httpService, boolean mutateEventBefore) {
       this.expressionEvaluator = expressionEvaluator;
       this.lockFactory = lockFactory;
       this.httpService = httpService;
+      this.mutateEventBefore = mutateEventBefore;
     }
 
     @Override
@@ -177,6 +288,16 @@ public class ProcessorInterceptorFactoryTestCase extends AbstractIntegrationTest
       assertThat(expressionEvaluator, not(nullValue()));
       assertThat(lockFactory, not(nullValue()));
       assertThat(httpService, not(nullValue()));
+
+      if (mutateEventBefore) {
+        event.addVariable("mutated", random());
+      }
+    }
+
+    @Override
+    public CompletableFuture<InterceptionEvent> around(ComponentLocation location, Map<String, Object> parameters,
+                                                       InterceptionEvent event, InterceptionAction action) {
+      return actioner.apply(action);
     }
   }
 
