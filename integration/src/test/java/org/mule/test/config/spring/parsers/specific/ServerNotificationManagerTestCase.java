daef8f02448ba8de4b438286c7c519cd42d8d533
Rodrigo Merino <rodrigo.merino@mulesoft.com>
2017-06-23 12:01:26 -0300

    MULE-12923: Add a way to support custom notifications from plugins

--- integration/src/test/java/org/mule/test/config/spring/parsers/specific/ServerNotificationManagerTestCase.java
+++ integration/src/test/java/org/mule/test/config/spring/parsers/specific/ServerNotificationManagerTestCase.java
@@ -6,21 +6,26 @@
  */
 package org.mule.test.config.spring.parsers.specific;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.junit.Assert.assertThat;
+import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
 
 import org.mule.runtime.core.api.MuleContext;
+import org.mule.runtime.core.api.context.notification.ListenerSubscriptionPair;
+import org.mule.runtime.core.api.context.notification.SecurityNotification;
 import org.mule.runtime.core.api.context.notification.SecurityNotificationListener;
 import org.mule.runtime.core.api.context.notification.ServerNotification;
 import org.mule.runtime.core.api.context.notification.ServerNotificationListener;
-import org.mule.runtime.core.api.security.UnauthorisedException;
-import org.mule.runtime.core.api.config.i18n.CoreMessages;
-import org.mule.runtime.core.api.context.notification.ListenerSubscriptionPair;
-import org.mule.runtime.core.api.context.notification.SecurityNotification;
 import org.mule.runtime.core.api.context.notification.ServerNotificationManager;
+import org.mule.runtime.core.api.security.UnauthorisedException;
+import org.mule.tck.probe.JUnitLambdaProbe;
 import org.mule.tck.probe.PollingProber;
-import org.mule.tck.probe.Probe;
 import org.mule.test.AbstractIntegrationTestCase;
 
 import java.util.Collection;
@@ -37,51 +42,53 @@ public class ServerNotificationManagerTestCase extends AbstractIntegrationTestCa
   @Test
   public void testDynamicAttribute() {
     ServerNotificationManager manager = muleContext.getNotificationManager();
-    assertTrue(manager.isNotificationDynamic());
+    assertThat(manager.isNotificationDynamic(), is(true));
   }
 
   @Test
   public void testRoutingConfiguration() {
     ServerNotificationManager manager = muleContext.getNotificationManager();
-    assertTrue(manager.getInterfaceToTypes().size() > 2);
+    assertThat(manager.getInterfaceToTypes().entrySet(), hasSize(greaterThan(2)));
     Object ifaces = manager.getInterfaceToTypes().get(TestInterface.class);
-    assertNotNull(ifaces);
-    assertTrue(ifaces instanceof Collection);
-    assertTrue(((Collection) ifaces).contains(TestEvent.class));
+    assertThat(ifaces, not(nullValue()));
+    assertThat(ifaces, instanceOf(Collection.class));
+    assertThat((Collection<Class>) ifaces, hasItem(TestEvent.class));
     ifaces = manager.getInterfaceToTypes().get(TestInterface2.class);
-    assertNotNull(ifaces);
-    assertTrue(ifaces instanceof Collection);
-    assertTrue(((Collection) ifaces).contains(SecurityNotification.class));
+    assertThat(ifaces, not(nullValue()));
+    assertThat(ifaces, instanceOf(Collection.class));
+    assertThat((Collection<Class>) ifaces, hasItem(SecurityNotification.class));
   }
 
   @Test
   public void testSimpleNotification() throws InterruptedException {
     ServerNotificationManager manager = muleContext.getNotificationManager();
-    Collection listeners = manager.getListeners();
+    Collection<ListenerSubscriptionPair> listeners = manager.getListeners();
     // Now all transformers are registered as listeners in order to get a context disposing notification
-    assertTrue(listeners.size() > 5);
+    assertThat(listeners, hasSize(greaterThan(5)));
     TestListener listener = (TestListener) muleContext.getRegistry().lookupObject("listener");
-    assertNotNull(listener);
-    assertFalse(listener.isCalled());
+    assertThat(listener, not(nullValue()));
+    assertThat(listener.isCalled(), is(false));
     manager.fireNotification(new TestEvent());
-    Thread.sleep(1000); // asynch events
-    assertTrue(listener.isCalled());
+
+    // asynch events
+    new PollingProber(1000, 50).check(new JUnitLambdaProbe(() -> listener.isCalled()));
   }
 
   @Test
   public void testExplicitlyConiguredNotificationListenerRegistration() throws InterruptedException {
     ServerNotificationManager manager = muleContext.getNotificationManager();
-    assertTrue(manager.getListeners()
-        .contains(new ListenerSubscriptionPair((ServerNotificationListener) muleContext.getRegistry().lookupObject("listener"),
-                                               null)));
-    assertTrue(manager.getListeners()
-        .contains(new ListenerSubscriptionPair((ServerNotificationListener) muleContext.getRegistry().lookupObject("listener2"),
-                                               null)));
-    assertTrue(manager.getListeners().contains(new ListenerSubscriptionPair((ServerNotificationListener) muleContext.getRegistry()
-        .lookupObject("securityListener"), null)));
-    assertTrue(manager.getListeners()
-        .contains(new ListenerSubscriptionPair((ServerNotificationListener) muleContext.getRegistry().lookupObject("listener3"),
-                                               "*")));
+    assertThat(manager.getListeners(),
+               hasItem(new ListenerSubscriptionPair((ServerNotificationListener) muleContext.getRegistry()
+                   .lookupObject("listener"), null)));
+    assertThat(manager.getListeners(),
+               hasItem(new ListenerSubscriptionPair((ServerNotificationListener) muleContext.getRegistry()
+                   .lookupObject("listener2"), null)));
+    assertThat(manager.getListeners(),
+               hasItem(new ListenerSubscriptionPair((ServerNotificationListener) muleContext.getRegistry()
+                   .lookupObject("securityListener"), null)));
+    assertThat(manager.getListeners(),
+               hasItem(new ListenerSubscriptionPair((ServerNotificationListener) muleContext.getRegistry()
+                   .lookupObject("listener3"), "*")));
   }
 
   @Test
@@ -90,42 +97,31 @@ public class ServerNotificationManagerTestCase extends AbstractIntegrationTestCa
 
     // Not registered as ad-hoc listener with null subscription as this is defined
     // explicitly.
-    assertFalse(manager.getListeners()
-        .contains(new ListenerSubscriptionPair((ServerNotificationListener) muleContext.getRegistry().lookupObject("listener3"),
-                                               null)));
+    assertThat(manager.getListeners(),
+               not(hasItem(new ListenerSubscriptionPair((ServerNotificationListener) muleContext.getRegistry()
+                   .lookupObject("listener3"), null))));
 
     // Registered as configured
-    assertTrue(manager.getListeners()
-        .contains(new ListenerSubscriptionPair((ServerNotificationListener) muleContext.getRegistry().lookupObject("listener4"),
-                                               null)));
+    assertThat(manager.getListeners(),
+               hasItem(new ListenerSubscriptionPair((ServerNotificationListener) muleContext.getRegistry()
+                   .lookupObject("listener4"), null)));
   }
 
   @Test
   public void testDisabledNotification() throws Exception {
     ServerNotificationManager manager = muleContext.getNotificationManager();
-    Collection listeners = manager.getListeners();
+    Collection<ListenerSubscriptionPair> listeners = manager.getListeners();
     // Now all transformers are registered as listeners in order to get a context disposing notification
-    assertTrue(listeners.size() > 5);
-    TestListener2 listener2 = (TestListener2) muleContext.getRegistry().lookupObject("listener2");
-    assertNotNull(listener2);
-    assertFalse(listener2.isCalled());
-    TestSecurityListener adminListener = (TestSecurityListener) muleContext.getRegistry().lookupObject("securityListener");
-    assertNotNull(adminListener);
-    assertFalse(adminListener.isCalled());
+    assertThat(listeners, hasSize(greaterThan(5)));
+    TestListener2 listener2 = muleContext.getRegistry().lookupObject("listener2");
+    assertThat(listener2, not(nullValue()));
+    assertThat(listener2.isCalled(), is(false));
+    TestSecurityListener adminListener = muleContext.getRegistry().lookupObject("securityListener");
+    assertThat(adminListener, not(nullValue()));
+    assertThat(adminListener.isCalled(), is(false));
     manager.fireNotification(new TestSecurityEvent(muleContext));
-    new PollingProber(2000, 100).check(new Probe() {
-
-      @Override
-      public boolean isSatisfied() {
-        return listener2.isCalled();
-      }
-
-      @Override
-      public String describeFailure() {
-        return "listener2 should be notified";
-      }
-    });
-    assertFalse(adminListener.isCalled());
+    new PollingProber(2000, 100).check(new JUnitLambdaProbe(() -> listener2.isCalled(), "listener2 should be notified"));
+    assertThat(adminListener.isCalled(), is(false));
   }
 
   protected static interface TestInterface extends ServerNotificationListener {
@@ -197,8 +193,7 @@ public class ServerNotificationManagerTestCase extends AbstractIntegrationTestCa
   protected static class TestSecurityEvent extends SecurityNotification {
 
     public TestSecurityEvent(MuleContext muleContext) throws Exception {
-      super(new UnauthorisedException(CoreMessages.createStaticMessage("dummy")),
-            0);
+      super(new UnauthorisedException(createStaticMessage("dummy")), 0);
     }
 
   }
