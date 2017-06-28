129edadb334a58f0034368b23db1c2dc06f97c76
Alejandro Pulver <alepulver@gmail.com>
2017-06-28 14:23:15 -0300

    MULE-12500: remove endorsed libraries (#136)

--- integration/src/test/java/org/mule/test/spring/SchemaValidationMule2225TestCase.java
+++ integration/src/test/java/org/mule/test/spring/SchemaValidationMule2225TestCase.java
@@ -6,8 +6,10 @@
  */
 package org.mule.test.spring;
 
-import org.mule.tck.junit4.AbstractMuleTestCase;
+import static org.junit.Assert.assertNotNull;
+
 import org.mule.runtime.core.api.util.IOUtils;
+import org.mule.tck.junit4.AbstractMuleTestCase;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,18 +23,11 @@ import javax.xml.validation.SchemaFactory;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
-import static org.junit.Assert.assertNotNull;
-
 /**
  * Note: this test will fail if off-line.
  */
 public class SchemaValidationMule2225TestCase extends AbstractMuleTestCase {
 
-  /**
-   * This test will fail when run with plain JDK 1.4 or even 1.5 - schema validation requires a proper JAXP installation in the
-   * JDK's endorsed directory. It works fine with Xerces 2.9.1 (as with mule 1.4.x) or a manually installed JAXP Sun RI on JDK
-   * 1.5; JDK 1.6 works out of the box.
-   */
   @Test
   public void testValidation() throws SAXException, IOException {
     SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
