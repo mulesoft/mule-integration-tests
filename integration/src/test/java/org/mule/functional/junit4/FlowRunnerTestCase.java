59533910b4b5b17661e3a1268997bbc85799bcfd
lucianoRM <lucianoRM@users.noreply.github.com>
2017-07-24 16:40:56 -0300

    Mule 13170: runExpectingException() should handle cases where exception is thrown from a source different than the expected one (#209)

---
+++ integration/src/test/java/org/mule/functional/junit4/FlowRunnerTestCase.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.functional.junit4;
+
+import static org.junit.Assert.assertNotNull;
+import org.mule.runtime.core.api.exception.MessagingException;
+import org.mule.test.AbstractIntegrationTestCase;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class FlowRunnerTestCase extends AbstractIntegrationTestCase {
+
+  @Rule
+  public ExpectedException exception = ExpectedException.none();
+
+  @Override
+  protected String getConfigFile() {
+    return "org/mule/functional/junit4/flow-runner-config.xml";
+  }
+
+  @Test
+  public void flowFinishesSuccessfullyWhenExpectingException() throws Exception {
+    MessagingException exception = flowRunner("okFailFlow").runExpectingException();
+    assertNotNull(exception);
+  }
+
+  @Test
+  public void flowRunFailsEvenWhenExpectingException() throws Exception {
+    exception.expect(AssertionError.class);
+    exception.expectMessage("evaluated false");
+    flowRunner("badFailFlow").runExpectingException();
+  }
+
+}
