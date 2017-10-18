651bfc4fc3d0272e25a752957c2357ce139b386c
Rodro <rodrigo.merino@mulesoft.com>
2017-10-18 16:43:45 -0300

    MULE-13817: findAllLocations in ComponentLocator does not return not initted components

--- integration/src/test/java/org/mule/test/integration/locator/LazyInitConfigurationComponentLocatorTestCase.java
+++ integration/src/test/java/org/mule/test/integration/locator/LazyInitConfigurationComponentLocatorTestCase.java
@@ -6,34 +6,33 @@
  */
 package org.mule.test.integration.locator;
 
-import static java.util.stream.Collectors.toList;
 import static java.util.Optional.empty;
+import static java.util.stream.Collectors.toList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.mule.runtime.api.component.location.Location.builder;
+import static org.mule.runtime.config.api.LazyComponentInitializer.LAZY_COMPONENT_INITIALIZER_SERVICE_KEY;
+import static org.mule.runtime.config.api.SpringXmlConfigurationBuilderFactory.createConfigurationBuilder;
+import static org.mule.test.allure.AllureConstants.ConfigurationComponentLocatorFeature.CONFIGURATION_COMPONENT_LOCATOR;
+import static org.mule.test.allure.AllureConstants.ConfigurationComponentLocatorFeature.ConfigurationComponentLocatorStory.SEARCH_CONFIGURATION;
 
-import io.qameta.allure.Description;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Story;
-import org.junit.Ignore;
-import org.junit.Test;
 import org.mule.runtime.api.component.location.ComponentLocation;
 import org.mule.runtime.config.api.LazyComponentInitializer;
 import org.mule.runtime.core.api.config.ConfigurationBuilder;
 import org.mule.test.AbstractIntegrationTestCase;
 
+import org.junit.Test;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import java.util.List;
-
-import static org.mule.runtime.config.api.LazyComponentInitializer.LAZY_COMPONENT_INITIALIZER_SERVICE_KEY;
-import static org.mule.runtime.config.api.SpringXmlConfigurationBuilderFactory.createConfigurationBuilder;
-import static org.mule.test.allure.AllureConstants.ConfigurationComponentLocatorFeature.CONFIGURATION_COMPONENT_LOCATOR;
-import static org.mule.test.allure.AllureConstants.ConfigurationComponentLocatorFeature.ConfigurationComponentLocatorStory.SEARCH_CONFIGURATION;
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 
 @Feature(CONFIGURATION_COMPONENT_LOCATOR)
 @Story(SEARCH_CONFIGURATION)
@@ -60,72 +59,92 @@ public class LazyInitConfigurationComponentLocatorTestCase extends AbstractInteg
 
   @Description("Lazy init should not create components until an operation is done")
   @Test
-  public void lazyInitDoesNotCalculateLocations() {
-    assertThat(muleContext.getConfigurationComponentLocator().findAllLocations(), hasSize(0));
-    assertThat(muleContext.getConfigurationComponentLocator().find(builder().globalName("myFlow").build()), is(empty()));
+  public void lazyInitCalculatesLocations() {
+    assertThat(locator
+        .findAllLocations()
+        .stream()
+        .map(ComponentLocation::getLocation)
+        .collect(toList()),
+               containsInAnyOrder("myFlow",
+                                  "myFlow/source",
+                                  "myFlow/source/0",
+                                  "myFlow/source/0/0",
+                                  "myFlow/processors/0",
+                                  "myFlow/processors/1",
+                                  "myFlow/processors/2",
+                                  "myFlow/processors/2/processors/0",
+                                  "myFlow/processors/2/processors/1",
+
+                                  "anotherFlow",
+                                  "anotherFlow/source",
+                                  "anotherFlow/source/0",
+                                  "anotherFlow/source/0/0",
+                                  "anotherFlow/processors/0",
+
+                                  "flowWithSubflow",
+                                  "flowWithSubflow/processors/0",
+                                  "mySubFlow",
+                                  "mySubFlow/processors/0",
+
+                                  "flowLvl0",
+                                  "flowLvl0/processors/0",
+                                  "flowLvl1",
+                                  "flowLvl1/processors/0",
+                                  "flowLvl2",
+                                  "flowLvl2/processors/0",
+                                  "flowLvl2/processors/0/0",
+                                  "flowLvl2/processors/1",
+                                  "dbConfig",
+                                  "dbConfig/0",
+                                  "requestConfig",
+                                  "requestConfig/0",
+                                  "tlsContextRef",
+                                  "tlsContextRef/0"));
+    assertThat(locator.find(builder().globalName("myFlow").build()), is(empty()));
+    assertThat(locator.find(builder().globalName("anotherFlow").build()), is(empty()));
   }
 
   @Description("Lazy init should create components when operation is done")
   @Test
   public void lazyMuleContextInitializesLocation() {
     lazyComponentInitializer.initializeComponent(builder().globalName("myFlow").build());
-    List<ComponentLocation> componentLocs = muleContext.getConfigurationComponentLocator().findAllLocations();
-    List<String> allComponentPaths = componentLocs.stream().map(ComponentLocation::getLocation).collect(toList());
-    assertThat(allComponentPaths, containsInAnyOrder(
-                                                     "myFlow",
-                                                     "myFlow/source",
-                                                     "myFlow/source/0",
-                                                     "myFlow/source/0/0",
-                                                     "myFlow/processors/0",
-                                                     "myFlow/processors/1",
-                                                     "myFlow/processors/2",
-                                                     "myFlow/processors/2/processors/0",
-                                                     "myFlow/processors/2/processors/1"));
-    assertThat(muleContext.getConfigurationComponentLocator().find(builder().globalName("myFlow").build()), is(not(empty())));
-    assertThat(muleContext.getConfigurationComponentLocator().find(builder().globalName("anotherFlow").build()), is(empty()));
+    assertThat(locator.find(builder().globalName("myFlow").build()), is(not(empty())));
+    assertThat(locator.find(builder().globalName("anotherFlow").build()), is(empty()));
+
+    assertThat(locator
+        .findAllLocations()
+        .stream()
+        .map(ComponentLocation::getLocation)
+        .collect(toList()), hasItem("myFlow/source"));
   }
 
   @Description("Lazy init should refresh the ConfigurationComponentLocator when initialize is done")
   @Test
   public void lazyMuleContextRefreshesConfigurationComponentLoader() {
     lazyComponentInitializer.initializeComponent(builder().globalName("myFlow").build());
-    assertThat(muleContext.getConfigurationComponentLocator().findAllLocations(), hasSize(9));
+    assertThat(locator.findAllLocations(), hasSize(32));
 
     lazyComponentInitializer.initializeComponent(builder().globalName("anotherFlow").build());
-    List<ComponentLocation> componentLocs = muleContext.getConfigurationComponentLocator().findAllLocations();
-    List<String> allComponentPaths = componentLocs.stream().map(ComponentLocation::getLocation).collect(toList());
-    assertThat(allComponentPaths, containsInAnyOrder(
-                                                     "anotherFlow",
-                                                     "anotherFlow/source",
-                                                     "anotherFlow/source/0",
-                                                     "anotherFlow/source/0/0",
-                                                     "anotherFlow/processors/0"));
-    assertThat(muleContext.getConfigurationComponentLocator().find(builder().globalName("myFlow").build()), is(empty()));
-    assertThat(muleContext.getConfigurationComponentLocator().find(builder().globalName("anotherFlow").build()),
+    assertThat(locator.findAllLocations(), hasSize(32));
+    assertThat(locator.find(builder().globalName("myFlow").build()), is(empty()));
+    assertThat(locator.find(builder().globalName("anotherFlow").build()),
                is(not(empty())));
   }
 
   @Test
   public void lazyMuleContextWithDeeperLevelConfig() {
     lazyComponentInitializer.initializeComponent(builder().globalName("flowLvl0").build());
-    List<ComponentLocation> componentLocs = muleContext.getConfigurationComponentLocator().findAllLocations();
-    List<String> allComponentPaths = componentLocs.stream().map(ComponentLocation::getLocation).collect(toList());
 
-    assertThat(allComponentPaths, containsInAnyOrder(
-                                                     "flowLvl0",
-                                                     "flowLvl0/processors/0",
-                                                     "flowLvl1",
-                                                     "flowLvl1/processors/0",
-                                                     "flowLvl2",
-                                                     "flowLvl2/processors/0",
-                                                     "flowLvl2/processors/0/0",
-                                                     "flowLvl2/processors/1",
-                                                     "dbConfig",
-                                                     "dbConfig/0",
-                                                     "requestConfig",
-                                                     "requestConfig/0",
-                                                     "tlsContextRef",
-                                                     "tlsContextRef/0"));
+    assertThat(locator.find(builder().globalName("flowLvl0").build()), is(not(empty())));
+    assertThat(locator.find(builder().globalName("flowLvl0").addProcessorsPart().addIndexPart(0).build()), is(not(empty())));
+    assertThat(locator.find(builder().globalName("flowLvl1").build()), is(not(empty())));
+    assertThat(locator.find(builder().globalName("flowLvl1").addProcessorsPart().addIndexPart(0).build()), is(not(empty())));
+    assertThat(locator.find(builder().globalName("flowLvl2").build()), is(not(empty())));
+    assertThat(locator.find(builder().globalName("flowLvl2").addProcessorsPart().addIndexPart(0).build()), is(not(empty())));
+    assertThat(locator.find(builder().globalName("flowLvl2").addProcessorsPart().addIndexPart(1).build()), is(not(empty())));
+    assertThat(locator.find(builder().globalName("dbConfig").build()), is(not(empty())));
+    assertThat(locator.find(builder().globalName("requestConfig").build()), is(not(empty())));
+    assertThat(locator.find(builder().globalName("tlsContextRef").build()), is(not(empty())));
   }
 
 }
