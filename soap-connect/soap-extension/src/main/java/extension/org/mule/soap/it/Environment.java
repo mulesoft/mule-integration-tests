7074ee5f9d6219ca014198cb600e6050b2888be3
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2017-10-24 18:26:11 -0300

    MULE-13867: Enum config parameters are not set correctly (#391)

---
+++ soap-connect/soap-extension/src/main/java/extension/org/mule/soap/it/Environment.java
@@ -0,0 +1,8 @@
+package extension.org.mule.soap.it;
+
+public enum Environment {
+
+  DEV,
+  TEST,
+  PROD
+}
--- soap-connect/soap-extension/src/main/java/extension/org/mule/soap/it/TestServiceProviderWithCustomHeaders.java
+++ soap-connect/soap-extension/src/main/java/extension/org/mule/soap/it/TestServiceProviderWithCustomHeaders.java
@@ -9,7 +9,11 @@ package extension.org.mule.soap.it;
 import static java.util.Collections.singletonList;
 import static java.util.Collections.singletonMap;
 import static org.mule.runtime.extension.api.soap.WebServiceDefinition.builder;
+import org.mule.runtime.api.lifecycle.Disposable;
+import org.mule.runtime.api.lifecycle.Initialisable;
+import org.mule.runtime.api.lifecycle.InitialisationException;
 import org.mule.runtime.extension.api.annotation.Alias;
+import org.mule.runtime.extension.api.annotation.param.Optional;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
 import org.mule.runtime.extension.api.soap.SoapServiceProvider;
 import org.mule.runtime.extension.api.soap.WebServiceDefinition;
@@ -18,7 +22,9 @@ import java.util.List;
 import java.util.Map;
 
 @Alias("custom-headers")
-public class TestServiceProviderWithCustomHeaders implements SoapServiceProvider {
+public class TestServiceProviderWithCustomHeaders implements SoapServiceProvider, Initialisable, Disposable {
+
+  public static Environment ENVIRONMENT;
 
   @Parameter
   private String url;
@@ -29,6 +35,20 @@ public class TestServiceProviderWithCustomHeaders implements SoapServiceProvider
   @Parameter
   private String port;
 
+  @Parameter
+  @Optional(defaultValue = "PROD")
+  private Environment environment;
+
+  @Override
+  public void initialise() throws InitialisationException {
+    ENVIRONMENT = environment;
+  }
+
+  @Override
+  public void dispose() {
+    ENVIRONMENT = null;
+  }
+
   public List<WebServiceDefinition> getWebServiceDefinitions() {
     return singletonList(builder().withId("2").withWsdlUrl(url).withService(service).withPort(port).build());
   }
@@ -36,5 +56,9 @@ public class TestServiceProviderWithCustomHeaders implements SoapServiceProvider
   @Override
   public Map<String, String> getCustomHeaders(WebServiceDefinition definition, String operation) {
     return singletonMap("headerIn", "<con:headerIn xmlns:con=\"http://service.soap.service.mule.org/\">OP=" + operation + "</con:headerIn>");
+  }
+
+  public Environment getEnvironment() {
+    return environment;
   }
 }
