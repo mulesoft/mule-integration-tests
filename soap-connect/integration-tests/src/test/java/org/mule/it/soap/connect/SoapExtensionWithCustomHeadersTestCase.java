200f02c57ad48d97a63e0452e58ce3d94ad98b9b
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2017-10-24 18:21:41 -0300

    MULE-13867: Enum config parameters are not set correctly

--- soap-connect/integration-tests/src/test/java/org/mule/it/soap/connect/SoapExtensionWithCustomHeadersTestCase.java
+++ soap-connect/integration-tests/src/test/java/org/mule/it/soap/connect/SoapExtensionWithCustomHeadersTestCase.java
@@ -6,9 +6,13 @@
  */
 package org.mule.it.soap.connect;
 
+import static extension.org.mule.soap.it.Environment.PROD;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.mule.service.soap.SoapTestUtils.assertSimilarXml;
-
 import org.mule.runtime.api.message.Message;
+
+import extension.org.mule.soap.it.TestServiceProviderWithCustomHeaders;
 import org.junit.Test;
 
 public class SoapExtensionWithCustomHeadersTestCase extends AbstractSimpleServiceFunctionalTestCase {
@@ -24,6 +28,11 @@ public class SoapExtensionWithCustomHeadersTestCase extends AbstractSimpleServic
     assertSimilarXml("<ns2:noParamsWithHeaderResponse xmlns:ns2=\"http://service.soap.service.mule.org/\">"
         + "<text>OP=noParamsWithHeader</text>"
         + "</ns2:noParamsWithHeaderResponse>", ((String) m.getPayload().getValue()));
+  }
+
+  @Test
+  public void serviceProviderWithEnumParameter() throws Exception {
+    assertThat(TestServiceProviderWithCustomHeaders.ENVIRONMENT, is(PROD));
   }
 
 }
