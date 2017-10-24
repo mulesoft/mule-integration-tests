f0ff3b6b496dc78575b598364f2dd84b0935e65c
pablolagreca <Neruda10>
2017-10-24 15:48:52 -0300

    MULE-13830 - fix test cases

--- integration/src/test/java/org/mule/test/core/context/notification/processors/ComponentLocationTestCase.java
+++ integration/src/test/java/org/mule/test/core/context/notification/processors/ComponentLocationTestCase.java
@@ -21,8 +21,8 @@ import static org.mule.runtime.api.component.TypedComponentIdentifier.ComponentT
 import static org.mule.runtime.api.component.TypedComponentIdentifier.ComponentType.SOURCE;
 import static org.mule.runtime.api.component.TypedComponentIdentifier.ComponentType.UNKNOWN;
 import static org.mule.runtime.api.component.TypedComponentIdentifier.builder;
-import static org.mule.runtime.config.internal.model.ApplicationModel.FLOW_IDENTIFIER;
-import static org.mule.runtime.config.internal.model.ApplicationModel.SUBFLOW_IDENTIFIER;
+import static org.mule.runtime.config.api.dsl.CoreDslConstants.FLOW_IDENTIFIER;
+import static org.mule.runtime.config.api.dsl.CoreDslConstants.SUBFLOW_IDENTIFIER;
 import static org.mule.test.allure.AllureConstants.ConfigurationComponentLocatorFeature.CONFIGURATION_COMPONENT_LOCATOR;
 import static org.mule.test.allure.AllureConstants.ConfigurationComponentLocatorFeature.ConfigurationComponentLocationStory.COMPONENT_LOCATION;
 import org.mule.runtime.api.component.Component;
