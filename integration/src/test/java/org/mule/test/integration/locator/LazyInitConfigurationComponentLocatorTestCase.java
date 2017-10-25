eebd0e4715c296630afc72ef3dcbc4a396f86f1b
pablolagreca <pablolagreca@hotmail.com>
2017-10-25 17:08:18 -0300

    MULE-13666 - Lazy Init does not handle spring:config (#395)

--- integration/src/test/java/org/mule/test/integration/locator/LazyInitConfigurationComponentLocatorTestCase.java
+++ integration/src/test/java/org/mule/test/integration/locator/LazyInitConfigurationComponentLocatorTestCase.java
@@ -13,19 +13,21 @@ import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.sameInstance;
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.mule.runtime.api.component.location.Location.builder;
+import static org.mule.runtime.api.component.location.Location.builderFromStringRepresentation;
 import static org.mule.runtime.config.api.LazyComponentInitializer.LAZY_COMPONENT_INITIALIZER_SERVICE_KEY;
 import static org.mule.runtime.config.api.SpringXmlConfigurationBuilderFactory.createConfigurationBuilder;
 import static org.mule.test.allure.AllureConstants.ConfigurationComponentLocatorFeature.CONFIGURATION_COMPONENT_LOCATOR;
 import static org.mule.test.allure.AllureConstants.ConfigurationComponentLocatorFeature.ConfigurationComponentLocatorStory.SEARCH_CONFIGURATION;
-
+import org.mule.extension.spring.api.SpringConfig;
+import org.mule.runtime.api.artifact.Registry;
 import org.mule.runtime.api.component.location.ComponentLocation;
 import org.mule.runtime.config.api.LazyComponentInitializer;
 import org.mule.runtime.core.api.config.ConfigurationBuilder;
 import org.mule.test.AbstractIntegrationTestCase;
-
-import org.junit.Test;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -33,10 +35,15 @@ import javax.inject.Named;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.junit.Test;
 
 @Feature(CONFIGURATION_COMPONENT_LOCATOR)
 @Story(SEARCH_CONFIGURATION)
 public class LazyInitConfigurationComponentLocatorTestCase extends AbstractIntegrationTestCase {
+
+  @Inject
+  private Registry registry;
 
   @Inject
   @Named(value = LAZY_COMPONENT_INITIALIZER_SERVICE_KEY)
@@ -44,10 +51,14 @@ public class LazyInitConfigurationComponentLocatorTestCase extends AbstractInteg
 
   @Override
   protected String[] getConfigFiles() {
-    // TODO (MULE-13666): add config "org/mule/test/integration/locator/component-locator-spring-config.xml"
-    // Cannot add it until MULE-13666 is not fixed, since lazy init will throw NPE
     return new String[] {"org/mule/test/integration/locator/component-locator-config.xml",
-        "org/mule/test/integration/locator/component-locator-levels-config.xml"};
+        "org/mule/test/integration/locator/component-locator-levels-config.xml",
+        "org/mule/test/integration/locator/component-locator-spring-config.xml"};
+  }
+
+  @Override
+  public boolean enableLazyInit() {
+    return true;
   }
 
   @Override
@@ -99,7 +110,8 @@ public class LazyInitConfigurationComponentLocatorTestCase extends AbstractInteg
                                   "requestConfig",
                                   "requestConfig/0",
                                   "tlsContextRef",
-                                  "tlsContextRef/0"));
+                                  "tlsContextRef/0",
+                                  "springConfig"));
     assertThat(locator.find(builder().globalName("myFlow").build()), is(empty()));
     assertThat(locator.find(builder().globalName("anotherFlow").build()), is(empty()));
   }
@@ -122,13 +134,35 @@ public class LazyInitConfigurationComponentLocatorTestCase extends AbstractInteg
   @Test
   public void lazyMuleContextRefreshesConfigurationComponentLoader() {
     lazyComponentInitializer.initializeComponent(builder().globalName("myFlow").build());
-    assertThat(locator.findAllLocations(), hasSize(32));
+    assertThat(locator.findAllLocations(), hasSize(33));
 
     lazyComponentInitializer.initializeComponent(builder().globalName("anotherFlow").build());
-    assertThat(locator.findAllLocations(), hasSize(32));
+    assertThat(locator.findAllLocations(), hasSize(33));
+
     assertThat(locator.find(builder().globalName("myFlow").build()), is(empty()));
     assertThat(locator.find(builder().globalName("anotherFlow").build()),
                is(not(empty())));
+  }
+
+  @Description("Lazy init should keep siblings enabled when requested")
+  @Test
+  public void lazyMuleContextSiblingNodesEnabled() {
+    lazyComponentInitializer.initializeComponents(componentLocation -> {
+      String location = componentLocation.getLocation();
+      return location.equals("myFlow/source/0") || location.equals("myFlow/processors/2/processors/0");
+    });
+    assertThat(muleContext.getConfigurationComponentLocator().findAllLocations(), hasSize(33));
+
+    lazyComponentInitializer.initializeComponents(componentLocation -> {
+      String location = componentLocation.getLocation();
+      return location.equals("myFlow/source/0") || location.equals("myFlow/processors/2/processors/0");
+    });
+
+    assertThat(locator.find(builderFromStringRepresentation("myFlow").build()), is(not(empty())));
+    assertThat(locator.find(builderFromStringRepresentation("myFlow/source").build()), is(not(empty())));
+    assertThat(locator.find(builderFromStringRepresentation("myFlow/processors/2").build()), is(not(empty())));
+    assertThat(locator.find(builderFromStringRepresentation("myFlow/processors/2/processors/0").build()), is(not(empty())));
+    assertThat(locator.find(builderFromStringRepresentation("springConfig").build()), is(not(empty())));
   }
 
   @Test
@@ -147,4 +181,42 @@ public class LazyInitConfigurationComponentLocatorTestCase extends AbstractInteg
     assertThat(locator.find(builder().globalName("tlsContextRef").build()), is(not(empty())));
   }
 
+
+  @Description("Lazy init should create spring components without dependencies")
+  @Test
+  public void lazyMuleContextInitializesSpringConfig() throws IllegalAccessException {
+    lazyComponentInitializer.initializeComponents(componentLocation -> componentLocation.getLocation().equals("myFlow"));
+
+
+    assertThat(locator.find(builderFromStringRepresentation("myFlow").build()), is(not(empty())));
+    assertThat(locator.find(builderFromStringRepresentation("myFlow/source").build()), is(not(empty())));
+    assertThat(locator.find(builderFromStringRepresentation("myFlow/processors/0").build()), is(not(empty())));
+    assertThat(locator.find(builderFromStringRepresentation("myFlow/processors/1").build()), is(not(empty())));
+    assertThat(locator.find(builderFromStringRepresentation("myFlow/processors/2").build()), is(not(empty())));
+    assertThat(locator.find(builderFromStringRepresentation("myFlow/processors/2/processors/0").build()), is(not(empty())));
+    assertThat(locator.find(builderFromStringRepresentation("myFlow/processors/2/processors/1").build()), is(not(empty())));
+    assertThat(locator.find(builderFromStringRepresentation("springConfig").build()), is(not(empty())));
+
+    assertThat(registry.lookupByName("springConfig").isPresent(), is(true));
+
+    SpringConfig springConfig = (SpringConfig) registry.lookupByName("springConfig").get();
+    Object applicationContext = FieldUtils.readField(springConfig, "applicationContext", true);
+    assertThat("springConfig was not configured", applicationContext, notNullValue());
+  }
+
+  @Description("Spring component should be created each time as the rest")
+  @Test
+  public void lazyMuleContextSpringConfigRebuilt() throws IllegalAccessException {
+    lazyComponentInitializer.initializeComponents(componentLocation -> componentLocation.getLocation().equals("myFlow"));
+    assertThat(locator.find(builderFromStringRepresentation("springConfig").build()), is(not(empty())));
+    Object firstObj = locator.find(builderFromStringRepresentation("springConfig").build()).get();
+
+    lazyComponentInitializer.initializeComponents(componentLocation -> componentLocation.getLocation().equals("anotherFlow"));
+    assertThat(locator.find(builderFromStringRepresentation("springConfig").build()), is(not(empty())));
+    Object secondObj = locator.find(builderFromStringRepresentation("springConfig").build()).get();
+    Object secondAppContext = FieldUtils.readField(secondObj, "applicationContext", true);
+    assertThat("springConfig was not configured", secondAppContext, notNullValue());
+
+    assertThat(firstObj, not(sameInstance(secondObj)));
+  }
 }
