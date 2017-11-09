4edbbd4a514aeb658d8e41d9905aa97a7c2bf57a
mbuchwald <martin.buchwald@mulesoft.com>
2017-11-09 18:01:05 -0300

    MULE-14016: set-variable is setting mime type */* always instead of using the DW return data type (#412)

--- integration/src/test/java/org/mule/test/core/transformers/simple/SetFlowVariableDataTypeTestCase.java
+++ integration/src/test/java/org/mule/test/core/transformers/simple/SetFlowVariableDataTypeTestCase.java
@@ -7,16 +7,18 @@
 
 package org.mule.test.core.transformers.simple;
 
-import static java.nio.charset.StandardCharsets.UTF_16;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mule.runtime.api.metadata.MediaType.ANY;
+import static org.mule.runtime.api.metadata.MediaType.JSON;
+import static org.mule.runtime.api.metadata.MediaType.XML;
 import static org.mule.tck.junit4.matcher.DataTypeMatcher.like;
 
-import org.mule.runtime.api.exception.MuleException;
-import org.mule.runtime.api.message.Message;
 import org.mule.runtime.api.metadata.DataType;
 import org.mule.runtime.api.metadata.MediaType;
+import org.mule.runtime.api.streaming.bytes.CursorStreamProvider;
 import org.mule.runtime.core.api.event.CoreEvent;
-import org.mule.runtime.core.api.processor.Processor;
 import org.mule.test.AbstractIntegrationTestCase;
 
 import org.junit.Test;
@@ -30,21 +32,19 @@ public class SetFlowVariableDataTypeTestCase extends AbstractIntegrationTestCase
 
   @Test
   public void setsPropertyDataType() throws Exception {
-    final CoreEvent muleEvent = flowRunner("main").withPayload(TEST_MESSAGE).run();
+    final CoreEvent event = flowRunner("setVariableFlow").withPayload(TEST_MESSAGE).run();
+    DataType dataType1 = event.getVariables().get("var1").getDataType();
+    DataType dataType2 = event.getVariables().get("var2").getDataType();
+    DataType dataType3 = event.getVariables().get("var3").getDataType();
+    DataType dataType4 = event.getVariables().get("var4").getDataType();
 
-    Message response = muleEvent.getMessage();
-    DataType dataType = (DataType) response.getPayload().getValue();
-    assertThat(dataType, like(String.class, MediaType.XML, UTF_16));
+    assertThat(dataType1, like(String.class, XML, UTF_8));
+    assertThat(dataType2, like(String.class, ANY, UTF_8));
+    assertThat(dataType3, like(String.class, XML, UTF_8));
+
+    final MediaType JSON_UTF8 = MediaType.create(JSON.getPrimaryType(), JSON.getSubType(), UTF_8);
+    assertThat(CursorStreamProvider.class.isAssignableFrom(dataType4.getType()), is(true));
+    assertThat(dataType4.getMediaType(), is(JSON_UTF8));
   }
 
-  public static class FlowVariableDataTypeAccessor implements Processor {
-
-    @Override
-    public CoreEvent process(CoreEvent event) throws MuleException {
-      return CoreEvent.builder(event)
-          .message(Message.builder(event.getMessage()).value(event.getVariables().get("testVariable").getDataType()).build())
-          .build();
-    }
-
-  }
 }
