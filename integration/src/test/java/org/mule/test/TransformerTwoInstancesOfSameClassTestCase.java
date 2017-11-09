821add490f86c403d818692fb976ca666998c9cf
luciano.raineri <luciano.raineri@mulesoft.com>
2017-11-09 15:23:05 -0300

    Added test to reproduce the issue

---
+++ integration/src/test/java/org/mule/test/TransformerTwoInstancesOfSameClassTestCase.java
@@ -0,0 +1,45 @@
+package org.mule.test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import org.mule.runtime.core.api.transformer.AbstractTransformer;
+import org.mule.runtime.core.api.transformer.TransformerException;
+
+import java.nio.charset.Charset;
+
+import org.junit.Test;
+
+public class TransformerTwoInstancesOfSameClassTestCase extends AbstractIntegrationTestCase {
+
+  @Override
+  protected String getConfigFile() {
+    return "org/mule/test/transformers/transformer-two-instances-same-class.xml";
+  }
+
+  @Test
+  public void differentValues() throws Exception {
+    final CustomTransformer appendStringA = (CustomTransformer) registry.lookupByName("appendStringA").get();
+    final CustomTransformer appendStringB = (CustomTransformer) registry.lookupByName("appendStringB").get();
+
+    assertThat(appendStringA.getMessage(), is(" A"));
+    assertThat(appendStringB.getMessage(), is(" B"));
+  }
+
+  public static class CustomTransformer extends AbstractTransformer {
+
+    private String message;
+
+    @Override
+    protected Object doTransform(Object src, Charset enc) throws TransformerException {
+      return src;
+    }
+
+    public void setMessage(String message) {
+      this.message = message;
+    }
+
+    public String getMessage() {
+      return this.message;
+    }
+  }
+}
