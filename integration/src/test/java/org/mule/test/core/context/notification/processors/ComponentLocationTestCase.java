860ba34c909438598562c6b6d452daf60a5f9737
luciano.raineri <luciano.raineri@mulesoft.com>
2017-11-15 14:21:22 -0300

    fixed flaky tets

--- integration/src/test/java/org/mule/test/core/context/notification/processors/ComponentLocationTestCase.java
+++ integration/src/test/java/org/mule/test/core/context/notification/processors/ComponentLocationTestCase.java
@@ -6,11 +6,13 @@
  */
 package org.mule.test.core.context.notification.processors;
 
+import static java.lang.String.format;
 import static java.util.Arrays.asList;
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import static org.mule.runtime.api.component.AbstractComponent.LOCATION_KEY;
 import static org.mule.runtime.api.component.ComponentIdentifier.buildFromStringRepresentation;
 import static org.mule.runtime.api.component.TypedComponentIdentifier.ComponentType.FLOW;
@@ -33,11 +35,15 @@ import org.mule.runtime.api.notification.MessageProcessorNotification;
 import org.mule.runtime.core.api.construct.Flow;
 import org.mule.runtime.dsl.api.component.config.DefaultComponentLocation;
 import org.mule.runtime.dsl.api.component.config.DefaultComponentLocation.DefaultLocationPart;
+import org.mule.tck.junit4.FlakinessDetectorTestRunner;
+import org.mule.tck.junit4.FlakyTest;
 import org.mule.tck.probe.PollingProber;
 import org.mule.tck.probe.Probe;
 import org.mule.test.AbstractIntegrationTestCase;
+import org.mule.test.runner.RunnerDelegateTo;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
 
 import javax.inject.Inject;
@@ -45,11 +51,11 @@ import javax.inject.Named;
 
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
-import org.junit.Ignore;
 import org.junit.Test;
 
 @Feature(CONFIGURATION_COMPONENT_LOCATOR)
 @Story(COMPONENT_LOCATION)
+@RunnerDelegateTo(FlakinessDetectorTestRunner.class)
 public class ComponentLocationTestCase extends AbstractIntegrationTestCase {
 
   private static final Optional<TypedComponentIdentifier> FLOW_TYPED_COMPONENT_IDENTIFIER =
@@ -296,6 +302,7 @@ public class ComponentLocationTestCase extends AbstractIntegrationTestCase {
   }
 
   @Test
+  @FlakyTest(times = 200)
   public void flowWithScatterGather() throws Exception {
     flowRunner("flowWithScatterGather").run();
     waitUntilNotificationsArrived(4);
@@ -304,28 +311,34 @@ public class ComponentLocationTestCase extends AbstractIntegrationTestCase {
     DefaultComponentLocation scatterGatherLocation =
         flowWithSplitterProcessorsLocation.appendLocationPart("0", SCATTER_GATHER, CONFIG_FILE_NAME, of(99));
     assertNextProcessorLocationIs(scatterGatherLocation);
+
     DefaultComponentLocation scatterGatherRoute0 = scatterGatherLocation
         .appendRoutePart()
         .appendLocationPart("0", ROUTE, CONFIG_FILE_NAME, of(100));
-    assertNextProcessorLocationIs(scatterGatherRoute0
-        .appendProcessorsPart()
-        .appendLocationPart("0", LOGGER, CONFIG_FILE_NAME, of(101)));
     DefaultComponentLocation scatterGatherRouter1 = scatterGatherLocation
         .appendRoutePart()
         .appendLocationPart("1", ROUTE, CONFIG_FILE_NAME, of(103));
-    assertNextProcessorLocationIs(scatterGatherRouter1
-        .appendProcessorsPart()
-        .appendLocationPart("0", VALIDATION_IS_TRUE, CONFIG_FILE_NAME, of(104)));
-    DefaultComponentLocation scatterGatherRouter2 = scatterGatherLocation.appendRoutePart()
+    DefaultComponentLocation scatterGatherRouter2 = scatterGatherLocation
+        .appendRoutePart()
         .appendLocationPart("2", ROUTE, CONFIG_FILE_NAME, of(106));
-    assertNextProcessorLocationIs(scatterGatherRouter2
+
+    List<DefaultComponentLocation> nextLocations = asList(scatterGatherRoute0
         .appendProcessorsPart()
-        .appendLocationPart("0", VALIDATION_IS_FALSE, CONFIG_FILE_NAME, of(107)));
+        .appendLocationPart("0", LOGGER, CONFIG_FILE_NAME, of(101)),
+                                                          scatterGatherRouter1
+                                                              .appendProcessorsPart()
+                                                              .appendLocationPart("0", VALIDATION_IS_TRUE, CONFIG_FILE_NAME,
+                                                                                  of(104)),
+                                                          scatterGatherRouter2
+                                                              .appendProcessorsPart()
+                                                              .appendLocationPart("0", VALIDATION_IS_FALSE, CONFIG_FILE_NAME,
+                                                                                  of(107)));
+
+    assertNextProcessorLocationsAre(nextLocations);
     assertNoNextProcessorNotification();
   }
 
   @Test
-  @Ignore("MULE-13456")
   public void flowWithAsync() throws Exception {
     flowRunner("flowWithAsync").run();
     waitUntilNotificationsArrived(3);
@@ -385,9 +398,29 @@ public class ComponentLocationTestCase extends AbstractIntegrationTestCase {
     assertThat(processorNotificationStore.getNotifications().isEmpty(), is(false));
     MessageProcessorNotification processorNotification =
         processorNotificationStore.getNotifications().get(0);
-    processorNotificationStore.getNotifications().remove(0);
     assertThat(processorNotification.getComponent().getLocation().getLocation(), is(componentLocation.getLocation()));
     assertThat(processorNotification.getComponent().getLocation(), is(componentLocation));
+    processorNotificationStore.getNotifications().remove(0);
+  }
+
+  //Check in any order
+  private void assertNextProcessorLocationsAre(List<DefaultComponentLocation> componentLocations) {
+    StringBuilder errors = new StringBuilder();
+    for (int i = 0; i < componentLocations.size(); i++) {
+      for (DefaultComponentLocation componentLocation : componentLocations) {
+        try {
+          assertNextProcessorLocationIs(componentLocation);
+          errors = new StringBuilder();
+          break;
+        } catch (AssertionError e) {
+          errors.append(e.getMessage());
+        }
+      }
+      String errorString = errors.toString();
+      if (!errorString.isEmpty()) {
+        fail(format("Not every componentLocation was found in the notification list. %s", errorString));
+      }
+    }
   }
 
   private ProcessorNotificationStore getNotificationsStore() {
