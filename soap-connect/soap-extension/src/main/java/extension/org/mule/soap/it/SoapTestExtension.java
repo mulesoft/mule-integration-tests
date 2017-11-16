d0deed0b86bd08a2aca595253f4d76ed2efccf8e
Juan Desimoni <juan.desimoni@mulesoft.com>
2017-11-16 14:10:33 -0300

    MULE-14095: Soap connect transport should not be bounded in mule soap support (#418)

--- soap-connect/soap-extension/src/main/java/extension/org/mule/soap/it/SoapTestExtension.java
+++ soap-connect/soap-extension/src/main/java/extension/org/mule/soap/it/SoapTestExtension.java
@@ -7,12 +7,12 @@
 package extension.org.mule.soap.it;
 
 import org.mule.runtime.extension.api.annotation.Extension;
-import org.mule.runtime.extension.api.soap.HttpMessageDispatcherProvider;
 import org.mule.runtime.extension.api.soap.annotation.Soap;
 import org.mule.runtime.extension.api.soap.annotation.SoapMessageDispatcherProviders;
+import org.mule.runtime.module.extension.soap.api.runtime.connection.transport.DefaultHttpMessageDispatcherProvider;
 
 @Soap({TestServiceProvider.class, TestServiceProviderWithCustomHeaders.class})
-@SoapMessageDispatcherProviders(HttpMessageDispatcherProvider.class)
+@SoapMessageDispatcherProviders(TestHttpDispatcherProvider.class)
 @Extension(name = "simple")
 public class SoapTestExtension {
 
