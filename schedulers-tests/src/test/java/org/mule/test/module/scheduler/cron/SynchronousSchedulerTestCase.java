9d18e29ba828ecdda015797ad7730d2c518d461b
Rodrigo Merino <elrodro83@users.noreply.github.com>
2017-12-04 14:07:10 -0300

    MULE-14152: Poll Doesn't wait for the execution to finish until it sends (#434)

--- schedulers-tests/src/test/java/org/mule/test/module/scheduler/cron/SynchronousSchedulerTestCase.java
+++ schedulers-tests/src/test/java/org/mule/test/module/scheduler/cron/SynchronousSchedulerTestCase.java
@@ -7,19 +7,23 @@
 package org.mule.test.module.scheduler.cron;
 
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.junit.Assert.assertThat;
+
 import org.mule.functional.api.component.EventCallback;
 import org.mule.runtime.core.api.MuleContext;
 import org.mule.runtime.core.api.event.CoreEvent;
-import org.mule.tck.probe.JUnitLambdaProbe;
-import org.mule.tck.probe.PollingProber;
 import org.mule.test.AbstractSchedulerTestCase;
+
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import org.junit.Test;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * <p>
@@ -28,7 +32,14 @@ import org.junit.Test;
  */
 public class SynchronousSchedulerTestCase extends AbstractSchedulerTestCase {
 
-  private static List<String> foo = new ArrayList<>();
+  private static List<String> fooSync = new ArrayList<>();
+  private static List<String> fooNonSync = new ArrayList<>();
+
+  @Before
+  public void before() {
+    FooSync.latch = new CountDownLatch(2);
+    FooNonSync.latch = new CountDownLatch(2);
+  }
 
   @Override
   protected String getConfigFile() {
@@ -36,19 +47,46 @@ public class SynchronousSchedulerTestCase extends AbstractSchedulerTestCase {
   }
 
   @Test
-  public void test() throws InterruptedException {
-    new PollingProber(7000, 100).check(new JUnitLambdaProbe(() -> {
-      assertThat(foo, hasSize(1));
-      return true;
-    }));
+  public void testSynchronous() throws InterruptedException {
+    assertThat(FooSync.latch.await(7000, MILLISECONDS), is(false));
+    assertThat(fooSync, hasSize(1));
   }
 
-  public static class Foo implements EventCallback {
+  @Test
+  public void testNonSynchronous() throws InterruptedException {
+    assertThat(FooNonSync.latch.await(7000, MILLISECONDS), is(true));
+    assertThat(fooNonSync, hasSize(greaterThanOrEqualTo(2)));
+  }
+
+  public static class FooNonSync implements EventCallback {
+
+    public static CountDownLatch latch;
 
     @Override
     public void eventReceived(CoreEvent event, Object component, MuleContext muleContext) throws Exception {
-      synchronized (foo) {
-        foo.add((String) event.getMessage().getPayload().getValue());
+      latch.countDown();
+
+      synchronized (fooNonSync) {
+        fooNonSync.add((String) event.getMessage().getPayload().getValue());
+        try {
+          Thread.sleep(10000);
+        } catch (InterruptedException e) {
+
+        }
+      }
+    }
+  }
+
+  public static class FooSync implements EventCallback {
+
+    public static CountDownLatch latch;
+
+    @Override
+    public void eventReceived(CoreEvent event, Object component, MuleContext muleContext) throws Exception {
+      latch.countDown();
+
+      synchronized (fooSync) {
+        fooSync.add((String) event.getMessage().getPayload().getValue());
         try {
           Thread.sleep(10000);
         } catch (InterruptedException e) {
