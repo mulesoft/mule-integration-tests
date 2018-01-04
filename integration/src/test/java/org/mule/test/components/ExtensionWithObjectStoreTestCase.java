c2bc0f474437ee044ee3fce76df704ce28ffda2c
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2018-01-04 18:51:34 -0300

    test

---
+++ integration/src/test/java/org/mule/test/components/ExtensionWithObjectStoreTestCase.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.components;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import org.mule.runtime.api.store.ObjectStore;
+import org.mule.runtime.api.store.ObjectStoreManager;
+import org.mule.test.AbstractIntegrationTestCase;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+
+public class ExtensionWithObjectStoreTestCase extends AbstractIntegrationTestCase {
+
+  @Inject
+  private ObjectStoreManager objectStoreManager;
+
+  @Override
+  protected String getConfigFile() {
+    return "extension-with-objectstore-config.xml";
+  }
+
+  @Test
+  public void storeOnGlobalStore() throws Exception {
+    flowRunner("storeMoneyOnGlobalStore").run();
+
+    ObjectStore<Long> objectStore = objectStoreManager.getObjectStore("bank");
+    assertThat(objectStore.retrieve("money"), equalTo(1234L));
+  }
+}
