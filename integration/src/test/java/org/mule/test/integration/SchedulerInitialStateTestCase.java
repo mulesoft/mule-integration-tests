057b53c622a5b098e42b2b66b559d2e69c45f144
pablolagreca <pablolagreca@hotmail.com>
2018-01-09 10:40:55 -0300

    Revert "MULE-14298 - Overriding _muleComponentInitialStateManager does not work (#457)" (#459)
    This reverts commit c3352bb349d1eff7939bad3e1a580af2c3717f5a.

--- integration/src/test/java/org/mule/test/integration/SchedulerInitialStateTestCase.java
+++ integration/src/test/java/org/mule/test/integration/SchedulerInitialStateTestCase.java
@@ -6,7 +6,6 @@
  */
 package org.mule.test.integration;
 
-import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
@@ -14,64 +13,42 @@ import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.junit.Assert.assertThat;
 import static org.mule.runtime.api.deployment.management.ComponentInitialStateManager.SERVICE_ID;
-import static org.mule.runtime.config.api.SpringXmlConfigurationBuilderFactory.createConfigurationBuilder;
 import static org.mule.test.allure.AllureConstants.SchedulerServiceFeature.SCHEDULER_SERVICE;
 import static org.mule.test.allure.AllureConstants.SchedulerServiceFeature.SchedulerServiceStory.SOURCE_MANAGEMENT;
+
 import org.mule.functional.api.component.TestConnectorQueueHandler;
-import org.mule.runtime.api.component.Component;
 import org.mule.runtime.api.component.location.Location;
 import org.mule.runtime.api.config.custom.ServiceConfigurator;
 import org.mule.runtime.api.deployment.management.ComponentInitialStateManager;
 import org.mule.runtime.api.event.Event;
+import org.mule.runtime.api.exception.MuleException;
+import org.mule.runtime.api.component.Component;
 import org.mule.runtime.api.source.SchedulerMessageSource;
-import org.mule.runtime.config.api.LazyComponentInitializer;
 import org.mule.runtime.core.api.MuleContext;
 import org.mule.runtime.core.api.config.ConfigurationBuilder;
+import org.mule.runtime.core.api.config.ConfigurationException;
 import org.mule.tck.probe.PollingProber;
 import org.mule.tck.probe.Probe;
 import org.mule.test.AbstractIntegrationTestCase;
-import org.mule.test.runner.RunnerDelegateTo;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
-import javax.inject.Inject;
+import org.junit.Test;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
-import org.junit.Test;
-import org.junit.runners.Parameterized;
 
 @Feature(SCHEDULER_SERVICE)
 @Story(SOURCE_MANAGEMENT)
-@RunnerDelegateTo(Parameterized.class)
 public class SchedulerInitialStateTestCase extends AbstractIntegrationTestCase {
 
   private List<Component> recordedOnStartMessageSources = new ArrayList<>();
 
-  @Inject
-  private LazyComponentInitializer lazyComponentInitializer;
-
-  @Parameterized.Parameter
-  public boolean lazyInitEnabled;
-
-  @Parameterized.Parameters(name = "{index}: Running tests for {0} (validating XML [{2}]) ")
-  public static Collection<Object[]> data() {
-    return asList(new Object[] {true}, new Object[] {false});
-  }
-
   @Override
   protected String getConfigFile() {
     return "org/mule/test/integration/scheduler-initial-state-management-config.xml";
-  }
-
-  @Override
-  protected ConfigurationBuilder getBuilder() throws Exception {
-    final ConfigurationBuilder configurationBuilder = createConfigurationBuilder(getConfigFile(), lazyInitEnabled);
-    configureSpringXmlConfigurationBuilder(configurationBuilder);
-    return configurationBuilder;
   }
 
   @Override
@@ -80,7 +57,7 @@ public class SchedulerInitialStateTestCase extends AbstractIntegrationTestCase {
     builders.add(new ConfigurationBuilder() {
 
       @Override
-      public void configure(MuleContext muleContext) {
+      public void configure(MuleContext muleContext) throws ConfigurationException {
         muleContext.getCustomizationService().overrideDefaultServiceImpl(SERVICE_ID, createCustomStateManager());
       }
 
@@ -105,7 +82,6 @@ public class SchedulerInitialStateTestCase extends AbstractIntegrationTestCase {
   @Description("ComponentInitialStateManager is called during startup for all message sources")
   @Test
   public void startMessageSourceRequestedOnStartup() {
-    lazyComponentInitializer.initializeComponents(componentLocation -> true);
     assertThat(recordedOnStartMessageSources, hasSize(2));
     assertThat(recordedOnStartMessageSources.stream()
         .map(component -> component.getLocation().getLocation()).collect(toList()),
@@ -114,8 +90,7 @@ public class SchedulerInitialStateTestCase extends AbstractIntegrationTestCase {
 
   @Description("ComponentInitialStateManager does not allow to start scheduler message sources")
   @Test
-  public void verifyMessageSourcesAreNotStarted() {
-    lazyComponentInitializer.initializeComponents(componentLocation -> true);
+  public void verifyMessageSourcesAreNotStarted() throws MuleException {
     SchedulerMessageSource schedulerMessageSource = (SchedulerMessageSource) muleContext.getConfigurationComponentLocator()
         .find(Location.builder().globalName("runningSchedulerOnStartup").addSourcePart().build()).get();
     assertThat(schedulerMessageSource.isStarted(), is(true));
