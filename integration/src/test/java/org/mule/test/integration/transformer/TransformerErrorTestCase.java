1e406725edd025de0955cfdb5b5248420c8ca1ff
Ana Felisatti <afelisatti@mulesoft.com>
2018-03-12 14:05:40 -0300

    MULE-14560: Not understandable exception is thrown when a transformation fails over a String with '%d' (#496)
    (cherry picked from commit 9161a9c9f9f1346d250a48c2eb041df507e417ca)

---
+++ integration/src/test/java/org/mule/test/integration/transformer/TransformerErrorTestCase.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.integration.transformer;
+
+import static org.hamcrest.Matchers.containsString;
+import org.mule.functional.api.exception.ExpectedError;
+import org.mule.test.AbstractIntegrationTestCase;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class TransformerErrorTestCase extends AbstractIntegrationTestCase {
+
+  @Rule
+  public ExpectedError expectedError = ExpectedError.none();
+
+  @Override
+  protected String getConfigFile() {
+    return "org/mule/test/transformers/transformation-error-config.xml";
+  }
+
+  @Test
+  public void errorFormattingDoesNotAffectData() throws Exception {
+    expectedError.expectErrorType("MULE", "TRANSFORMATION");
+    expectedError.expectMessage(containsString("Could not find a transformer"));
+    flowRunner("transformFail").run();
+  }
+
+}
