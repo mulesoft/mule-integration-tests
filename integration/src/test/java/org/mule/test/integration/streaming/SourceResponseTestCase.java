01a37f947a3fcac877a2b4f09b8e17644370301e
pablolagreca <Neruda10>
2018-04-06 15:01:06 -0300

    MULE-14735 - Avoid iterating parameters map on operation policies (#511)
    (cherry picked from commit feb5232178a7219b9ab1b9a7b953e56acb7bd7f6)

---
+++ integration/src/test/java/org/mule/test/integration/streaming/SourceResponseTestCase.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.integration.streaming;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertThat;
+import org.mule.runtime.core.api.util.IOUtils;
+import org.mule.runtime.http.api.HttpService;
+import org.mule.runtime.http.api.domain.message.request.HttpRequest;
+import org.mule.runtime.http.api.domain.message.response.HttpResponse;
+import org.mule.service.http.TestHttpClient;
+import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.test.AbstractIntegrationTestCase;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class SourceResponseTestCase extends AbstractIntegrationTestCase {
+
+  @Rule
+  public DynamicPort httpPort = new DynamicPort("httpPort");
+
+  @Rule
+  public TestHttpClient httpClient = new TestHttpClient.Builder(getService(HttpService.class)).build();
+
+  @Override
+  protected String getConfigFile() {
+    return "org/mule/test/integration/streaming/source-response-config.xml";
+  }
+
+  @Test
+  public void responseParameterWithCursor() throws IOException, TimeoutException {
+    HttpResponse httpResponse =
+        httpClient.send(HttpRequest.builder().uri(String.format("http://localhost:%s/", httpPort.getNumber())).build(),
+                        RECEIVE_TIMEOUT, true, null);
+    assertThat(httpResponse.getStatusCode(), is(200));
+    String responsePayload = IOUtils.toString(httpResponse.getEntity().getContent());
+    assertThat(responsePayload, containsString("<name>Foo</name>"));
+    assertThat(responsePayload, containsString("<lastName>Bar</lastName>"));
+  }
+
+}
