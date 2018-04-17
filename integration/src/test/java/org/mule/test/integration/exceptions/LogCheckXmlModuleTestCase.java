9076bbf1352f95c1ff1ef2fc647d0abe2fed831e
Juan Lautaro Fernandez <fernandez.lautaro@gmail.com>
2018-04-17 11:49:12 -0300

    MULE-14647: smart connector locations reflect call invocation rather than implementation (#518)

---
+++ integration/src/test/java/org/mule/test/integration/exceptions/LogCheckXmlModuleTestCase.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.integration.exceptions;
+
+import static org.mule.runtime.api.exception.MuleException.MULE_VERBOSE_EXCEPTIONS;
+import static org.mule.runtime.api.exception.MuleException.refreshVerboseExceptions;
+import org.junit.AfterClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mule.tck.junit4.rule.SystemProperty;
+import org.mule.test.AbstractIntegrationTestCase;
+
+public class LogCheckXmlModuleTestCase extends AbstractIntegrationTestCase {
+
+  // Just to ensure the previous value is set after the test
+  @ClassRule
+  public static SystemProperty verboseExceptions = new SystemProperty(MULE_VERBOSE_EXCEPTIONS, Boolean.toString(false));
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  @AfterClass
+  public static void afterClass() {
+    refreshVerboseExceptions();
+  }
+
+  @Override
+  protected String getConfigFile() {
+    return "org/mule/test/integration/exceptions/log-check-xml-module-config.xml";
+  }
+
+  @Test
+  public void runXmlSdkOperationError() throws Exception {
+    runSuccesses(true, "xmlSdkOperationError");
+  }
+
+  @Test
+  public void runXmlSdkOperationErrorNested() throws Exception {
+    runSuccesses(true, "xmlSdkOperationErrorNested");
+  }
+
+  private void runSuccesses(boolean verboseExceptions, String flowName) throws Exception {
+    System.setProperty(MULE_VERBOSE_EXCEPTIONS, Boolean.toString(verboseExceptions));
+    refreshVerboseExceptions();
+    flowRunner(flowName).run();
+  }
+
+}
