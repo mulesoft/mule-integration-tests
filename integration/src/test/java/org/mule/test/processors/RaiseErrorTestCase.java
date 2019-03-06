736d96d8576b54c67c21e23f7f7c54b88cf77896
Luciano RaineriMarchina <luciano.raineri@mulesoft.com>
2019-03-06 15:22:22 -0300

    added tests to show failurew

--- integration/src/test/java/org/mule/test/processors/RaiseErrorTestCase.java
+++ integration/src/test/java/org/mule/test/processors/RaiseErrorTestCase.java
@@ -109,4 +109,10 @@ public class RaiseErrorTestCase extends AbstractIntegrationTestCase {
                hasPayload(equalTo("Scatter gather route failed.")));
   }
 
+  @Test
+  public void withinForEachAndTryScope() throws Exception {
+    //If error is properly catch, nothing should fail.
+    flowRunner("tryAndForEach").run();
+  }
+
 }
