d19f0a7bc809a4e727c778892f1bd17164d86854
Rodrigo Merino <elrodro83@users.noreply.github.com>
2019-04-05 13:17:39 -0300

    MULE-16411: Fix issues affecting mule-integration tests on Java 11 (#698)
    * Support Java 11 in soap-service test support

--- soap-connect/integration-tests/src/test/java/org/mule/it/soap/connect/AbstractSimpleServiceFunctionalTestCase.java
+++ soap-connect/integration-tests/src/test/java/org/mule/it/soap/connect/AbstractSimpleServiceFunctionalTestCase.java
@@ -7,9 +7,8 @@
 package org.mule.it.soap.connect;
 
 import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
-import org.mule.service.soap.server.HttpServer;
-import org.mule.service.soap.service.Soap11Service;
 import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.tck.junit4.rule.ExternalProcess;
 import org.mule.tck.junit4.rule.SystemProperty;
 
 import org.custommonkey.xmlunit.XMLUnit;
@@ -20,18 +19,20 @@ public abstract class AbstractSimpleServiceFunctionalTestCase extends MuleArtifa
   @Rule
   public DynamicPort port = new DynamicPort("testPort");
 
-  private HttpServer server = new HttpServer(port.getNumber(), null, null, new Soap11Service());;
+  // TODO MULE-16661 Use a Docker image instead of this
+  // This needs to run with a plain classloader to avoid conflicting libs in different artifacts in the Mule classloader
+  // hierarchy, in order to avoid a LinkageError when running in JDK 11 or higher.
+  @Rule
+  public ExternalProcess server =
+      new ExternalProcess(line -> line.contains("org.eclipse.jetty.server.ServerConnector: Started ServerConnector"),
+                          "java", "-cp", System.getProperty("soapHttpServerClasspath"), "org.mule.service.soap.server.HttpServer",
+                          "" + port.getNumber(), "org.mule.service.soap.service.Soap11Service");
 
   @Rule
-  public SystemProperty address = new SystemProperty("address", server.getDefaultAddress());;
+  public SystemProperty address = new SystemProperty("address", "http://localhost:" + port.getNumber() + "/server");
 
   @Override
   protected void doSetUp() throws Exception {
     XMLUnit.setIgnoreWhitespace(true);
-  }
-
-  @Override
-  protected void doTearDown() throws Exception {
-    server.stop();
   }
 }
