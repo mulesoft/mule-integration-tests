3d9afee9111ed1dffc2bd8351f0b1483a1527cb9
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2019-05-26 23:41:13 -0300

    MULE-16899: ClassCast exception when registry lookup finds no match

---
+++ integration/src/test/java/org/mule/test/spring/LookupNullObjectTestCase.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.spring;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mule.test.allure.AllureConstants.LifecycleAndDependencyInjectionFeature.LIFECYCLE_AND_DEPENDENCY_INJECTION;
+import static org.mule.test.allure.AllureConstants.LifecycleAndDependencyInjectionFeature.NULL_OBJECTS_IN_SPRING5_REGISTRY;
+import org.mule.test.AbstractIntegrationTestCase;
+
+import javax.transaction.TransactionManager;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.Test;
+
+@Feature(LIFECYCLE_AND_DEPENDENCY_INJECTION)
+@Story(NULL_OBJECTS_IN_SPRING5_REGISTRY)
+public class LookupNullObjectTestCase extends AbstractIntegrationTestCase {
+
+  @Override
+  protected String[] getConfigFiles() {
+    return new String[] {};
+  }
+
+  @Test
+  public void noTransactionManagerThroughMuleContext() {
+    TransactionManager txManager = muleContext.getTransactionManager();
+    assertThat(txManager, is(nullValue()));
+  }
+
+  @Test
+  public void noTransactionManagerThroughRegistryLookup() {
+    assertThat(registry.lookupByType(TransactionManager.class).isPresent(), is(false));
+    assertThat(registry.lookupAllByType(TransactionManager.class), hasSize(0));
+  }
+}
