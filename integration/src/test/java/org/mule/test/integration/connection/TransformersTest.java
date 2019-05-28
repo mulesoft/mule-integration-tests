b6ea0a522b4cb73048b64d5b2b4fea7f2fd8d8d0
Maria Mariotti <mmariotti@mmariotti-ltm.internal.salesforce.com>
2019-05-28 14:31:55 -0300

    MULE-15991: added license headers.

--- integration/src/test/java/org/mule/test/integration/connection/TransformersTest.java
+++ integration/src/test/java/org/mule/test/integration/connection/TransformersTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.test.integration.connection;
 
 
@@ -15,29 +21,31 @@ import static org.hamcrest.core.IsEqual.equalTo;
 
 public class TransformersTest extends LazyConnectionsTestCase {
 
-    @Rule
-    public DynamicPort port = new DynamicPort("port");
+  @Rule
+  public DynamicPort port = new DynamicPort("port");
 
 
-    @Rule
-    public DynamicPort port2 = new DynamicPort("port2");
+  @Rule
+  public DynamicPort port2 = new DynamicPort("port2");
 
 
-    @Rule
-    public TestHttpClient httpClient = new TestHttpClient.Builder(getService(HttpService.class)).build();
+  @Rule
+  public TestHttpClient httpClient = new TestHttpClient.Builder(getService(HttpService.class)).build();
 
-    @Override
-    protected String getConfigFile() { return "transformer-test-config.xml";}
+  @Override
+  protected String getConfigFile() {
+    return "transformer-test-config.xml";
+  }
 
-    @Test
-    public void iteratorToByteArrayTransformerThrowsException() throws Exception {
-        HttpResponse response = Request.Post(getUrl("test/listener")).execute().returnResponse();
-        System.out.println(IOUtils.toString(response.getEntity().getContent()));
-        assertThat(response.getStatusLine().getStatusCode(), equalTo(500));
-    }
+  @Test
+  public void iteratorToByteArrayTransformerThrowsException() throws Exception {
+    HttpResponse response = Request.Post(getUrl("test/listener")).execute().returnResponse();
+    System.out.println(IOUtils.toString(response.getEntity().getContent()));
+    assertThat(response.getStatusLine().getStatusCode(), equalTo(500));
+  }
 
-    private String getUrl(String path) {
-        return String.format("http://localhost:%s/%s", port.getNumber(), path);
-    }
+  private String getUrl(String path) {
+    return String.format("http://localhost:%s/%s", port.getNumber(), path);
+  }
 
 }
