53eff4044061eff23ea9b6e834b99b9482039c07
Juli <luzzijulieta@gmail.com>
2019-06-26 10:37:17 -0300

    MULE-16984: Interception API: Smart connectors inside a choice router are not skipped properly

--- integration/src/test/java/org/mule/test/core/context/notification/processors/ModuleComponentPathTestCase.java
+++ integration/src/test/java/org/mule/test/core/context/notification/processors/ModuleComponentPathTestCase.java
@@ -36,8 +36,8 @@ import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.initialiseIfNee
 import static org.mule.runtime.dsl.api.xml.parser.XmlConfigurationDocumentLoader.noValidationDocumentLoader;
 import static org.mule.runtime.dsl.api.xml.parser.XmlConfigurationProcessor.processXmlConfiguration;
 import static org.mule.runtime.module.extension.api.util.MuleExtensionUtils.createDefaultExtensionManager;
-import static org.mule.test.allure.AllureConstants.ConfigurationComponentLocatorFeature.ConfigurationComponentLocationStory.CONFIGURATION_COMPONENT_LOCATION;
-import static org.mule.test.allure.AllureConstants.ConfigurationComponentLocatorFeature.ConfigurationComponentLocationStory.LazyConnectionsStory.COMPONENT_LOCATION;
+import static org.mule.test.allure.AllureConstants.ConfigurationComponentLocatorFeature.CONFIGURATION_COMPONENT_LOCATOR;
+import static org.mule.test.allure.AllureConstants.ConfigurationComponentLocatorFeature.ConfigurationComponentLocationStory.COMPONENT_LOCATION;
 import org.mule.runtime.api.component.TypedComponentIdentifier;
 import org.mule.runtime.api.component.location.ComponentLocation;
 import org.mule.runtime.api.component.location.Location;
@@ -94,7 +94,7 @@ import org.junit.Test;
 import org.xml.sax.EntityResolver;
 
 @DisplayName("XML Connectors Path generation")
-@Feature(CONFIGURATION_COMPONENT_LOCATION)
+@Feature(CONFIGURATION_COMPONENT_LOCATOR)
 @Story(COMPONENT_LOCATION)
 public class ModuleComponentPathTestCase extends AbstractIntegrationTestCase {
 
