2eafafe8879177b37907185c4a79e81e879ed061
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-10-03 08:38:07 -0300

    Revert "MULE-17185: Static flow-ref cycle detection test cases (#829)"
    This reverts commit 0ecfecca89b16a2fc5c32c14396b7379c65b1000.

--- integration/src/test/java/org/mule/test/construct/FlowRefTestCase.java
+++ integration/src/test/java/org/mule/test/construct/FlowRefTestCase.java
@@ -11,9 +11,7 @@ import static java.lang.String.format;
 import static java.util.Collections.synchronizedList;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static java.util.stream.Collectors.toList;
-import static org.apache.logging.log4j.core.util.Throwables.getRootCause;
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.endsWith;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.Matchers.hasSize;
@@ -21,7 +19,6 @@ import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
 import static org.mule.functional.api.exception.ExpectedError.none;
 import static org.mule.functional.junit4.matchers.ThrowableMessageMatcher.hasMessage;
 import static org.mule.runtime.api.notification.MessageProcessorNotification.MESSAGE_PROCESSOR_POST_INVOKE;
@@ -31,6 +28,7 @@ import static org.mule.runtime.core.api.exception.Errors.Identifiers.ROUTING_ERR
 import static org.mule.runtime.http.api.HttpConstants.HttpStatus.SERVICE_UNAVAILABLE;
 import static org.mule.runtime.http.api.HttpConstants.Method.GET;
 import static org.mule.tck.probe.PollingProber.probe;
+
 import org.mule.functional.api.component.EventCallback;
 import org.mule.functional.api.exception.ExpectedError;
 import org.mule.runtime.api.message.Message;
@@ -43,7 +41,6 @@ import org.mule.runtime.http.api.client.HttpRequestOptions;
 import org.mule.runtime.http.api.domain.message.request.HttpRequest;
 import org.mule.runtime.http.api.domain.message.response.HttpResponse;
 import org.mule.service.http.TestHttpClient;
-import org.mule.tck.junit4.matcher.ErrorTypeMatcher;
 import org.mule.tck.junit4.rule.DynamicPort;
 import org.mule.test.AbstractIntegrationTestCase;
 
@@ -54,12 +51,13 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import io.qameta.allure.Issue;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
+
+import io.qameta.allure.Issue;
 
 public class FlowRefTestCase extends AbstractIntegrationTestCase {
 
@@ -209,17 +207,7 @@ public class FlowRefTestCase extends AbstractIntegrationTestCase {
 
   @Test
   public void recursiveSubFlow() throws Exception {
-    testRecursiveFlowrefsAreDetectedFor("recursiveSubFlowCaller", "recursiveSubFlow");
-  }
-
-  @Test
-  public void crossedRecursiveSubFlow() {
-    testRecursiveFlowrefsAreDetectedFor("crossedRecursiveSubflow", "recurse1");
-  }
-
-  @Test
-  public void tripleCrossedRecursiveSubFlow() throws Exception {
-    testRecursiveFlowrefsAreDetectedFor("tripleCrossedRecursiveSubflow", "tripleRecurse1");
+    flowRunner("recursiveSubFlowCaller").runExpectingException(hasMessage(containsString(CONTEXT_DEPTH_MESSAGE)));
   }
 
   @Test
@@ -307,26 +295,6 @@ public class FlowRefTestCase extends AbstractIntegrationTestCase {
     probe(RECEIVE_TIMEOUT, 50, () -> awaiting.get() >= 1);
     assertThat(httpClient.send(request).getStatusCode(), is(SERVICE_UNAVAILABLE.getStatusCode()));
   }
-
-  @Test
-  public void flowWithStoppedTargetFlowFailsToProcess() throws Exception {
-    flowRunner("stoppedTargetFlow1").runExpectingException(ErrorTypeMatcher.errorType("MULE", "UNKNOWN"));
-  }
-
-  private void testRecursiveFlowrefsAreDetectedFor(String callingFlowName, String offendingFlowName) {
-    try {
-      // This will attempt to start the flow. That's the moment the subscription is triggered from downstream, and that's where
-      // the inter-flow-ref cycle is checked.
-      flowRunner(callingFlowName);
-      fail("Expected and error regarding a flowref cycle from " + callingFlowName + ", and with the offending flow being "
-          + offendingFlowName);
-    } catch (Exception e) {
-      Throwable rootCause = getRootCause(e);
-      assertThat(rootCause.getMessage(),
-                 endsWith(format("Found a possible infinite recursion involving flow named %s", offendingFlowName)));
-    }
-  }
-
 
   private static CountDownLatch latch;
   private static AtomicInteger awaiting = new AtomicInteger();
