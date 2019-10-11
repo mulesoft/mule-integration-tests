9b700cc74a6590a050f910783289c7c2b2922307
Rodrigo Merino <elrodro83@users.noreply.github.com>
2019-10-11 15:24:26 -0300

    MULE-17575: Fix ScatterGatherRouterTestCase flaky test (#852)
    (cherry picked from commit 52f81637795bd2c87ef6692642960b0a23688792)

--- integration/src/test/java/org/mule/test/routing/ScatterGatherRouterTestCase.java
+++ integration/src/test/java/org/mule/test/routing/ScatterGatherRouterTestCase.java
@@ -35,6 +35,7 @@ import org.mule.runtime.api.util.concurrent.Latch;
 import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.runtime.core.api.expression.ExpressionRuntimeException;
 import org.mule.runtime.core.api.processor.Processor;
+import org.mule.tck.junit4.rule.SystemProperty;
 import org.mule.test.AbstractIntegrationTestCase;
 
 import java.io.ByteArrayInputStream;
@@ -61,6 +62,9 @@ public class ScatterGatherRouterTestCase extends AbstractIntegrationTestCase {
   @Rule
   public ExpectedException expectedException = none();
 
+  @Rule
+  public SystemProperty timeout = new SystemProperty("scatterGather.timeout", "" + RECEIVE_TIMEOUT);
+
   @Override
   protected String getConfigFile() {
     return "scatter-gather-test.xml";
@@ -69,6 +73,11 @@ public class ScatterGatherRouterTestCase extends AbstractIntegrationTestCase {
   @Override
   protected void doSetUp() throws Exception {
     capturedThreads = newKeySet();
+  }
+
+  @Override
+  protected void doTearDown() throws Exception {
+    capturedThreads = null;
   }
 
   @Test
