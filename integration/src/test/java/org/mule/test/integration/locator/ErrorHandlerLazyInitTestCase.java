5bb46dc6c9641dcb74bb796f55ab56c32511078d
Guillermo Fernandes <gsfernandes@users.noreply.github.com>
2019-12-05 14:04:18 -0300

    MULE-16965: MuleRuntimeException when creating namespace for custom e… (#892)
    ...rror

--- integration/src/test/java/org/mule/test/integration/locator/ErrorHandlerLazyInitTestCase.java
+++ integration/src/test/java/org/mule/test/integration/locator/ErrorHandlerLazyInitTestCase.java
@@ -6,6 +6,7 @@
  */
 package org.mule.test.integration.locator;
 
+import static java.util.Arrays.stream;
 import static java.util.Optional.empty;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsNot.not;
@@ -15,8 +16,10 @@ import static org.mule.runtime.config.api.LazyComponentInitializer.LAZY_COMPONEN
 import static org.mule.runtime.config.api.SpringXmlConfigurationBuilderFactory.createConfigurationBuilder;
 import static org.mule.test.allure.AllureConstants.ConfigurationComponentLocatorFeature.CONFIGURATION_COMPONENT_LOCATOR;
 import static org.mule.test.allure.AllureConstants.ConfigurationComponentLocatorFeature.ConfigurationComponentLocatorStory.SEARCH_CONFIGURATION;
+
 import org.mule.runtime.api.artifact.Registry;
 import org.mule.runtime.api.component.ComponentIdentifier;
+import org.mule.runtime.api.component.location.Location;
 import org.mule.runtime.api.exception.ErrorTypeRepository;
 import org.mule.runtime.api.message.ErrorType;
 import org.mule.runtime.config.api.LazyComponentInitializer;
@@ -61,21 +64,61 @@ public class ErrorHandlerLazyInitTestCase extends AbstractIntegrationTestCase {
   }
 
   @Test
-  public void customErrorTypesShouldDiscovered() {
-    lazyComponentInitializer.initializeComponent(builder().globalName("mainFlow").build());
+  public void registerCustomErrorsFromErrorHandler() {
+    doCustomErrorTypesShouldDiscoveredTest(builder().globalName("mainFlow").build(), "APP:ERROR_TYPE_1", "APP:ERROR_TYPE_2");
+  }
+
+  @Test
+  public void registerCustomErrorsFromErrorMapping() {
+    doCustomErrorTypesShouldDiscoveredTest(builder().globalName("errorMappingFlow").build(), "APP:ERROR_TYPE_1",
+                                           "APP:ERROR_TYPE_2");
+  }
+
+  @Test
+  public void registerCustomErrorsFromErrorMappingOnlyOnce() {
+    doCustomErrorTypesShouldDiscoveredTest(builder().globalName("errorMappingFlow").build(), "APP:ERROR_TYPE_1",
+                                           "APP:ERROR_TYPE_2");
+    doCustomErrorTypesShouldDiscoveredTest(builder().globalName("errorMappingFlow2").build(), "APP:ERROR_TYPE_1",
+                                           "APP:ERROR_TYPE_2", "APP:ERROR_TYPE_5");
+  }
+
+  @Test
+  public void registerCustomErrorsFromRaiseError() {
+    doCustomErrorTypesShouldDiscoveredTest(builder().globalName("raiseErrorSubFlow").build(), "APP:ERROR_TYPE_1");
+  }
+
+  @Test
+  public void notEnabledErrorFlow() {
+    doCustomErrorTypesShouldDiscoveredTest(builder().globalName("raiseErrorSubFlow").build(), "APP:ERROR_TYPE_1",
+                                           "APP:ERROR_TYPE_3");
+  }
+
+  @Test
+  public void errorShouldNotBeRegisteredFromErrorHandlerNotReferenced() {
+    doCustomErrorTypesShouldDiscoveredTest(builder().globalName("notEnabledErrorHandler").build(), false, "APP:ERROR_TYPE_4");
+  }
+
+  private void doCustomErrorTypesShouldDiscoveredTest(Location location, boolean includeErrorTypes, String... errorTypes) {
+    lazyComponentInitializer.initializeComponent(location);
 
     ErrorTypeRepository errorTypeRepository = registry.lookupByType(ErrorTypeRepository.class)
         .orElseThrow(() -> new AssertionError("Cannot access errorTypeRepository"));
 
-    assertApplicationCustomErrorTypes(errorTypeRepository, "APP:ERROR_TYPE_1");
-    assertApplicationCustomErrorTypes(errorTypeRepository, "APP:ERROR_TYPE_2");
+    stream(errorTypes).forEach(errorType -> {
+      Optional<ErrorType> appMyErrorType =
+          errorTypeRepository.getErrorType(ComponentIdentifier.buildFromStringRepresentation(errorType));
+
+      if (includeErrorTypes) {
+        assertThat(appMyErrorType, not(empty()));
+        assertThat(appMyErrorType.get().getParentErrorType(), equalTo(errorTypeRepository.getAnyErrorType()));
+      } else {
+        assertThat(appMyErrorType, equalTo(empty()));
+      }
+    });
   }
 
-  public void assertApplicationCustomErrorTypes(ErrorTypeRepository errorTypeRepository, String errorType) {
-    Optional<ErrorType> appMyErrorType =
-        errorTypeRepository.getErrorType(ComponentIdentifier.buildFromStringRepresentation(errorType));
-    assertThat(appMyErrorType, not(empty()));
-    assertThat(appMyErrorType.get().getParentErrorType(), equalTo(errorTypeRepository.getAnyErrorType()));
+  private void doCustomErrorTypesShouldDiscoveredTest(Location location, String... errorTypes) {
+    doCustomErrorTypesShouldDiscoveredTest(location, true, errorTypes);
   }
 
 }
