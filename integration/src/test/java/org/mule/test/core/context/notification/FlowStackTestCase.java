8aa3ad6168af241c7baa5f83d026412e43e02d61
Rodrigo Merino <elrodro83@users.noreply.github.com>
2020-01-02 17:49:02 -0300

    MULE-17911: Add missing allure annotations (#911)

--- integration/src/test/java/org/mule/test/core/context/notification/FlowStackTestCase.java
+++ integration/src/test/java/org/mule/test/core/context/notification/FlowStackTestCase.java
@@ -13,6 +13,8 @@ import static org.junit.Assert.assertThat;
 import static org.mule.tck.util.FlowTraceUtils.assertStackElements;
 import static org.mule.tck.util.FlowTraceUtils.isFlowStackElement;
 import static org.mule.tck.util.FlowTraceUtils.FlowStackAsserter.stackToAssert;
+import static org.mule.test.allure.AllureConstants.Logging.LOGGING;
+import static org.mule.test.allure.AllureConstants.Logging.LoggingStory.FLOW_STACK;
 
 import org.mule.runtime.api.notification.MessageProcessorNotification;
 import org.mule.runtime.api.notification.MessageProcessorNotificationListener;
@@ -24,6 +26,11 @@ import java.util.concurrent.CountDownLatch;
 import org.junit.Before;
 import org.junit.Test;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+
+@Feature(LOGGING)
+@Story(FLOW_STACK)
 public class FlowStackTestCase extends AbstractIntegrationTestCase {
 
   @Override
--- integration/src/test/java/org/mule/test/integration/exceptions/LogCheckFlowStackTestCase.java
+++ integration/src/test/java/org/mule/test/integration/exceptions/LogCheckFlowStackTestCase.java
@@ -7,6 +7,8 @@
 package org.mule.test.integration.exceptions;
 
 import static org.mule.tck.junit4.rule.VerboseExceptions.setVerboseExceptions;
+import static org.mule.test.allure.AllureConstants.Logging.LOGGING;
+import static org.mule.test.allure.AllureConstants.Logging.LoggingStory.FLOW_STACK;
 
 import org.mule.tck.junit4.rule.VerboseExceptions;
 import org.mule.test.AbstractIntegrationTestCase;
@@ -16,6 +18,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+
+@Feature(LOGGING)
+@Story(FLOW_STACK)
 public class LogCheckFlowStackTestCase extends AbstractIntegrationTestCase {
 
   // Just to ensure the previous value is set after the test
