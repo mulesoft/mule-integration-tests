21ee511cae2573bd56f61b46904a4549cd0ff855
Rodrigo Merino <elrodro83@users.noreply.github.com>
2020-02-26 09:34:56 -0300

    MULE-16933: Increase coverage for processing strategy dispose in unit (#936)
    * Addendum for tx

--- integration/src/test/java/org/mule/shutdown/GracefulShutdownTxSourceTestCase.java
+++ integration/src/test/java/org/mule/shutdown/GracefulShutdownTxSourceTestCase.java
@@ -6,22 +6,20 @@
  */
 package org.mule.shutdown;
 
-import static java.lang.Runtime.getRuntime;
-import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
 import static org.mule.runtime.api.util.MuleSystemProperties.MULE_LIFECYCLE_FAIL_ON_FIRST_DISPOSE_ERROR;
 import static org.mule.test.allure.AllureConstants.LifecycleAndDependencyInjectionFeature.LIFECYCLE_AND_DEPENDENCY_INJECTION;
 import static org.mule.test.allure.AllureConstants.LifecycleAndDependencyInjectionFeature.GracefulShutdownStory.GRACEFUL_SHUTDOWN_STORY;
 
+import org.mule.functional.api.component.TestConnectorQueueHandler;
 import org.mule.tck.junit4.rule.SystemProperty;
 import org.mule.test.AbstractIntegrationTestCase;
-import org.mule.test.runner.RunnerDelegateTo;
 
-import java.util.List;
-
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
@@ -29,42 +27,27 @@ import io.qameta.allure.Story;
 
 @Feature(LIFECYCLE_AND_DEPENDENCY_INJECTION)
 @Story(GRACEFUL_SHUTDOWN_STORY)
-@RunnerDelegateTo(Parameterized.class)
-public class GracefulShutdownTestCase extends AbstractIntegrationTestCase {
-
-  @Parameters(name = "{0}")
-  public static List<String> parameters() {
-    return asList("org/mule/shutdown/flow-with-operation.xml",
-                  "org/mule/shutdown/flow-with-failing-operation.xml",
-                  "org/mule/shutdown/flow-with-flow-ref.xml",
-                  "org/mule/shutdown/flow-with-tx-flow-ref.xml",
-                  "org/mule/shutdown/flow-with-tx-scope.xml");
-  }
+public class GracefulShutdownTxSourceTestCase extends AbstractIntegrationTestCase {
 
   @Rule
   public SystemProperty propagateDisposeError = new SystemProperty(MULE_LIFECYCLE_FAIL_ON_FIRST_DISPOSE_ERROR, "");
 
-  private final String configFile;
+  private TestConnectorQueueHandler queueHandler;
 
-  public GracefulShutdownTestCase(String configFile) {
-    this.configFile = configFile;
+  @Before
+  public void before() {
+    queueHandler = new TestConnectorQueueHandler(registry);
   }
 
   @Override
   protected String getConfigFile() {
-    return configFile;
+    return "org/mule/shutdown/flow-with-tx-source.xml";
   }
 
   @Test
   @Description("Verify that the graceful shutdown occurs in a timely manner")
   public void flowStopTimelyManner() {
-    for (int i = 0; i < getRuntime().availableProcessors() * 4; ++i) {
-      try {
-        flowRunner("flow").run();
-      } catch (Exception e) {
-        // Nothing to do
-      }
-    }
+    assertThat(queueHandler.read("txFlowRan", RECEIVE_TIMEOUT), not(nullValue()));
   }
 
   @Override
