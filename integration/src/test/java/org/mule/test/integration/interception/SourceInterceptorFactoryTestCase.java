b1f89ecdbdbaba287f3b4d8b58e90c88780bd2f5
Diego Strubolini <diegostrubolini@users.noreply.github.com>
2020-03-03 14:48:32 -0300

    MULE-18123 Source not being intercepted when exception in smart connector (#942)

--- integration/src/test/java/org/mule/test/integration/interception/SourceInterceptorFactoryTestCase.java
+++ integration/src/test/java/org/mule/test/integration/interception/SourceInterceptorFactoryTestCase.java
@@ -7,6 +7,7 @@
 package org.mule.test.integration.interception;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
@@ -23,6 +24,7 @@ import org.mule.runtime.api.interception.InterceptionEvent;
 import org.mule.runtime.api.interception.ProcessorParameterValue;
 import org.mule.runtime.api.interception.SourceInterceptor;
 import org.mule.runtime.api.interception.SourceInterceptorFactory;
+import org.mule.runtime.api.message.ErrorType;
 import org.mule.runtime.core.api.construct.Flow;
 import org.mule.test.AbstractIntegrationTestCase;
 import org.mule.test.integration.interception.ProcessorInterceptorFactoryTestCase.InterceptionParameters;
@@ -127,6 +129,37 @@ public class SourceInterceptorFactoryTestCase extends AbstractIntegrationTestCas
     InterceptionParameters heisenbergSourceInterceptionParameter = interceptionParameters.get(interceptionParameters.size() - 1);
     assertThat(heisenbergSourceInterceptionParameter.toString(),
                heisenbergSourceInterceptionParameter.getParameters().entrySet(), hasSize(8));
+  }
+
+  @Test
+  public void sourceInterceptedWithFailingProcessor() throws Exception {
+    startFlow("sourceInterceptedWithFailingProcessor");
+
+    CountDownLatch afterCalledLatch = new CountDownLatch(1);
+
+    SourceCallbackInterceptor.afterCallback = (event, thrown) -> {
+      if (event.getError().isPresent()) {
+        ErrorType errorType = event.getError().get().getErrorType();
+        assertThat(errorType.getNamespace(), equalTo("MODULE-USING-CORE"));
+        assertThat(errorType.getIdentifier(), equalTo("RAISED"));
+        afterCalledLatch.countDown();
+      }
+    };
+
+    assertThat(afterCalledLatch.await(RECEIVE_TIMEOUT, MILLISECONDS), is(true));
+    List<InterceptionParameters> interceptionParameters = SourceCallbackInterceptor.interceptionParameters;
+
+    assertThat(interceptionParameters, hasSize(greaterThanOrEqualTo(1)));
+    InterceptionParameters heisenbergSourceInterceptionParameter = interceptionParameters.get(0);
+    assertThat(heisenbergSourceInterceptionParameter.getParameters().entrySet(), hasSize(8));
+    assertThat(heisenbergSourceInterceptionParameter.getParameters(), hasKey("fail"));
+    assertThat(heisenbergSourceInterceptionParameter.getParameters(), hasKey("config-ref"));
+    assertThat(heisenbergSourceInterceptionParameter.getParameters(), hasKey("initialBatchNumber"));
+    assertThat(heisenbergSourceInterceptionParameter.getParameters(), hasKey("payment"));
+    assertThat(heisenbergSourceInterceptionParameter.getParameters(), hasKey("frequency"));
+    assertThat(heisenbergSourceInterceptionParameter.getParameters(), hasKey("propagateError"));
+    assertThat(heisenbergSourceInterceptionParameter.getParameters(), hasKey("corePoolSize"));
+    assertThat(heisenbergSourceInterceptionParameter.getParameters(), hasKey("onCapacityOverload"));
   }
 
   public static class SourceCallbackInterceptorFactory implements SourceInterceptorFactory {
