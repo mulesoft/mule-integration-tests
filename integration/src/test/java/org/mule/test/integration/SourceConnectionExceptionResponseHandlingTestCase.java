bfc8349086c7a79d1d5ee4f6973d834bd7c61e7d
fabian.gonzalez <fabian.gonzalez@192.168.0.22>
2020-04-24 14:22:12 -0300

    MULE-18343: Source is not notified of failure on ConnectionException in flow

---
+++ integration/src/test/java/org/mule/test/integration/SourceConnectionExceptionResponseHandlingTestCase.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.integration;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mule.runtime.http.api.HttpConstants.HttpStatus.INTERNAL_SERVER_ERROR;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mule.runtime.http.api.HttpService;
+import org.mule.runtime.http.api.domain.message.request.HttpRequest;
+import org.mule.runtime.http.api.domain.message.response.HttpResponse;
+import org.mule.service.http.TestHttpClient;
+import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.test.AbstractIntegrationTestCase;
+
+public class SourceConnectionExceptionResponseHandlingTestCase extends AbstractIntegrationTestCase {
+
+  @Rule
+  public DynamicPort httpPort = new DynamicPort("httpPort");
+
+  @Rule
+  public TestHttpClient httpClient = new TestHttpClient.Builder(getService(HttpService.class)).build();
+
+  @Override
+  protected String getConfigFile() {
+    return "org/mule/test/integration/source-connection-exception-response-handling-test-case.xml";
+  }
+
+  @Test
+  public void responseParameterWithCursor() throws IOException, TimeoutException {
+    HttpResponse httpResponse =
+        httpClient.send(HttpRequest.builder().uri(String.format("http://localhost:%s/", httpPort.getNumber())).build());
+    assertThat(httpResponse.getStatusCode(), is(INTERNAL_SERVER_ERROR.getStatusCode()));
+  }
+
+}
