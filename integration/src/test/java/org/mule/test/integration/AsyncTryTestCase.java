f9c59689746b3fbb5fb759c0f3a3acebbc631918
Ezequiel Werner <eze210@gmail.com>
2020-04-27 15:47:36 -0300

    Adding test

---
+++ integration/src/test/java/org/mule/test/integration/AsyncTryTestCase.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.integration;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mule.functional.api.component.InvocationCountMessageProcessor.getNumberOfInvocationsFor;
+import org.mule.runtime.api.util.concurrent.Latch;
+import org.mule.test.AbstractIntegrationTestCase;
+import org.mule.test.runner.RunnerDelegateTo;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import io.qameta.allure.Issue;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+
+@Issue("MULE-18353")
+@RunnerDelegateTo(Parameterized.class)
+public class AsyncTryTestCase extends AbstractIntegrationTestCase {
+
+  private String flowName;
+  private Latch latch;
+
+  @Parameterized.Parameters(name = "{0}")
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][] {
+        {"Synchronous", "trigger-sync"},
+        {"Asynchronous direct", "trigger-async-direct"},
+        {"Asynchronous Flow", "trigger-async-flow"},
+        {"Asynchronous SubFlow", "trigger-async-subflow"},
+        {"Asynchronous SubFlow", "trigger-async-subflow"},
+        {"Asynchronous SubFlow", "trigger-async-subflow"},
+        {"Asynchronous SubFlow", "trigger-async-subflow"},
+        {"Simplified SubFlow", "trigger-simplified"},
+    });
+  }
+
+  public AsyncTryTestCase(String type, String flowName) {
+    this.flowName = flowName;
+  }
+
+  @Override
+  protected String getConfigFile() {
+    return "async-try-config.xml";
+  }
+
+  @Before
+  public void setupLatch() {
+    latch = new Latch();
+  }
+
+  @Test
+  public void executesTheProcessorAfterTry() throws Exception {
+    flowRunner(flowName).withVariable("latch", latch).run();
+    latch.await(20, SECONDS);
+    assertThat(getNumberOfInvocationsFor("invocation-counter"), is(1));
+  }
+}
