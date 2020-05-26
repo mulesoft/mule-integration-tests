60acc30cb045ff192fcae86e35de4c9eda897c57
Martín Buchwald <martin.buchwald@mulesoft.com>
2020-05-26 12:55:06 -0300

    allurex2

--- integration/src/test/java/org/mule/test/integration/exceptions/GlobalErrorHandlerTestCase.java
+++ integration/src/test/java/org/mule/test/integration/exceptions/GlobalErrorHandlerTestCase.java
@@ -12,6 +12,7 @@ import static org.mule.test.allure.AllureConstants.ErrorHandlingFeature.ERROR_HA
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
 import io.qameta.allure.Story;
 import org.junit.Test;
 import org.mule.runtime.core.api.event.CoreEvent;
@@ -27,6 +28,7 @@ public class GlobalErrorHandlerTestCase extends AbstractIntegrationTestCase {
   }
 
   @Test
+  @Issue("MULE-18439")
   @Description("Flow with error handler reference and name matching")
   public void errorHandlerWithSelfReference() throws Exception {
     // This should fail, but in 4.2 it didn't so we must accept it in 4.3, since it does work
@@ -35,6 +37,7 @@ public class GlobalErrorHandlerTestCase extends AbstractIntegrationTestCase {
   }
 
   @Test
+  @Issue("MULE-18439")
   @Description("Flow with error handler with on-error-continue reference and name matching")
   public void errorHandlerWithSelfReferenceToContinue() throws Exception {
     // This should fail, but in 4.2 it didn't so we must accept it in 4.3, since it does work
@@ -43,6 +46,7 @@ public class GlobalErrorHandlerTestCase extends AbstractIntegrationTestCase {
   }
 
   @Test
+  @Issue("MULE-18439")
   @Description("Flow with error handler with on-error-continue reference and name matching, and matching on-error-continue in global error handler")
   public void errorHandlerWithSelfReferenceToContinueReferencedInGlobalEH() throws Exception {
     // This should fail, but in 4.2 it didn't so we must accept it in 4.3, since it does work
