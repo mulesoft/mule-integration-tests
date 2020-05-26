e166e04a608a8da4c22d3e52e5d9306764f4c747
Martín Buchwald <martin.buchwald@mulesoft.com>
2020-05-26 10:16:49 -0300

    format

--- integration/src/test/java/org/mule/test/integration/exceptions/GlobalErrorHandlerTestCase.java
+++ integration/src/test/java/org/mule/test/integration/exceptions/GlobalErrorHandlerTestCase.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.test.integration.exceptions;
 
 import static org.hamcrest.core.Is.is;
@@ -9,15 +15,15 @@ import org.mule.test.AbstractIntegrationTestCase;
 
 public class GlobalErrorHandlerTestCase extends AbstractIntegrationTestCase {
 
-    @Override
-    protected String getConfigFile() {
-        return "org/mule/test/integration/exceptions/global-error-handler.xml";
-    }
+  @Override
+  protected String getConfigFile() {
+    return "org/mule/test/integration/exceptions/global-error-handler.xml";
+  }
 
-    @Test
-    public void errorHandlerWithSelfReference() throws Exception {
-        // This should fail, but in 4.2 it didn't so we must accept it in 4.3, since it does work
-        CoreEvent event = flowRunner("flowWithErrorHandlerSelfReferencing").run();
-        assertThat(event.getMessage().getPayload().getValue(), is("Chocotorta"));
-    }
+  @Test
+  public void errorHandlerWithSelfReference() throws Exception {
+    // This should fail, but in 4.2 it didn't so we must accept it in 4.3, since it does work
+    CoreEvent event = flowRunner("flowWithErrorHandlerSelfReferencing").run();
+    assertThat(event.getMessage().getPayload().getValue(), is("Chocotorta"));
+  }
 }
