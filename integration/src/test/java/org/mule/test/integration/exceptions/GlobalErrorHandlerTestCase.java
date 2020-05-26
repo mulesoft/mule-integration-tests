8e3f257c83781039997488e484264cbb19a48e0e
Martín Buchwald <martin.buchwald@mulesoft.com>
2020-05-26 12:46:11 -0300

    allure

--- integration/src/test/java/org/mule/test/integration/exceptions/GlobalErrorHandlerTestCase.java
+++ integration/src/test/java/org/mule/test/integration/exceptions/GlobalErrorHandlerTestCase.java
@@ -8,11 +8,17 @@ package org.mule.test.integration.exceptions;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mule.test.allure.AllureConstants.ErrorHandlingFeature.ERROR_HANDLING;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.junit.Test;
 import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.test.AbstractIntegrationTestCase;
 
+@Feature(ERROR_HANDLING)
+@Story("Global Error handlers and 'circular references'")
 public class GlobalErrorHandlerTestCase extends AbstractIntegrationTestCase {
 
   @Override
@@ -21,6 +27,7 @@ public class GlobalErrorHandlerTestCase extends AbstractIntegrationTestCase {
   }
 
   @Test
+  @Description("Flow with error handler reference and name matching")
   public void errorHandlerWithSelfReference() throws Exception {
     // This should fail, but in 4.2 it didn't so we must accept it in 4.3, since it does work
     CoreEvent event = flowRunner("flowWithErrorHandlerSelfReferencing").run();
@@ -28,6 +35,7 @@ public class GlobalErrorHandlerTestCase extends AbstractIntegrationTestCase {
   }
 
   @Test
+  @Description("Flow with error handler with on-error-continue reference and name matching")
   public void errorHandlerWithSelfReferenceToContinue() throws Exception {
     // This should fail, but in 4.2 it didn't so we must accept it in 4.3, since it does work
     CoreEvent event = flowRunner("flowWithErrorHandlerSelfReferencingContinue").run();
@@ -35,6 +43,7 @@ public class GlobalErrorHandlerTestCase extends AbstractIntegrationTestCase {
   }
 
   @Test
+  @Description("Flow with error handler with on-error-continue reference and name matching, and matching on-error-continue in global error handler")
   public void errorHandlerWithSelfReferenceToContinueReferencedInGlobalEH() throws Exception {
     // This should fail, but in 4.2 it didn't so we must accept it in 4.3, since it does work
     CoreEvent event = flowRunner("flowWithErrorHandlerSelfReferencingToReferencedContinue").run();
