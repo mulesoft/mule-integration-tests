2bef7fe5163b910fb4c183d7ab3a795208268d18
Nicolas Santiago Di Nucci <nico.dinu@gmail.com>
2020-09-21 11:22:53 -0300

    Adds requested changes

--- oauth/src/test/java/org/mule/test/module/extension/oauth/authcode/OAuthConnectivityValidationRefreshExtensionTestCase.java
+++ oauth/src/test/java/org/mule/test/module/extension/oauth/authcode/OAuthConnectivityValidationRefreshExtensionTestCase.java
@@ -6,6 +6,8 @@
  */
 package org.mule.test.module.extension.oauth.authcode;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mule.runtime.api.connectivity.ConnectivityTestingService.CONNECTIVITY_TESTING_SERVICE_KEY;
@@ -50,6 +52,7 @@ public class OAuthConnectivityValidationRefreshExtensionTestCase extends BaseOAu
     ConnectionValidationResult connectionValidationResult = connectivityTestingService
         .testConnection(Location.builder().globalName("oauth").build());
     assertThat(connectionValidationResult.isValid(), is(true));
+    wireMock.verify(postRequestedFor(urlPathEqualTo("/" + TOKEN_PATH)));
   }
 
 }
