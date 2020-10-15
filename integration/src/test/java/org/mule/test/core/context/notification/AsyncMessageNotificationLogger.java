417b934f1d980c3ec9ed94238078f95fdfd2c840
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2020-10-15 06:48:01 -0300

    MULE-18883: PipelineMessageNotification PROCESS_COMPLETE lost when propagate errors (#1190)
    (cherry picked from commit 37d4292376e736e68aa152538ec67f0eb8f0928f)

--- integration/src/test/java/org/mule/test/core/context/notification/AsyncMessageNotificationLogger.java
+++ integration/src/test/java/org/mule/test/core/context/notification/AsyncMessageNotificationLogger.java
@@ -7,26 +7,15 @@
 package org.mule.test.core.context.notification;
 
 import org.mule.runtime.api.notification.AsyncMessageNotificationListener;
-import org.mule.tck.core.context.notification.NotificationLogger;
+import org.mule.tck.core.context.notification.AbstractNotificationLogger;
 import org.mule.runtime.api.notification.AsyncMessageNotification;
 
-import java.util.List;
-
-public class AsyncMessageNotificationLogger extends PipelineAndAsyncMessageNotificationLogger
-    implements AsyncMessageNotificationListener<AsyncMessageNotification>, NotificationLogger {
+public class AsyncMessageNotificationLogger extends AbstractNotificationLogger<AsyncMessageNotification>
+    implements AsyncMessageNotificationListener<AsyncMessageNotification> {
 
   @Override
   public boolean isBlocking() {
     return false;
   }
 
-  @Override
-  public synchronized void onNotification(AsyncMessageNotification notification) {
-    notifications.addLast(notification);
-  }
-
-  @Override
-  public List getNotifications() {
-    return notifications;
-  }
 }
