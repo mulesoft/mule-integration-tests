787678a36c256120404c9d1a7c35dca02b7dbea0
Rodrigo Merino <elrodro83@users.noreply.github.com>
2020-11-24 09:23:54 -0300

    MULE-19008: Fix new parsing mechanism when dealing with inconsistent frequency parameter (#1267)

--- schedulers-tests/src/test/java/org/mule/test/integration/SchedulerWIthCustomDefaultFrequencyTestCase.java
+++ schedulers-tests/src/test/java/org/mule/test/integration/SchedulerWIthCustomDefaultFrequencyTestCase.java
@@ -10,6 +10,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mule.runtime.api.component.ComponentIdentifier.buildFromStringRepresentation;
 import static org.mule.runtime.api.util.MuleSystemProperties.DEFAULT_SCHEDULER_FIXED_FREQUENCY;
+import static org.mule.test.allure.AllureConstants.SchedulerFeature.SCHEDULER;
 
 import org.mule.runtime.api.source.SchedulerMessageSource;
 import org.mule.runtime.core.api.source.scheduler.FixedFrequencyScheduler;
@@ -19,7 +20,11 @@ import org.mule.test.AbstractSchedulerTestCase;
 import org.junit.Rule;
 import org.junit.Test;
 
-public class SchedulerWIthCustomDefaultFrequency extends AbstractSchedulerTestCase {
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+
+@Feature(SCHEDULER)
+public class SchedulerWIthCustomDefaultFrequencyTestCase extends AbstractSchedulerTestCase {
 
   private static final long CUSTOM_FREQ = 42;
 
@@ -33,6 +38,7 @@ public class SchedulerWIthCustomDefaultFrequency extends AbstractSchedulerTestCa
   }
 
   @Test
+  @Issue("MULE-18262")
   public void customDefaultFreq() {
     SchedulerMessageSource source = (SchedulerMessageSource) locator.find(buildFromStringRepresentation("scheduler")).get(0);
     FixedFrequencyScheduler conf = (FixedFrequencyScheduler) source.getConfiguration();
