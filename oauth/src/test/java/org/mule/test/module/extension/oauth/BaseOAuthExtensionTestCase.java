0fb59309b3bd13c7e2008da5ba4e8ed1656f0332
Juan Grethe <jgrethe@itba.edu.ar>
2020-12-03 09:55:42 -0300

    MULE-18962: fix flaky test (#1293)
    * test with flaky detector
    
    * test fix
    
    * test again
    
    * log response
    
    * remove flakiness detector

--- oauth/src/test/java/org/mule/test/module/extension/oauth/BaseOAuthExtensionTestCase.java
+++ oauth/src/test/java/org/mule/test/module/extension/oauth/BaseOAuthExtensionTestCase.java
@@ -139,7 +139,7 @@ public abstract class BaseOAuthExtensionTestCase extends AbstractExtensionFuncti
   }
 
   protected void simulateDanceStart(int port) throws IOException {
-    wireMock.stubFor(get(urlMatching("/" + LOCAL_AUTH_PATH)).willReturn(aResponse().withStatus(OK.getStatusCode())));
+    wireMock.stubFor(get(urlMatching("/" + LOCAL_AUTH_PATH + ".*")).willReturn(aResponse().withStatus(OK.getStatusCode())));
     ImmutableMap.Builder<String, String> queryParamsBuilder = ImmutableMap.builder();
     if (ownerId != null) {
       queryParamsBuilder.put("resourceOwnerId", ownerId);
@@ -149,8 +149,9 @@ public abstract class BaseOAuthExtensionTestCase extends AbstractExtensionFuncti
         .build();
 
     String localAuthUrl = toUrl(LOCAL_AUTH_PATH, port);
-    Get(localAuthUrl + "?" + encodeQueryString(queryParams))
+    Get(localAuthUrl + "?" + encodeQueryString(queryParams)).addHeader("Connection", "close")
         .connectTimeout(REQUEST_TIMEOUT).socketTimeout(REQUEST_TIMEOUT).execute();
+
   }
 
   protected void simulateCallback() {
@@ -172,7 +173,7 @@ public abstract class BaseOAuthExtensionTestCase extends AbstractExtensionFuncti
     stubTokenUrl(accessTokenContent());
 
     check(REQUEST_TIMEOUT, 500, () -> {
-      Response response = Get(toUrl(CALLBACK_PATH, port) + "?" + encodeQueryString(queryParams))
+      Response response = Get(toUrl(CALLBACK_PATH, port) + "?" + encodeQueryString(queryParams)).addHeader("Connection", "close")
           .connectTimeout(REQUEST_TIMEOUT).socketTimeout(REQUEST_TIMEOUT).execute();
 
       assertThat(response.returnResponse().getStatusLine().getStatusCode(), is(OK.getStatusCode()));
