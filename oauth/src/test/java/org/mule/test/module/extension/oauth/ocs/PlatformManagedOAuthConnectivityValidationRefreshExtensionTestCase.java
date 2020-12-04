8ba0e78913ccc622402ec1b2fcd568b7c74531a8
Nicolas Santiago Di Nucci <nico.dinu@gmail.com>
2020-12-04 14:04:52 -0300

    EE-7568: While doing test connectivity for OCS connections if the token has been expired a refresh token is not triggered

--- oauth/src/test/java/org/mule/test/module/extension/oauth/ocs/PlatformManagedOAuthConnectivityValidationRefreshExtensionTestCase.java
+++ oauth/src/test/java/org/mule/test/module/extension/oauth/ocs/PlatformManagedOAuthConnectivityValidationRefreshExtensionTestCase.java
@@ -4,82 +4,81 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.test.module.extension.oauth.authcode;
+package org.mule.test.module.extension.oauth.ocs;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-import static org.hamcrest.CoreMatchers.is;
+import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mule.test.oauth.TestOAuthRefreshValidationProvider.TIMES_REFRESH_IS_NEEDED;
 import static org.mule.runtime.api.connectivity.ConnectivityTestingService.CONNECTIVITY_TESTING_SERVICE_KEY;
 
 import org.mule.runtime.api.component.location.Location;
 import org.mule.runtime.api.connection.ConnectionValidationResult;
 import org.mule.runtime.api.connectivity.ConnectivityTestingService;
-import org.mule.test.module.extension.oauth.BaseOAuthExtensionTestCase;
+
+import java.util.Collection;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-
-import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runners.Parameterized;
 
-public class OAuthConnectivityValidationRefreshExtensionTestCase extends BaseOAuthExtensionTestCase {
+public class PlatformManagedOAuthConnectivityValidationRefreshExtensionTestCase
+    extends PlatformManagedOAuthConfigurationTestCase {
 
-  @Override
-  protected String[] getConfigFiles() {
-    return new String[] {"auth-code-oauth-validated-connection-extension-config.xml", "oauth-extension-flows.xml"};
-  }
+  private static final String OAUTH_PLATFORM_CONFIG = "oauth-platform";
 
   @Inject
   @Named(CONNECTIVITY_TESTING_SERVICE_KEY)
   protected ConnectivityTestingService connectivityTestingService;
 
+  @Parameterized.Parameters(name = "{0}")
+  public static Collection<Object[]> data() {
+    return asList(new Object[][] {
+        {"With refresh validation configuration", "ocs/with-refresh-validation-platform-managed-config.xml", true}
+    });
+  }
+
   @Before
-  public void setOwnerId() throws Exception {
-    ownerId = getCustomOwnerId();
-    storedOwnerId = getCustomOwnerId() + "-oauth";
+  public void resetCounter() throws Exception {
+    TIMES_REFRESH_IS_NEEDED = 0;
   }
 
   @Test
   public void refreshTokenOnConnectionValidation() throws Exception {
-    simulateCallback();
-
-    WireMock.reset();
-    stubRefreshToken();
+    TIMES_REFRESH_IS_NEEDED = 1;
 
     ConnectionValidationResult connectionValidationResult = testConnection();
     assertThat(connectionValidationResult.isValid(), is(true));
-    wireMock.verify(postRequestedFor(urlPathEqualTo("/" + TOKEN_PATH)));
+    verify(mockPlatformDancer, times(1)).refreshToken();
   }
 
   @Test
   public void refreshedTokenAlreadyExpiredOnConnectionValidation() throws Exception {
-    simulateCallback();
-
-    stubRefreshedTokenAlreadyExpiredOnce();
+    TIMES_REFRESH_IS_NEEDED = 2;
 
     ConnectionValidationResult connectionValidationResult = testConnection();
-    verifyTokenRefreshedTwice();
     assertThat(connectionValidationResult.isValid(), is(true));
+    verify(mockPlatformDancer, times(2)).refreshToken();
   }
 
   @Test
   public void refreshedTokenAlreadyExpiredTwiceOnConnectionValidation() throws Exception {
-    simulateCallback();
-
-    stubRefreshedTokenAlreadyExpiredTwice();
+    TIMES_REFRESH_IS_NEEDED = 3;
 
     ConnectionValidationResult connectionValidationResult = testConnection();
-    verifyTokenRefreshedTwice();
     assertThat(connectionValidationResult.isValid(), is(false));
+    verify(mockPlatformDancer, times(2)).refreshToken();
 
     expectExpiredTokenException();
     throw connectionValidationResult.getException();
   }
 
   private ConnectionValidationResult testConnection() {
-    return connectivityTestingService.testConnection(Location.builder().globalName("oauth").build());
+    return connectivityTestingService.testConnection(Location.builder().globalName(OAUTH_PLATFORM_CONFIG).build());
   }
-}
 
+}
