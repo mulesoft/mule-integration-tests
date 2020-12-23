b4f12b55b8b2ee5596335df4955313e027f09327
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2020-12-23 15:49:41 -0300

    MULE-19038: add integration test

---
+++ integration/src/test/java/org/mule/test/integration/properties/ReservedPropertyNamesTestCase.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.integration.properties;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mule.test.allure.AllureConstants.ConfigurationProperties.CONFIGURATION_PROPERTIES;
+import static org.mule.test.allure.AllureConstants.ConfigurationProperties.ComponentConfigurationAttributesStory.CONFIGURATION_PROPERTIES_RESOLVER_STORY;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.inject.Inject;
+
+import com.google.common.collect.ImmutableMap;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.mule.runtime.api.component.ConfigurationProperties;
+import org.mule.runtime.api.config.custom.ServiceConfigurator;
+import org.mule.runtime.api.meta.MuleVersion;
+import org.mule.runtime.core.api.MuleContext;
+import org.mule.runtime.core.api.config.ConfigurationBuilder;
+import org.mule.runtime.core.api.config.DefaultMuleConfiguration;
+import org.mule.test.AbstractIntegrationTestCase;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.mule.test.runner.RunnerDelegateTo;
+
+@Feature(CONFIGURATION_PROPERTIES)
+@Story(CONFIGURATION_PROPERTIES_RESOLVER_STORY)
+@RunnerDelegateTo(Parameterized.class)
+public class ReservedPropertyNamesTestCase extends AbstractIntegrationTestCase {
+
+  public static final String APP_NAME_PROPERTY = "app.name";
+  public static final String APP_NAME = "my-app";
+  public static final String TEST_PROPERTY_NAME = "key";
+
+  @Inject
+  private ConfigurationProperties configurationProperties;
+
+  private final String minMuleVersion;
+
+  private final String expectedPropertyValue;
+
+  @Parameters(name = "With minMuleVersion {0}")
+  public static Object[][] parameters() {
+    return new Object[][] {
+        new Object[] {"4.1.5", "overridden.value"},
+        new Object[] {"4.2.2", "overridden.value"},
+        new Object[] {"4.3.0", "value"}
+    };
+  }
+
+  public ReservedPropertyNamesTestCase(String minMuleVersion, String expectedPropertyValue) {
+    this.minMuleVersion = minMuleVersion;
+    this.expectedPropertyValue = expectedPropertyValue;
+  }
+
+  @Test
+  public void propertiesAreResolvedCorrectly() {
+    Optional<String> key = configurationProperties.resolveStringProperty(TEST_PROPERTY_NAME);
+
+    assertThat(key.isPresent(), is(true));
+    assertThat(key.get(), is(expectedPropertyValue));
+  }
+
+  @Override
+  protected String getConfigFile() {
+    return "properties/reserved-property-names-config.xml";
+  }
+
+  @Override
+  protected void addBuilders(List<ConfigurationBuilder> builders) {
+    builders.add(new ConfigurationBuilder() {
+
+      @Override
+      public void addServiceConfigurator(ServiceConfigurator serviceConfigurator) {
+
+      }
+
+      @Override
+      public void configure(MuleContext muleContext) {
+        if (minMuleVersion != null) {
+          ((DefaultMuleConfiguration) muleContext.getConfiguration()).setMinMuleVersion(new MuleVersion(minMuleVersion));
+        }
+      }
+    });
+    super.addBuilders(builders);
+  }
+
+  @Override
+  protected Map<String, String> artifactProperties() {
+    return ImmutableMap.<String, String>builder()
+        .put(APP_NAME_PROPERTY, APP_NAME)
+        .build();
+  }
+
+}
