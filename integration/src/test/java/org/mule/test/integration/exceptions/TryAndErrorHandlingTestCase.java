186cd76d80b3d8740d842bb55232785f1e6ba285
Ivan Fritzler <58004191+IvanAndresFritzler@users.noreply.github.com>
2021-01-11 20:08:39 -0300

    MULE-19118: Error is dropped during recursive error handling

--- integration/src/test/java/org/mule/test/integration/exceptions/TryAndErrorHandlingTestCase.java
+++ integration/src/test/java/org/mule/test/integration/exceptions/TryAndErrorHandlingTestCase.java
@@ -6,8 +6,10 @@
  */
 package org.mule.test.integration.exceptions;
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNull.notNullValue;
 import static org.mule.functional.api.component.InvocationCountMessageProcessor.getNumberOfInvocationsFor;
 import static org.mule.test.allure.AllureConstants.ErrorHandlingFeature.ERROR_HANDLING;
 import static org.mule.test.allure.AllureConstants.ErrorHandlingFeature.ErrorHandlingStory.ERROR_HANDLER;
@@ -16,12 +18,19 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.junit.Test;
+import org.mule.runtime.api.message.Message;
 import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.test.AbstractIntegrationTestCase;
+import org.mule.tests.api.TestQueueManager;
+
+import javax.inject.Inject;
 
 @Feature(ERROR_HANDLING)
 @Story(ERROR_HANDLER)
 public class TryAndErrorHandlingTestCase extends AbstractIntegrationTestCase {
+
+  @Inject
+  private TestQueueManager queueManager;
 
   @Override
   protected String getConfigFile() {
@@ -34,6 +43,20 @@ public class TryAndErrorHandlingTestCase extends AbstractIntegrationTestCase {
     CoreEvent event = flowRunner("tryInErrorHandler").run();
     assertThat(event.getMessage().getPayload().getValue(), is("hello"));
     assertThat(getNumberOfInvocationsFor("try-in-eh"), is(0));
+  }
+
+  @Test
+  public void tryWithRecursiveOnErrorContinueInsideSubflow() throws Exception {
+    flowRunner("tryWithRecursiveOnErrorContinueInsideSubflow").run();
+    Message response = queueManager.read("dlq", RECEIVE_TIMEOUT, MILLISECONDS).getMessage();
+    assertThat(response, notNullValue());
+  }
+
+  @Test
+  public void tryWithRecursiveOnErrorContinueInsideFlow() throws Exception {
+    flowRunner("tryWithRecursiveOnErrorContinueInsideFlow").run();
+    Message response = queueManager.read("dlq", RECEIVE_TIMEOUT, MILLISECONDS).getMessage();
+    assertThat(response, notNullValue());
   }
 
 }
