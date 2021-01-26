c4cddb46e624cdbbf94987a1db7f2de46facdb42
Ivan Fritzler <58004191+IvanAndresFritzler@users.noreply.github.com>
2021-01-26 17:24:18 -0300

    MULE-19139: Mapped Error Type gets replaced by MULE:UNKNOWN
    (cherry picked from commit 1b17b77c300b1ab7301c8989fa602fc00d82f857)

---
+++ integration/src/test/java/org/mule/test/integration/exceptions/ValidationModuleErrorHandlingTestCase.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.integration.exceptions;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Story;
+import org.junit.Test;
+import org.mule.test.AbstractIntegrationTestCase;
+import static org.mule.tck.junit4.matcher.ErrorTypeMatcher.errorType;
+import static org.mule.test.allure.AllureConstants.ErrorHandlingFeature.ERROR_HANDLING;
+import static org.mule.test.allure.AllureConstants.ErrorHandlingFeature.ErrorHandlingStory.ERROR_HANDLER;
+import static org.mule.test.allure.AllureConstants.ErrorHandlingFeature.ErrorHandlingStory.ERROR_MAPPINGS;
+
+@Feature(ERROR_HANDLING)
+@Story(ERROR_HANDLER)
+@Story(ERROR_MAPPINGS)
+public class ValidationModuleErrorHandlingTestCase extends AbstractIntegrationTestCase {
+
+  @Override
+  protected String getConfigFile() {
+    return "org/mule/test/integration/exceptions/validation-module-error-handling.xml";
+  }
+
+  @Test
+  @Issue("MULE-19139")
+  public void validationAllWithErrorMapping() throws Exception {
+    flowRunner("validationAllWithErrorMapping").runExpectingException(errorType("TEST", "NULL"));
+  }
+
+  @Test
+  @Issue("MULE-19139")
+  public void validationWithErrorMapping() throws Exception {
+    flowRunner("validationWithErrorMapping").runExpectingException(errorType("TEST", "NULL"));
+  }
+
+}
