8958ba5ed164a65ab27de16598ca35ae4d5b8ffb
Rodrigo Merino <elrodro83@users.noreply.github.com>
2021-04-30 18:34:02 -0300

    MULE-19380: Tooling: Do a proper lazy init in AbstractArtifactAgnosticService (#1430)

--- tooling-support-tests/src/test/java/org/mule/runtime/module/tooling/DslConfigurationElementDeclarationTestingTestCase.java
+++ tooling-support-tests/src/test/java/org/mule/runtime/module/tooling/DslConfigurationElementDeclarationTestingTestCase.java
@@ -11,8 +11,8 @@ import static org.junit.rules.ExpectedException.none;
 import static org.mule.runtime.module.tooling.TestExtensionDeclarationUtils.configurationDeclarationMissingRequiredParameter;
 import static org.mule.runtime.module.tooling.TestExtensionDeclarationUtils.connectionDeclaration;
 
-import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.app.declaration.api.fluent.ArtifactDeclarer;
+import org.mule.runtime.core.api.config.ConfigurationException;
 import org.mule.runtime.deployment.model.api.DeploymentInitException;
 
 import org.junit.Rule;
@@ -35,8 +35,8 @@ public class DslConfigurationElementDeclarationTestingTestCase extends Declarati
 
   @Test
   public void testConnectionMissingRequiredParameterOnConfiguration() {
-    expectedException.expect(MuleRuntimeException.class);
-    expectedException.expectCause(instanceOf(DeploymentInitException.class));
+    expectedException.expect(DeploymentInitException.class);
+    expectedException.expectCause(instanceOf(ConfigurationException.class));
     expectedException.expectMessage("[unknown:-1]: Element <tst:config> is missing required parameter 'notActingParameter'.");
     session.testConnection(CONFIG_MISSING_PARAMETERS_NAME);
   }
