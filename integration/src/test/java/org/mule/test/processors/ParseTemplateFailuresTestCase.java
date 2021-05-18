baba6252a093c931fd6e2a5643381c106f522bdf
Rodro <rodrigo.merino@mulesoft.com>
2021-05-18 16:09:32 -0300

    MULE-19429: Properly validate required expressions in parameters

---
+++ integration/src/test/java/org/mule/test/processors/ParseTemplateFailuresTestCase.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.processors;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mule.test.allure.AllureConstants.ComponentsFeature.CORE_COMPONENTS;
+import static org.mule.test.allure.AllureConstants.ComponentsFeature.ParseTemplateStory.PARSE_TEMPLATE;
+import static org.mule.test.allure.AllureConstants.MuleDsl.DslValidationStory.DSL_VALIDATION_STORY;
+
+import org.mule.functional.junit4.AbstractConfigurationFailuresTestCase;
+import org.mule.runtime.core.api.config.ConfigurationException;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Stories;
+import io.qameta.allure.Story;
+
+@Feature(CORE_COMPONENTS)
+@Stories({@Story(PARSE_TEMPLATE), @Story(DSL_VALIDATION_STORY)})
+public class ParseTemplateFailuresTestCase extends AbstractConfigurationFailuresTestCase {
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  @Test
+  public void withWrongTargetValue() throws Exception {
+    expectedException.expect(ConfigurationException.class);
+    expectedException
+        .expectMessage(containsString("A static value ('not_an_expression') was given for parameter 'targetValue' but it requires an expression"));
+    loadConfiguration("org/mule/processors/parse-template-wrong-target-value-config.xml");
+  }
+
+}
