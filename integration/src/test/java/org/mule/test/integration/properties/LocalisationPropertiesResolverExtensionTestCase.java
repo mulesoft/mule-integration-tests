8b7e7a2e20c9f01f1834bb73c3b84e31100fc3a0
sofiamorseletto <sofimorseletto@gmail.com>
2021-05-20 17:40:29 -0300

    Fixed PR comments

--- integration/src/test/java/org/mule/test/integration/properties/LocalisationPropertiesResolverExtensionTestCase.java
+++ integration/src/test/java/org/mule/test/integration/properties/LocalisationPropertiesResolverExtensionTestCase.java
@@ -9,18 +9,27 @@ package org.mule.test.integration.properties;
 import static java.util.Arrays.asList;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.mule.test.allure.AllureConstants.ConfigurationProperties.CONFIGURATION_PROPERTIES;
+import static org.mule.test.allure.AllureConstants.ConfigurationProperties.ComponentConfigurationAttributesStory.CONFIGURATION_PROPERTIES_RESOLVER_STORY;
 
 import org.mule.runtime.api.component.ConfigurationProperties;
 import org.mule.test.AbstractIntegrationTestCase;
 import org.mule.test.runner.RunnerDelegateTo;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.Collection;
+import java.util.Locale;
 
 import javax.inject.Inject;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.junit.Test;
 import org.junit.runners.Parameterized;
 
+@Feature(CONFIGURATION_PROPERTIES)
+@Story(CONFIGURATION_PROPERTIES_RESOLVER_STORY)
 @RunnerDelegateTo(Parameterized.class)
 public class LocalisationPropertiesResolverExtensionTestCase extends AbstractIntegrationTestCase {
 
@@ -44,8 +53,12 @@ public class LocalisationPropertiesResolverExtensionTestCase extends AbstractInt
   }
 
   @Test
-  public void propertiesAreResolvedCorrectly() {
-    assertThat(configurationProperties.resolveStringProperty("key1").get(), is("25.837.889,45"));
+  public void propertiesAreResolvedCorrectlyAndNumberIsFormattedAccordingToLocale() {
+    NumberFormat nf = NumberFormat.getInstance(new Locale("pt", "PT"));
+    DecimalFormat formatter = (DecimalFormat) nf;
+    formatter.applyPattern("#,##0.00");
+    String expectedResult = formatter.format(Double.parseDouble("25837889.4535"));
+    assertThat(configurationProperties.resolveStringProperty("key1").get(), is(expectedResult));
   }
 }
 
