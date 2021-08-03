faf4c4aad68530ee75983a5354149362dfb038eb
Martín Buchwald <martin.buchwald@mulesoft.com>
2021-08-03 18:12:10 -0300

    MULE-19684: Default Error handler has to be considered when having an on error propagate + type = any + when expression (#1518) (#1519)

--- integration/src/test/java/org/mule/test/integration/exceptions/OnErrorWithTypeAndWhenExpressionTestCase.java
+++ integration/src/test/java/org/mule/test/integration/exceptions/OnErrorWithTypeAndWhenExpressionTestCase.java
@@ -6,14 +6,19 @@
  */
 package org.mule.test.integration.exceptions;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mule.functional.api.exception.ExpectedError.none;
 import static org.mule.test.allure.AllureConstants.ErrorHandlingFeature.ERROR_HANDLING;
 
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runners.Parameterized;
+import org.mule.functional.api.exception.ExpectedError;
+import org.mule.runtime.api.exception.DefaultMuleException;
 import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.test.AbstractIntegrationTestCase;
 import org.mule.test.runner.RunnerDelegateTo;
@@ -27,6 +32,10 @@ import java.util.Collection;
 public class OnErrorWithTypeAndWhenExpressionTestCase extends AbstractIntegrationTestCase {
 
   private String flowName;
+  private boolean expectFailure;
+
+  @Rule
+  public ExpectedError expectedException = none();
 
   @Override
   protected String getConfigFile() {
@@ -36,23 +45,28 @@ public class OnErrorWithTypeAndWhenExpressionTestCase extends AbstractIntegratio
   @Parameterized.Parameters(name = "{0}")
   public static Collection<Object[]> data() {
     return Arrays.asList(new Object[][] {
-        {"withType"},
-        {"withIncorrectType"},
-        {"withWhen"},
-        {"withIncorrectWhen"},
-        {"withWhenAndType"},
-        {"withCorrectWhenIncorrectType"},
-        {"withIncorrectWhenCorrectType"},
-        {"withIncorrectWhenIncorrectType"}
+        {"withType", false},
+        {"withIncorrectType", false},
+        {"withWhen", false},
+        {"withIncorrectWhen", false},
+        {"withWhenAndType", false},
+        {"withCorrectWhenIncorrectType", false},
+        {"withIncorrectWhenCorrectType", false},
+        {"withIncorrectWhenIncorrectType", false},
+        {"defaultWhenConditionNotMet", true},
     });
   }
 
-  public OnErrorWithTypeAndWhenExpressionTestCase(String flowName) {
+  public OnErrorWithTypeAndWhenExpressionTestCase(String flowName, boolean expectFailure) {
     this.flowName = flowName;
+    this.expectFailure = expectFailure;
   }
 
   @Test
   public void runTest() throws Exception {
+    if (expectFailure) {
+      expectedException.expectMessage(containsString("An error occurred"));
+    }
     CoreEvent event = flowRunner(flowName).run();
     assertThat(event.getMessage().getPayload().getValue(), is("Correct"));
   }
