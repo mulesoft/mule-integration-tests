02358000c55441f780416edb477ae9e6390dc2d5
rbourbonnavarro <rodrigobourbon44@gmail.com>
2021-12-27 10:34:54 -0300

    Implementing requested changes

--- integration/src/test/java/org/mule/test/config/spring/CustomObjectSerializerLazyInitTestCase.java
+++ integration/src/test/java/org/mule/test/config/spring/CustomObjectSerializerLazyInitTestCase.java
@@ -7,7 +7,9 @@
 package org.mule.test.config.spring;
 
 import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
 import io.qameta.allure.Issue;
+import io.qameta.allure.Story;
 import org.junit.Test;
 import org.mule.runtime.api.component.location.Location;
 import org.mule.runtime.api.serialization.ObjectSerializer;
@@ -18,11 +20,19 @@ import org.mule.test.AbstractIntegrationTestCase;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
 import static org.mule.runtime.api.component.location.Location.builder;
 import static org.mule.runtime.config.api.LazyComponentInitializer.LAZY_COMPONENT_INITIALIZER_SERVICE_KEY;
+import static org.mule.test.allure.AllureConstants.LazyInitializationFeature.LAZY_INITIALIZATION;
+import static org.mule.test.allure.AllureConstants.ObjectSerializer.CUSTOM_OBJECT_SERIALIZER;
 
+@Feature(LAZY_INITIALIZATION)
+@Story(CUSTOM_OBJECT_SERIALIZER)
 public class CustomObjectSerializerLazyInitTestCase extends AbstractIntegrationTestCase {
+
+  private static final String FIRST_LOCATION = "flow1";
+  private static final String SECOND_LOCATION = "flow2";
 
   @Inject
   @Named(value = LAZY_COMPONENT_INITIALIZER_SERVICE_KEY)
@@ -43,14 +53,14 @@ public class CustomObjectSerializerLazyInitTestCase extends AbstractIntegrationT
   @Description("Tests that the default object serializer gets correctly overridden whenever lazy initialization is run " +
       "more than once")
   public void lazyInitPerformedTwice() {
-    Location location = builder().globalName("flow1").build();
+    Location location = builder().globalName(FIRST_LOCATION).build();
     lazyComponentInitializer.initializeComponent(location);
-    assertTrue(muleContext.getObjectSerializer() instanceof TestSerializationProtocol);
+    assertThat(muleContext.getObjectSerializer(), instanceOf(TestSerializationProtocol.class));
 
     /* initialize another component so beans get regenerated */
-    location = builder().globalName("flow2").build();
+    location = builder().globalName(SECOND_LOCATION).build();
     lazyComponentInitializer.initializeComponent(location);
-    assertTrue(muleContext.getObjectSerializer() instanceof TestSerializationProtocol);
+    assertThat(muleContext.getObjectSerializer(), instanceOf(TestSerializationProtocol.class));
   }
 
   public static class TestSerializationProtocol implements ObjectSerializer {
