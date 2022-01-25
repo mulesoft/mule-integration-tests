1bc90280c3adf8b13330c959e5a13160db717b0e
sofiamorseletto <sofimorseletto@gmail.com>
2022-01-25 12:49:26 -0300

    MULE-20059: Migrate LoggingLibsSupportTestCase from system-tests

---
+++ logging/src/test/java/org/mule/runtime/test/integration/logging/LoggingLibsSupportTestCase.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.test.integration.logging;
+
+import static java.lang.String.format;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.mule.runtime.api.util.MuleSystemProperties.MULE_SIMPLE_LOG;
+import static org.mule.tck.probe.PollingProber.probe;
+import static org.mule.test.infrastructure.FileContainsInLine.hasLine;
+
+import org.mule.runtime.module.deployment.impl.internal.builder.ApplicationFileBuilder;
+import org.mule.runtime.module.deployment.impl.internal.builder.JarFileBuilder;
+import org.mule.tck.junit4.FlakinessDetectorTestRunner;
+import org.mule.tck.junit4.FlakyTest;
+import org.mule.tck.util.CompilerUtils;
+import org.mule.test.infrastructure.deployment.AbstractFakeMuleServerTestCase;
+import org.mule.test.runner.RunnerDelegateTo;
+
+import java.io.File;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
+public class LoggingLibsSupportTestCase extends AbstractFakeMuleServerTestCase {
+
+  @Rule
+  public UseMuleLog4jContextFactory muleLogging = new UseMuleLog4jContextFactory();
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    SLF4JBridgeHandler.removeHandlersForRootLogger();
+    SLF4JBridgeHandler.install();
+  }
+
+  @Test
+  public void eachLoggingLibraryShouldLogSuccessfully() throws Exception {
+    final ApplicationFileBuilder loggingAppFileBuilder =
+        new ApplicationFileBuilder("logging-app").definedBy("log/logging-libs/logging-libs-app.xml")
+            .dependingOn(new JarFileBuilder("loggerLibsClient",
+                                            new CompilerUtils.JarCompiler()
+                                                .compiling(new File(LoggingLibsSupportTestCase.class
+                                                    .getResource("/log/logging-libs/java/logging/LoggerLibsClient.java").toURI()))
+                                                .compile("logger-libs-client.jar")));
+
+    muleServer.start();
+    muleServer.deploy(loggingAppFileBuilder.getArtifactFile().toURI().toURL(), "logging-app");
+
+    File file = new File(muleServer.getLogsDir().toString() + "/mule-app-logging-app.log");
+
+    probe(() -> hasLine(containsString("My logger is SLF4J")).matches(file),
+          () -> format("Text '%s' not present in the logs", "My logger is SLF4J"));
+    probe(() -> hasLine(containsString("My logger is LOG4J")).matches(file),
+          () -> format("Text '%s' not present in the logs", "My logger is LOG4J"));
+    probe(() -> hasLine(containsString("My logger is JCL")).matches(file),
+          () -> format("Text '%s' not present in the logs", "My logger is JCL"));
+    probe(() -> hasLine(containsString("My logger is JUL")).matches(file),
+          () -> format("Text '%s' not present in the logs", "My logger is JUL"));
+  }
+}
