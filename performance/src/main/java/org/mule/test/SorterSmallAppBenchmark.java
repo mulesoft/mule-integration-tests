a765538eb874e82082039ab8d740c7af9a3758e7
Hyeran Park <hyeran.park@mulesoft.com>
2022-03-24 09:54:47 -0700

    Use abstract class for reusable parts

--- performance/src/main/java/org/mule/test/SorterSmallAppBenchmark.java
+++ performance/src/main/java/org/mule/test/SorterSmallAppBenchmark.java
@@ -7,188 +7,75 @@
 package org.mule.test;
 
 
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
-
 import static org.openjdk.jmh.annotations.Mode.AverageTime;
 
-import org.mule.functional.junit4.FunctionalTestCase;
-import org.mule.runtime.api.el.ExpressionLanguage;
-import org.mule.runtime.api.lifecycle.Initialisable;
-import org.mule.runtime.api.lock.LockFactory;
-import org.mule.runtime.api.store.ObjectStoreManager;
-import org.mule.runtime.config.internal.DependencyGraphLifecycleObjectSorter;
-import org.mule.runtime.config.internal.registry.SpringLifecycleObjectSorter;
-import org.mule.runtime.config.internal.registry.SpringRegistry;
-import org.mule.runtime.config.internal.resolvers.AutoDiscoveredDependencyResolver;
-import org.mule.runtime.config.internal.resolvers.ConfigurationDependencyResolver;
-import org.mule.runtime.config.internal.resolvers.DeclaredDependencyResolver;
-import org.mule.runtime.config.internal.resolvers.DependencyGraphBeanDependencyResolver;
-import org.mule.runtime.core.api.MuleContext;
-import org.mule.runtime.core.api.config.Config;
-import org.mule.runtime.core.api.config.MuleConfiguration;
-import org.mule.runtime.core.api.construct.FlowConstruct;
-import org.mule.runtime.core.api.security.SecurityManager;
-import org.mule.runtime.core.api.streaming.StreamingManager;
-import org.mule.runtime.core.api.util.queue.QueueManager;
-import org.mule.runtime.core.internal.context.MuleContextWithRegistry;
-import org.mule.runtime.core.internal.el.mvel.ExpressionLanguageExtension;
 import org.mule.runtime.core.internal.lifecycle.phases.LifecycleObjectSorter;
-import org.mule.runtime.core.internal.registry.MuleRegistry;
-import org.mule.runtime.extension.api.runtime.config.ConfigurationProvider;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import javax.inject.Inject;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 
-@Fork(1)
 @State(Scope.Benchmark)
-@OutputTimeUnit(MILLISECONDS)
-public class SorterSmallAppBenchmark extends FunctionalTestCase {
-
-  @Inject
-  private MuleContext muleContext;
-  private SpringRegistry springRegistry;
-  private ConfigurationDependencyResolver configurationDependencyResolver;
-  private DeclaredDependencyResolver declaredDependencyResolver;
-  private AutoDiscoveredDependencyResolver autoDiscoveredDependencyResolver;
-  private Map<String, Object> lookupObjects;
-  private DependencyGraphBeanDependencyResolver resolver;
-  private DependencyGraphLifecycleObjectSorter graphSorter;
-  private SpringLifecycleObjectSorter springSorter;
-  private final Class<?>[] allowedTypes = new Class<?>[] {
-      LockFactory.class,
-      ObjectStoreManager.class,
-      ExpressionLanguageExtension.class,
-      ExpressionLanguage.class,
-      QueueManager.class,
-      StreamingManager.class,
-      ConfigurationProvider.class,
-      Config.class,
-      SecurityManager.class,
-      FlowConstruct.class,
-      MuleConfiguration.class,
-      Initialisable.class
-  };
+public class SorterSmallAppBenchmark extends AbstractSorterBenchmark {
 
   @Override
   protected String getConfigFile() {
     return "benchmark/small-app.xml";
   }
 
-  @Override
-  protected boolean isStartContext() {
-    return false;
-  }
-
-  @Override
-  protected boolean doTestClassInjection() {
-    return true;
-  }
-
-  @Setup
-  public void setUp() throws Throwable {
-    setUpMuleContext();
-
-    springRegistry = getSpringRegistry(muleContext);
-
-    declaredDependencyResolver = new DeclaredDependencyResolver(springRegistry);
-    autoDiscoveredDependencyResolver = new AutoDiscoveredDependencyResolver(springRegistry);
-    configurationDependencyResolver = springRegistry.getConfigurationDependencyResolver();
-
-    resolver = new DependencyGraphBeanDependencyResolver(configurationDependencyResolver, declaredDependencyResolver,
-                                                         autoDiscoveredDependencyResolver, springRegistry);
-
-    lookupObjects = springRegistry.lookupByType(Object.class);
-
-    graphSorter = getGraphSorter();
-    addObjectsToSorter(graphSorter);
-    springSorter = getSpringSorter();
-    addObjectsToSorter(springSorter);
-  }
-
   @Benchmark
   @BenchmarkMode(AverageTime)
   public List<Object> smallAppGraphSorter() {
-    LifecycleObjectSorter graphSorter = getGraphSorter();
-    addObjectsToSorter(graphSorter);
+    // add objects
+    LifecycleObjectSorter graphSorter = getGraphSorter(resolver);
+    addObjectsToSorter(graphSorter, lookupObjects);
 
-    // sort vertices
+    // sort objects
     return graphSorter.getSortedObjects();
   }
 
   @Benchmark
   @BenchmarkMode(AverageTime)
   public void smallAppGraphSorterAddObjectsOnly() {
-    LifecycleObjectSorter graphSorter = getGraphSorter();
-    addObjectsToSorter(graphSorter);
+    // add objects
+    LifecycleObjectSorter graphSorter = getGraphSorter(resolver);
+    addObjectsToSorter(graphSorter, lookupObjects);
   }
-
 
   @Benchmark
   @BenchmarkMode(AverageTime)
   public List<Object> smallAppGraphSorterGetSortedObjectsOnly() {
-    // sort vertices
+    // sort objects
     return graphSorter.getSortedObjects();
   }
 
   @Benchmark
   @BenchmarkMode(AverageTime)
   public List<Object> smallAppSpringSorter() {
-    LifecycleObjectSorter springSorter = getSpringSorter();
-    addObjectsToSorter(springSorter);
+    // add objects
+    LifecycleObjectSorter springSorter = getSpringSorter(springRegistry);
+    addObjectsToSorter(springSorter, lookupObjects);
 
-    // sort vertices
+    // sort objects
     return springSorter.getSortedObjects();
   }
 
   @Benchmark
   @BenchmarkMode(AverageTime)
   public void smallAppSpringSorterAddObjectsOnly() {
-    LifecycleObjectSorter springSorter = getSpringSorter();
-    addObjectsToSorter(springSorter);
+    // add objects
+    LifecycleObjectSorter springSorter = getSpringSorter(springRegistry);
+    addObjectsToSorter(springSorter, lookupObjects);
   }
 
   @Benchmark
   @BenchmarkMode(AverageTime)
   public List<Object> smallAppSpringSorterGetSortedObjectsOnly() {
-    // sort vertices
+    // sort objects
     return springSorter.getSortedObjects();
-  }
-
-  private SpringRegistry getSpringRegistry(MuleContext muleContext) throws NoSuchFieldException, IllegalAccessException {
-    // Use Java Reflection API in order to get the SpringRegistry
-    MuleRegistry muleRegistry = ((MuleContextWithRegistry) muleContext).getRegistry();
-    Field registryField = muleRegistry.getClass().getDeclaredField("registry");
-    registryField.setAccessible(true);
-    return (SpringRegistry) registryField.get(muleRegistry);
-  }
-
-  private DependencyGraphLifecycleObjectSorter getGraphSorter() {
-    return new DependencyGraphLifecycleObjectSorter(resolver, allowedTypes);
-  }
-
-  private SpringLifecycleObjectSorter getSpringSorter() {
-    return new SpringLifecycleObjectSorter(allowedTypes, springRegistry);
-  }
-
-  private void addObjectsToSorter(LifecycleObjectSorter sorter) {
-    List<String> lookupObjectNames = new ArrayList<>();
-    // add objects with a dependency graph sorter
-    lookupObjects.forEach((key, value) -> {
-      sorter.addObject(key, value);
-      lookupObjectNames.add(key);
-    });
-    sorter.setLifeCycleObjectNameOrder(lookupObjectNames);
   }
 
 }
