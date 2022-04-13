1a417cc91e8f65f96f262a2e32a3ab48a2e45d87
Nicolas Santiago Di Nucci <nico.dinu@gmail.com>
2022-04-13 05:26:12 -0300

    progress

--- oauth/src/test/java/org/mule/test/module/extension/oauth/ocs/PlatformManagedOAuthConfigurationParametersTestCase.java
+++ oauth/src/test/java/org/mule/test/module/extension/oauth/ocs/PlatformManagedOAuthConfigurationParametersTestCase.java
@@ -6,8 +6,11 @@
  */
 package org.mule.test.module.extension.oauth.ocs;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mule.test.oauth.ConnectionType.DUO;
 import static org.mule.test.oauth.ConnectionType.HYPER;
 
@@ -16,7 +19,9 @@ import org.mule.test.oauth.ConnectionType;
 import org.mule.test.oauth.TestOAuthConnection;
 import org.mule.test.oauth.TestOAuthConnectionState;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
@@ -39,6 +44,16 @@ public class PlatformManagedOAuthConfigurationParametersTestCase extends Platfor
   private static final String DETAILS_PRIORITY_PARAMETER_VALUE = "55";
   private static final String OAUTH_CONNECTION_TYPE_PARAMETER_NAME = "oauthConnectionType";
   private static final String OAUTH_CONNECTION_TYPE_PARAMETER_VALUE = "HYPER";
+  private static final String FIRST_SOME_NUMBER = "1";
+  private static final String SECOND_SOME_NUMBER = "2";
+  private static final String THIRD_SOME_NUMBER = "3";
+  private static final String SOME_NUMBERS_PARAMETER_NAME = "someConnectionNumbers";
+  private static final String CONNECTION_PROPERTIES_MAP_PARAMETER_NAME = "someMapOfConnectionProperties";
+  private static final String CONNECTION_PROPERTIES_MAP_FIRST_KEY = "first";
+  private static final String CONNECTION_PROPERTIES_MAP_SECOND_KEY = "second";
+  private static final List<String> SOME_NUMBERS_PARAMETER_VALUE =
+      asList(new String[] {FIRST_SOME_NUMBER, SECOND_SOME_NUMBER, THIRD_SOME_NUMBER});
+  private static final String SOME_CONNECTION_PROPERTIES_PARAMETER_NAME = "someOauthConnectionProperties";
   private static final ConnectionType OAUTH_CONNECTION_TYPE_PARAMETER_ENUM_VALUE = HYPER;
   private static final ConnectionType CONNECTION_TYPE_FIELD_ENUM_VALUE = DUO;
   private static final ConnectionProperties CONNECTION_PROPERTIES =
@@ -48,15 +63,24 @@ public class PlatformManagedOAuthConfigurationParametersTestCase extends Platfor
   protected Map<String, Object> getDescriptorParameters() {
     Map<String, Object> descriptorParameters = super.getDescriptorParameters();
     Map<String, String> complexParameterMap = new HashMap<>();
+    List<Map<String, String>> someConnectionPropertiesValue = new ArrayList<>();
+    Map<String, Map<String, String>> someMapOfConnectionPropertiesValue = new HashMap<>();
     complexParameterMap.put(CONNECTION_DESCRIPTION_FIELD_NAME, CONNECTION_DESCRIPTION_FIELD_VALUE);
     complexParameterMap.put(CONNECTION_TYPE_FIELD_NAME, CONNECTION_TYPE_FIELD_VALUE);
-    // descriptorParameters.put(COMPLEX_PARAMETER_NAME, complexParameterMap);
-    // descriptorParameters.put(COMPLEX_PARAMETER_NAME_IN_SHOWINDSL_PARAMETER_GROUP, complexParameterMap);
-    // descriptorParameters.put(COMPLEX_PARAMETER_NAME_IN_NON_DEFAULT_PARAMETER_GROUP, complexParameterMap);
-    // descriptorParameters.put(PROFILE_LEVEL_PARAMETER_NAME, PROFILE_LEVEL_PARAMETER_VALUE);
-    // descriptorParameters.put(DETAILS_PRIORITY_PARAMETER_NAME, DETAILS_PRIORITY_PARAMETER_VALUE);
-    // descriptorParameters.put(SECURITY_LEVEL_PARAMETER_NAME, SECURITY_LEVEL_PARAMETER_VALUE);
-    // descriptorParameters.put(OAUTH_CONNECTION_TYPE_PARAMETER_NAME, OAUTH_CONNECTION_TYPE_PARAMETER_VALUE);
+    someConnectionPropertiesValue.add(complexParameterMap);
+    someConnectionPropertiesValue.add(complexParameterMap);
+    someMapOfConnectionPropertiesValue.put(CONNECTION_PROPERTIES_MAP_FIRST_KEY, complexParameterMap);
+    someMapOfConnectionPropertiesValue.put(CONNECTION_PROPERTIES_MAP_SECOND_KEY, complexParameterMap);
+    descriptorParameters.put(CONNECTION_PROPERTIES_MAP_PARAMETER_NAME, someMapOfConnectionPropertiesValue);
+    descriptorParameters.put(SOME_CONNECTION_PROPERTIES_PARAMETER_NAME, someConnectionPropertiesValue);
+    descriptorParameters.put(COMPLEX_PARAMETER_NAME, complexParameterMap);
+    descriptorParameters.put(COMPLEX_PARAMETER_NAME_IN_SHOWINDSL_PARAMETER_GROUP, complexParameterMap);
+    descriptorParameters.put(COMPLEX_PARAMETER_NAME_IN_NON_DEFAULT_PARAMETER_GROUP, complexParameterMap);
+    descriptorParameters.put(PROFILE_LEVEL_PARAMETER_NAME, PROFILE_LEVEL_PARAMETER_VALUE);
+    descriptorParameters.put(DETAILS_PRIORITY_PARAMETER_NAME, DETAILS_PRIORITY_PARAMETER_VALUE);
+    descriptorParameters.put(SECURITY_LEVEL_PARAMETER_NAME, SECURITY_LEVEL_PARAMETER_VALUE);
+    descriptorParameters.put(OAUTH_CONNECTION_TYPE_PARAMETER_NAME, OAUTH_CONNECTION_TYPE_PARAMETER_VALUE);
+    descriptorParameters.put(SOME_NUMBERS_PARAMETER_NAME, SOME_NUMBERS_PARAMETER_VALUE);
     return descriptorParameters;
   }
 
@@ -110,6 +134,36 @@ public class PlatformManagedOAuthConfigurationParametersTestCase extends Platfor
     TestOAuthConnectionState connectionState = connection.getState();
     assertThat(connectionState.getSecurityLevel(),
                equalTo(SECURITY_LEVEL_PARAMETER_INT_VALUE));
+  }
+
+  @Test
+  public void arrayOfIntegerParameter() throws Exception {
+    TestOAuthConnection connection = (TestOAuthConnection) flowRunner("getConnection").run().getMessage().getPayload().getValue();
+    TestOAuthConnectionState connectionState = connection.getState();
+    List<Integer> someConnectionNumbers = connectionState.getSomeConnectionNumbers();
+    assertThat(someConnectionNumbers, hasSize(SOME_NUMBERS_PARAMETER_VALUE.size()));
+    assertThat(someConnectionNumbers, containsInAnyOrder(Integer.valueOf(FIRST_SOME_NUMBER), Integer.valueOf(SECOND_SOME_NUMBER),
+                                                         Integer.valueOf(THIRD_SOME_NUMBER)));
+  }
+
+  @Test
+  public void arrayOfComplexTypeParameter() throws Exception {
+    TestOAuthConnection connection = (TestOAuthConnection) flowRunner("getConnection").run().getMessage().getPayload().getValue();
+    TestOAuthConnectionState connectionState = connection.getState();
+    List<ConnectionProperties> someOauthConnectionProperties = connectionState.getSomeOauthConnectionProperties();
+    assertThat(someOauthConnectionProperties, hasSize(2));
+    someOauthConnectionProperties
+        .forEach(someOauthConnectionProperty -> assertThat(someOauthConnectionProperty, equalTo(CONNECTION_PROPERTIES)));
+  }
+
+  @Test
+  public void mapOfComplexValueTypeParameter() throws Exception {
+    TestOAuthConnection connection = (TestOAuthConnection) flowRunner("getConnection").run().getMessage().getPayload().getValue();
+    TestOAuthConnectionState connectionState = connection.getState();
+    Map<String, ConnectionProperties> someOauthMapConnectionProperties = connectionState.getSomeMapOfConnectionProperties();
+    assertThat(someOauthMapConnectionProperties.entrySet(), hasSize(2));
+    assertThat(someOauthMapConnectionProperties.get(CONNECTION_PROPERTIES_MAP_FIRST_KEY), equalTo(CONNECTION_PROPERTIES));
+    assertThat(someOauthMapConnectionProperties.get(CONNECTION_PROPERTIES_MAP_SECOND_KEY), equalTo(CONNECTION_PROPERTIES));
   }
 
 }
