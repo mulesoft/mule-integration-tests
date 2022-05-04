f61f3248d54bcef79d41dea473e830077c3d1b53
Nicolas Santiago Di Nucci <nico.dinu@gmail.com>
2022-05-04 13:55:44 -0300

    remove unnecessary imports

--- oauth/src/test/java/org/mule/test/module/extension/oauth/clientcredentials/OAuthClientCredentialsInvalidConfigExtensionTestCase.java
+++ oauth/src/test/java/org/mule/test/module/extension/oauth/clientcredentials/OAuthClientCredentialsInvalidConfigExtensionTestCase.java
@@ -6,37 +6,15 @@
  */
 package org.mule.test.module.extension.oauth.clientcredentials;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
-import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import static org.junit.rules.ExpectedException.none;
-import static org.mule.extension.http.api.HttpHeaders.Names.CONTENT_TYPE;
-import static org.mule.runtime.http.api.HttpConstants.HttpStatus.OK;
-import static org.mule.runtime.oauth.api.state.ResourceOwnerOAuthContext.DEFAULT_RESOURCE_OWNER_ID;
 
-import java.util.Map;
-
-import org.hamcrest.CoreMatchers;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import org.mule.runtime.api.lifecycle.LifecycleException;
-import org.mule.runtime.api.store.ObjectStore;
-import org.mule.runtime.api.util.LazyValue;
-import org.mule.runtime.extension.api.connectivity.oauth.ClientCredentialsState;
-import org.mule.runtime.oauth.api.state.ResourceOwnerOAuthContext;
 import org.mule.test.module.extension.oauth.BaseOAuthExtensionTestCase;
-import org.mule.test.oauth.TestOAuthConnection;
-import org.mule.test.oauth.TestOAuthConnectionState;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.rules.ExpectedException;
 
 public class OAuthClientCredentialsInvalidConfigExtensionTestCase extends BaseOAuthExtensionTestCase {
