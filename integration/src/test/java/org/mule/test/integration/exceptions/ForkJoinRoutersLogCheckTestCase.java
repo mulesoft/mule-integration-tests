5038f0a28c7453d6028c8a05cb0db7ddc3f4f0ce
Hyeran Park <hyeran.park@mulesoft.com>
2022-06-03 10:54:30 -0700

    edit

--- integration/src/test/java/org/mule/test/integration/exceptions/ForkJoinRoutersLogCheckTestCase.java
+++ integration/src/test/java/org/mule/test/integration/exceptions/ForkJoinRoutersLogCheckTestCase.java
@@ -1,19 +1,23 @@
 package org.mule.test.integration.exceptions;
 
 import static org.mule.runtime.api.util.MuleSystemProperties.MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG;
-import static org.mule.tck.junit4.rule.VerboseExceptions.setVerboseExceptions;
 import static org.mule.test.allure.AllureConstants.Logging.LOGGING;
 import static org.mule.test.allure.AllureConstants.Logging.LoggingStory.ERROR_REPORTING;
 
 import static java.lang.System.setProperty;
+import static java.util.Arrays.asList;
+
+import static org.junit.runners.Parameterized.Parameters;
 
 import org.mule.runtime.api.component.AbstractComponent;
 import org.mule.runtime.api.exception.MuleException;
 import org.mule.runtime.api.i18n.I18nMessageFactory;
 import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.runtime.core.api.processor.Processor;
-import org.mule.tck.junit4.rule.VerboseExceptions;
+import org.mule.tck.junit4.rule.SystemProperty;
 import org.mule.test.AbstractIntegrationTestCase;
+
+import java.util.List;
 
 import io.qameta.allure.Feature;
 import io.qameta.allure.Issue;
@@ -21,97 +25,105 @@ import io.qameta.allure.Story;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 @Feature(LOGGING)
 @Story(ERROR_REPORTING)
 public class ForkJoinRoutersLogCheckTestCase extends AbstractIntegrationTestCase {
 
-    // Just to ensure the previous value is set after the test
-    @ClassRule
-    public static VerboseExceptions verboseExceptions = new VerboseExceptions(false);
-
     @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+    public SystemProperty legacyCompositeRoutingExceptionLog;
 
     @Override
     protected String getConfigFile() {
         return "org/mule/test/integration/exceptions/fork-join-routers-log-config.xml";
     }
 
+    @Parameters(name = "Legacy log: {0}")
+    public static List<Object[]> parameters() {
+        return asList(
+                new Object[] {true},
+                new Object[] {false});
+    }
+
+    public ForkJoinRoutersLogCheckTestCase(boolean legacyCompositeRoutingExceptionLog) {
+        this.legacyCompositeRoutingExceptionLog = new SystemProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG, Boolean.toString(legacyCompositeRoutingExceptionLog));
+    }
+
     @Test
     @Issue("W-10965130")
     public void compositeRoutingExceptionForParallelForEach() throws Exception {
-        boolean originalProperty = Boolean.parseBoolean(System.getProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG));
-        setProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG, Boolean.toString(false));
-        runSuccesses(false, "parallelForEachFlow");
-        setProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG, Boolean.toString(originalProperty));
+//        boolean originalProperty = Boolean.parseBoolean(System.getProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG));
+//        setProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG, Boolean.toString(false));
+        runSuccesses("parallelForEachFlow");
+//        setProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG, Boolean.toString(originalProperty));
     }
 
     @Test
     @Issue("W-10965130")
     public void compositeRoutingExceptionForScatterGather() throws Exception {
-        boolean originalProperty = Boolean.parseBoolean(System.getProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG));
-        setProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG, Boolean.toString(false));
-        runSuccesses(false, "scatterGatherFlow");
-        setProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG, Boolean.toString(originalProperty));
+//        boolean originalProperty = Boolean.parseBoolean(System.getProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG));
+//        setProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG, Boolean.toString(false));
+        runSuccesses("scatterGatherFlow");
+//        setProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG, Boolean.toString(originalProperty));
 
     }
 
     @Test
     @Issue("W-10965130")
     public void compositeRoutingExceptionForParallelForEachPreviousVersionLog() throws Exception {
-        boolean originalProperty = Boolean.parseBoolean(System.getProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG));
-        setProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG, Boolean.toString(true));
-        runSuccesses(false, "previousParallelForEachFlow");
-        setProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG, Boolean.toString(originalProperty));
+//        boolean originalProperty = Boolean.parseBoolean(System.getProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG));
+//        setProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG, Boolean.toString(true));
+        runSuccesses("previousParallelForEachFlow");
+//        setProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG, Boolean.toString(originalProperty));
     }
 
     @Test
     @Issue("W-10965130")
     public void compositeRoutingExceptionForScatterGatherPreviousVersionLog() throws Exception {
-        boolean originalProperty = Boolean.parseBoolean(System.getProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG));
-        setProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG, Boolean.toString(true));
-        runSuccesses(false, "previousScatterGatherFlow");
-        setProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG, Boolean.toString(originalProperty));
+//        boolean originalProperty = Boolean.parseBoolean(System.getProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG));
+//        setProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG, Boolean.toString(true));
+        runSuccesses("previousScatterGatherFlow");
+//        setProperty(MULE_PRINT_LEGACY_COMPOSITE_EXCEPTION_LOG, Boolean.toString(originalProperty));
     }
 
 
-    private void runSuccesses(boolean verboseExceptions, String flowName) throws Exception {
-        setVerboseExceptions(verboseExceptions);
+    private void runSuccesses(String flowName) throws Exception {
         flowRunner(flowName).run();
     }
 
-    public static class CustomException extends MuleException {
-
-        private static final long serialVersionUID = -5911115770998812278L;
-        private static final String MESSAGE = "Error";
-
-        public CustomException() {
-            super(I18nMessageFactory.createStaticMessage(MESSAGE));
-        }
-
-        @Override
-        public String getDetailedMessage() {
-            return MESSAGE;
-        }
-
-        @Override
-        public String getVerboseMessage() {
-            return MESSAGE;
-        }
-
-        @Override
-        public String getSummaryMessage() {
-            return MESSAGE;
-        }
-    }
-
-    public static final class ThrowNpeProcessor extends AbstractComponent implements Processor {
-
-        @Override
-        public CoreEvent process(CoreEvent event) throws MuleException {
-            throw new NullPointerException("expected");
-        }
-    }
+//    public static class CustomException extends MuleException {
+//
+//        private static final long serialVersionUID = -5911115770998812278L;
+//        private static final String MESSAGE = "Error";
+//
+//        public CustomException() {
+//            super(I18nMessageFactory.createStaticMessage(MESSAGE));
+//        }
+//
+//        @Override
+//        public String getDetailedMessage() {
+//            return MESSAGE;
+//        }
+//
+//        @Override
+//        public String getVerboseMessage() {
+//            return MESSAGE;
+//        }
+//
+//        @Override
+//        public String getSummaryMessage() {
+//            return MESSAGE;
+//        }
+//    }
+//
+//    public static final class ThrowNpeProcessor extends AbstractComponent implements Processor {
+//
+//        @Override
+//        public CoreEvent process(CoreEvent event) throws MuleException {
+//            throw new NullPointerException("expected");
+//        }
+//    }
 }
