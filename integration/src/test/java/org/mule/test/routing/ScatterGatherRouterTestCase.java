a90a0e2945d7d4ddcdfe10d2fbe96a7014ba2134
Axel Sanguinetti <asanguinetti@mulesoft.com>
2022-07-11 11:01:37 -0300

    W-11215958: Fixing typo in tests. (#1750)

--- integration/src/test/java/org/mule/test/routing/ScatterGatherRouterTestCase.java
+++ integration/src/test/java/org/mule/test/routing/ScatterGatherRouterTestCase.java
@@ -141,7 +141,7 @@ public class ScatterGatherRouterTestCase extends AbstractIntegrationTestCase {
   public void routeWithExpressionException() throws Exception {
     assertRouteException("routeWithExpressionException",
                          message -> assertThat(message, both(containsString(EXCEPTION_MESSAGE_TITLE_PREFIX)).and(
-                                                                                                                 containsString("1: org.mule.runtime.core.api.expression.ExpressionRuntimeException: \"Script 'invalidExpr ' has errors:"))),
+                                                                                                                 containsString("1: org.mule.runtime.core.api.expression.ExpressionRuntimeException: \"Script 'invalidExpr' has errors:"))),
                          ExpressionRuntimeException.class);
   }
 
