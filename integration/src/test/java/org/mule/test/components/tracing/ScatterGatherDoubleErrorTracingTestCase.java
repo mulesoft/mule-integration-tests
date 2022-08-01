7b8d187bf6079174d0b268fa29fc5824cd2d4c12
sofiamorseletto <sofimorseletto@gmail.com>
2022-08-01 11:26:40 -0300

    W-11492981: A.3.2 Implement the production of units for Scatter Gather according to the spec A.3.1.
    (cherry picked from commit ca79cc30d6ac566cb763d072f3935f958244ab7b)
    (cherry picked from commit 429e3a3b353ee39bac14c1323bd32321a6974d1d)

---
+++ integration/src/test/java/org/mule/test/components/tracing/ScatterGatherDoubleErrorTracingTestCase.java
@@ -0,0 +1,105 @@
+package org.mule.test.components.tracing;
+
+import static org.mule.test.allure.AllureConstants.Profiling.PROFILING;
+import static org.mule.test.allure.AllureConstants.Profiling.ProfilingServiceStory.DEFAULT_CORE_EVENT_TRACER;
+
+import static org.hamcrest.Matchers.either;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import org.mule.runtime.core.privileged.profiling.CapturedExportedSpan;
+import org.mule.runtime.core.privileged.profiling.ExportedSpanCapturer;
+import org.mule.runtime.core.privileged.profiling.PrivilegedProfilingService;
+import org.mule.test.AbstractIntegrationTestCase;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.Test;
+
+@Feature(PROFILING)
+@Story(DEFAULT_CORE_EVENT_TRACER)
+public class ScatterGatherDoubleErrorTracingTestCase extends AbstractIntegrationTestCase {
+
+  public static final String EXPECTED_ROUTE_SPAN_NAME = "mule:scatter-gather:route";
+  public static final String EXPECTED_SCATTER_GATHER_SPAN_NAME = "mule:scatter-gather";
+  public static final String EXPECTED_LOGGER_SPAN_NAME = "mule:logger";
+  public static final String EXPECTED_HTTP_REQUEST_SPAN_NAME = "http:request";
+  public static final String EXPECTED_SET_PAYLOAD_SPAN_NAME = "mule:set-payload";
+  public static final String SCATTER_GATHER_FLOW = "scatter-gather-flow";
+  public static final String EXPECTED_FLOW_SPAN_NAME = "mule:flow";
+  public static final String NO_PARENT_SPAN = "0000000000000000";
+
+  @Inject
+  PrivilegedProfilingService profilingService;
+
+  @Override
+  protected String getConfigFile() {
+    return "tracing/scatter-gather-double-error.xml";
+  }
+
+  @Test
+  public void testScatterGatherFlowWithError() throws Exception {
+    ExportedSpanCapturer spanCapturer = profilingService.getSpanExportManager().getExportedSpanCapturer();
+
+    try {
+      try {
+        flowRunner(SCATTER_GATHER_FLOW).withPayload(TEST_PAYLOAD).run();
+      } catch (Exception ignored) {
+      }
+      Collection<CapturedExportedSpan> exportedSpans = spanCapturer.getExportedSpans();
+
+      CapturedExportedSpan muleFlowSpan =
+          exportedSpans.stream().filter(span -> span.getName().equals(EXPECTED_FLOW_SPAN_NAME)).findFirst().orElse(null);
+
+      CapturedExportedSpan scatterGatherSpan =
+          exportedSpans.stream().filter(span -> span.getName().equals(EXPECTED_SCATTER_GATHER_SPAN_NAME)).findFirst()
+              .orElse(null);
+
+      List<CapturedExportedSpan> muleRouteSpanList =
+          exportedSpans.stream().filter(span -> span.getName().equals(EXPECTED_ROUTE_SPAN_NAME)).collect(Collectors.toList());
+
+      List<CapturedExportedSpan> httpRequestSpanList =
+          exportedSpans.stream().filter(span -> span.getName().equals(EXPECTED_HTTP_REQUEST_SPAN_NAME))
+              .collect(Collectors.toList());
+      CapturedExportedSpan httpRequestFirstCapturedRouteSpan =
+          httpRequestSpanList.stream().filter(span -> span.getParentSpanId().equals(muleRouteSpanList.get(0).getSpanId()))
+              .findFirst().orElse(null);
+      CapturedExportedSpan httpRequestSecondCapturedRouteSpan =
+          httpRequestSpanList.stream().filter(span -> span.getParentSpanId().equals(muleRouteSpanList.get(1).getSpanId()))
+              .findFirst().orElse(null);
+
+      List<CapturedExportedSpan> setPayloadSpanList =
+          exportedSpans.stream().filter(span -> span.getName().equals(EXPECTED_SET_PAYLOAD_SPAN_NAME))
+              .collect(Collectors.toList());
+
+      CapturedExportedSpan loggerSpan =
+          exportedSpans.stream().filter(span -> span.getName().equals(EXPECTED_LOGGER_SPAN_NAME)).findFirst().orElse(null);
+
+      assertThat(exportedSpans, hasSize(7));
+      assertThat(muleFlowSpan.getParentSpanId(), equalTo(NO_PARENT_SPAN));
+      assertThat(scatterGatherSpan, notNullValue());
+      assertThat(scatterGatherSpan.getParentSpanId(), equalTo(muleFlowSpan.getSpanId()));
+      assertThat(muleRouteSpanList, hasSize(2));
+      muleRouteSpanList
+          .forEach(muleRouteSpan -> assertThat(muleRouteSpan.getParentSpanId(), equalTo(scatterGatherSpan.getSpanId())));
+      assertThat(httpRequestSpanList, hasSize(2));
+      assertThat(httpRequestFirstCapturedRouteSpan, notNullValue());
+      assertThat(httpRequestSecondCapturedRouteSpan, notNullValue());
+      assertThat(setPayloadSpanList, hasSize(1));
+      assertThat(setPayloadSpanList.get(0).getParentSpanId(),
+                 either(equalTo(muleRouteSpanList.get(0).getSpanId())).or(equalTo(muleRouteSpanList.get(1).getSpanId())));
+      assertThat(loggerSpan, nullValue());
+    } finally {
+      spanCapturer.dispose();
+    }
+  }
+}
---
+++ integration/src/test/java/org/mule/test/components/tracing/ScatterGatherSuccessTracingTestCase.java
@@ -0,0 +1,94 @@
+package org.mule.test.components.tracing;
+
+import static org.mule.test.allure.AllureConstants.Profiling.PROFILING;
+import static org.mule.test.allure.AllureConstants.Profiling.ProfilingServiceStory.DEFAULT_CORE_EVENT_TRACER;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import org.mule.runtime.core.privileged.profiling.CapturedExportedSpan;
+import org.mule.runtime.core.privileged.profiling.ExportedSpanCapturer;
+import org.mule.runtime.core.privileged.profiling.PrivilegedProfilingService;
+import org.mule.test.AbstractIntegrationTestCase;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.Test;
+
+@Feature(PROFILING)
+@Story(DEFAULT_CORE_EVENT_TRACER)
+public class ScatterGatherSuccessTracingTestCase extends AbstractIntegrationTestCase {
+
+  public static final String EXPECTED_ROUTE_SPAN_NAME = "mule:scatter-gather:route";
+  public static final String EXPECTED_SCATTER_GATHER_SPAN_NAME = "mule:scatter-gather";
+  public static final String EXPECTED_LOGGER_SPAN_NAME = "mule:logger";
+  public static final String EXPECTED_SET_PAYLOAD_SPAN_NAME = "mule:set-payload";
+  public static final String SCATTER_GATHER_FLOW = "scatter-gather-flow";
+  public static final String EXPECTED_FLOW_SPAN_NAME = "mule:flow";
+  public static final String NO_PARENT_SPAN = "0000000000000000";
+
+  @Inject
+  PrivilegedProfilingService profilingService;
+
+  @Override
+  protected String getConfigFile() {
+    return "tracing/scatter-gather-success.xml";
+  }
+
+  @Test
+  public void testScatterGatherFlow() throws Exception {
+    ExportedSpanCapturer spanCapturer = profilingService.getSpanExportManager().getExportedSpanCapturer();
+
+    try {
+      flowRunner(SCATTER_GATHER_FLOW).withPayload(TEST_PAYLOAD).run().getMessage();
+      Collection<CapturedExportedSpan> exportedSpans = spanCapturer.getExportedSpans();
+
+      CapturedExportedSpan muleFlowSpan =
+          exportedSpans.stream().filter(span -> span.getName().equals(EXPECTED_FLOW_SPAN_NAME)).findFirst().orElse(null);
+
+      CapturedExportedSpan scatterGatherSpan =
+          exportedSpans.stream().filter(span -> span.getName().equals(EXPECTED_SCATTER_GATHER_SPAN_NAME)).findFirst()
+              .orElse(null);
+
+      List<CapturedExportedSpan> muleRouteSpanList =
+          exportedSpans.stream().filter(span -> span.getName().equals(EXPECTED_ROUTE_SPAN_NAME)).collect(Collectors.toList());
+
+      List<CapturedExportedSpan> setPayloadSpanList =
+          exportedSpans.stream().filter(span -> span.getName().equals(EXPECTED_SET_PAYLOAD_SPAN_NAME))
+              .collect(Collectors.toList());
+      CapturedExportedSpan setPayloadFirstCapturedRouteSpan =
+          setPayloadSpanList.stream().filter(span -> span.getParentSpanId().equals(muleRouteSpanList.get(0).getSpanId()))
+              .findFirst().orElse(null);
+      CapturedExportedSpan setPayloadSecondCapturedRouteSpan =
+          setPayloadSpanList.stream().filter(span -> span.getParentSpanId().equals(muleRouteSpanList.get(1).getSpanId()))
+              .findFirst().orElse(null);
+
+      CapturedExportedSpan loggerSpan =
+          exportedSpans.stream().filter(span -> span.getName().equals(EXPECTED_LOGGER_SPAN_NAME)).findFirst().orElse(null);
+
+      assertThat(exportedSpans, hasSize(7));
+      assertThat(muleFlowSpan.getParentSpanId(), equalTo(NO_PARENT_SPAN));
+      assertThat(scatterGatherSpan, notNullValue());
+      assertThat(scatterGatherSpan.getParentSpanId(), equalTo(muleFlowSpan.getSpanId()));
+      assertThat(muleRouteSpanList, hasSize(2));
+      muleRouteSpanList
+          .forEach(muleRouteSpan -> assertThat(muleRouteSpan.getParentSpanId(), equalTo(scatterGatherSpan.getSpanId())));
+      assertThat(setPayloadSpanList, hasSize(2));
+      assertThat(setPayloadFirstCapturedRouteSpan, notNullValue());
+      assertThat(setPayloadSecondCapturedRouteSpan, notNullValue());
+      assertThat(loggerSpan, notNullValue());
+      assertThat(loggerSpan.getParentSpanId(), equalTo(muleFlowSpan.getSpanId()));
+    } finally {
+      spanCapturer.dispose();
+    }
+  }
+}
+
