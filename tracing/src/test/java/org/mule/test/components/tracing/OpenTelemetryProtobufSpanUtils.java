5fe62de284cfd8c57363115464d70579d27c7ddf
Fabian Gonzalez <fabian.gonzalez@mulesoft.com>
2023-01-12 15:53:50 -0300

    W-12368855: Span start and span end is the same

--- tracing/src/test/java/org/mule/test/components/tracing/OpenTelemetryProtobufSpanUtils.java
+++ tracing/src/test/java/org/mule/test/components/tracing/OpenTelemetryProtobufSpanUtils.java
@@ -121,6 +121,17 @@ public class OpenTelemetryProtobufSpanUtils {
       return String.format("a span with name: [%s], ID: [%s] and parent Span ID: [%s]", getName(), getSpanId(),
                            getParentSpanId());
     }
+
+    @Override
+    public long getStartEpochSpanNanos() {
+      openTelemetryProtobufSpan.getStartTimeUnixNano();
+    }
+
+    @Override
+    public long getEndSpanEpochNanos() {
+      openTelemetryProtobufSpan.getEndTimeUnixNano();
+    }
+
   }
 
 }
