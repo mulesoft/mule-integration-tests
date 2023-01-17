49f27cc90aa63292324b4257b45e4b668cd614af
Juan Grethe <jgrethe@salesforce.com>
2023-01-17 10:34:12 -0300

    add description to test

--- extensions-xml-support/src/test/java/org/mule/test/functional/XmlSdkConfigDeploymentTestCase.java
+++ extensions-xml-support/src/test/java/org/mule/test/functional/XmlSdkConfigDeploymentTestCase.java
@@ -17,6 +17,7 @@ import org.mule.runtime.api.exception.MuleException;
 import org.mule.runtime.extension.api.runtime.config.ConfigurationInstance;
 import org.mule.tck.junit4.rule.SystemProperty;
 
+import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Issue;
 import org.junit.Rule;
@@ -45,6 +46,15 @@ public class XmlSdkConfigDeploymentTestCase extends AbstractCeXmlExtensionMuleAr
   }
 
   @Test
+  @Description("This test is testing that an app with an xml-sdk config and a lot of request configs deploys in a timely manner."
+      + "If this test timeout, it's possibly because it's getting stuck initializing the beans."
+      + "During the initialization of the spring registry, Spring tries to get the object type of the beans definition list to"
+      + "see if that factory class returns an object that could be injected in one of their fields. "
+      + "If in that factory the ObjectTypeClass field is not static, "
+      + "like in [getObjectTypeWithoutInitializingTheFields#getObjectTypeWithoutInitializingTheFields] or in"
+      + "[getObjectTypeWithoutInitializingTheFields#testGetObjectTypeReturnsSuperIfImplementsObjectTypeProvider,"
+      + "Spring will have to fully initialize the bean generating a performance issue where the"
+      + "apps probably never finish deploying")
   public void testDeployment() throws Exception {
     assertConfigPresent("theConfigurationNameFromTheAppThatWontBeMacroExpanded");
     assertConfigPresent("anotherConfigurationToShowThereIsNoClashOnMacroExpansion");
