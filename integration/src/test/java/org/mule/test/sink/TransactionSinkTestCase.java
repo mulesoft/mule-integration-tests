97a89323d706e287d6a13958a1b3a457bb698953
Hyeran Park <hyeran.park@mulesoft.com>
2023-02-04 04:15:57 +0900

    address PR comments

--- integration/src/test/java/org/mule/test/sink/TransactionSinkTestCase.java
+++ integration/src/test/java/org/mule/test/sink/TransactionSinkTestCase.java
@@ -6,14 +6,21 @@
  */
 package org.mule.test.sink;
 
+import static org.mule.test.allure.AllureConstants.TransactionFeature.TRANSACTION;
+import static org.mule.test.allure.AllureConstants.TransactionFeature.XaStory.XA_TRANSACTION;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import org.mule.test.AbstractIntegrationTestCase;
 
+import io.qameta.allure.Feature;
 import io.qameta.allure.Issue;
+import io.qameta.allure.Story;
 import org.junit.Test;
 
+@Feature(TRANSACTION)
+@Story(XA_TRANSACTION)
 @Issue("W-12128703")
 public class TransactionSinkTestCase extends AbstractIntegrationTestCase {
 
