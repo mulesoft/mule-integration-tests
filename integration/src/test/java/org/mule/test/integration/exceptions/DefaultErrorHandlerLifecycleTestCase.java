a54d81d44b6ea1f35cbc4d1a3363b933d11ffc46
Mariano Rean <mrean@mulesoft.com>
2023-02-16 11:42:20 -0300

    Revert "W-12181063: [Global Error Handling] remove the kill switch for enabling the memory fixes in 4.5 (#1912) (#1914)"
    This reverts commit 5958287173df0e32b751f2bc71d4de65d7f633ee.

--- integration/src/test/java/org/mule/test/integration/exceptions/DefaultErrorHandlerLifecycleTestCase.java
+++ integration/src/test/java/org/mule/test/integration/exceptions/DefaultErrorHandlerLifecycleTestCase.java
@@ -6,6 +6,7 @@
  */
 package org.mule.test.integration.exceptions;
 
+import static org.mule.runtime.core.privileged.exception.TemplateOnErrorHandler.reuseGlobalErrorHandler;
 import static org.mule.test.allure.AllureConstants.ErrorHandlingFeature.ERROR_HANDLING;
 import static org.mule.test.allure.AllureConstants.ErrorHandlingFeature.ErrorHandlingStory.DEFAULT_ERROR_HANDLER;
 
@@ -63,6 +64,7 @@ public class DefaultErrorHandlerLifecycleTestCase extends AbstractIntegrationTes
 
   @BeforeClass
   public static void beforeClass() {
+    reuseGlobalErrorHandler = true;
     previous = componentBuildingDefinitionRegistryFactory;
     componentBuildingDefinitionRegistryFactory = new TestComponentBuildingDefinitionRegistryFactory();
     componentBuildingDefinitionRegistryFactory.setRefreshRuntimeComponentBuildingDefinitions(true);
@@ -70,6 +72,7 @@ public class DefaultErrorHandlerLifecycleTestCase extends AbstractIntegrationTes
 
   @AfterClass
   public static void afterClass() {
+    reuseGlobalErrorHandler = null;
     componentBuildingDefinitionRegistryFactory = previous;
   }
 
