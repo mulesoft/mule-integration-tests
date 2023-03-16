bc7039d556b9ed2ae5814c8ccc87bb05d5c84fc5
Mariano Rean <mrean@mulesoft.com>
2023-03-16 14:00:42 -0300

    W-12556497: Parallel-for-each gets stuck with global error handler optimizations (#1982)
    (cherry picked from commit f0a356ab11e9199fc45b5d17f10c47dc25b11668)

---
+++ integration/src/test/java/org/mule/test/routing/ParallelForEachWithGlobalErrorHandlerTestCase.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.routing;
+
+import static org.mule.test.allure.AllureConstants.RoutersFeature.ParallelForEachStory.PARALLEL_FOR_EACH;
+import static org.mule.test.allure.AllureConstants.RoutersFeature.ROUTERS;
+
+import org.mule.test.AbstractIntegrationTestCase;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Story;
+import org.junit.Test;
+
+@Feature(ROUTERS)
+@Story(PARALLEL_FOR_EACH)
+public class ParallelForEachWithGlobalErrorHandlerTestCase extends AbstractIntegrationTestCase {
+
+  @Override
+  protected String getConfigFile() {
+    return "routers/parallel-for-each-global-error-handler.xml";
+  }
+
+  @Test(expected = Exception.class)
+  @Issue("W-12556497")
+  public void parallelForEachWithErrorHandling() throws Exception {
+    flowRunner("parallelForEachWithGlobalErrorHandling").run();
+  }
+
+  @Override
+  protected boolean isGracefulShutdown() {
+    return true;
+  }
+}
