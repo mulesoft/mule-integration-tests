e1e956b8619381c65208f9282ae3f17602c046f7
Ivan Fritzler <ifritzler@salesforce.com>
2023-05-31 02:40:35 -0300

    W-13103997

--- logging/src/test/java/org/mule/runtime/test/integration/logging/OpenTelemetryProtobufSpanUtils.java
+++ logging/src/test/java/org/mule/runtime/test/integration/logging/OpenTelemetryProtobufSpanUtils.java
@@ -143,7 +143,7 @@ public class OpenTelemetryProtobufSpanUtils {
 
     @Override
     public String getStatusAsString() {
-      return openTelemetryProtobufSpan.getStatus().getCode().toString();
+      return openTelemetryProtobufSpan.getStatus().getCode().toString().toUpperCase();
     }
 
     public String getLocation() {
