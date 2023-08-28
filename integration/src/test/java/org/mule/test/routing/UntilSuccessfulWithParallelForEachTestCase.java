12ec9de00d346ee7b656e95d8ea5a9283aef65cd
Fabian Gonzalez <fabian.gonzalez@mulesoft.com>
2023-08-28 10:43:59 -0300

    W-14011209: Race condition results in a NPE due to sharing of Map for RetryContext in UntilSuccessful (#2119)

---
+++ integration/src/test/java/org/mule/test/routing/UntilSuccessfulWithParallelForEachTestCase.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.routing;
+
+import static org.mule.test.allure.AllureConstants.RoutersFeature.ROUTERS;
+import static org.mule.test.allure.AllureConstants.RoutersFeature.UntilSuccessfulStory.UNTIL_SUCCESSFUL;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.mule.runtime.core.api.event.CoreEvent;
+import org.mule.test.AbstractIntegrationTestCase;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Story;
+import org.junit.Test;
+
+@Feature(ROUTERS)
+@Story(UNTIL_SUCCESSFUL)
+@Issue("W-14011209")
+public class UntilSuccessfulWithParallelForEachTestCase extends AbstractIntegrationTestCase {
+
+  public static final String STARTING_FLOW = "startingFlow";
+  public static final String RESPONSE_PAYLOAD = "OK";
+
+  @Override
+  protected String getConfigFile() {
+    return "until-successful-with-parallel-foreach.xml";
+  }
+
+  @Test
+  public void testConcurrentUntilSuccessfulAfterAnUntilSuccessful() throws Exception {
+    CoreEvent muleEvent = flowRunner(STARTING_FLOW).run();
+    assertThat(muleEvent.getMessage().getPayload().getValue(), is(RESPONSE_PAYLOAD));
+  }
+}
