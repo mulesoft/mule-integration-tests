3b0f857ef72cf81ba28b0ea13dfdfe865f0ec39e
Mariano Gonzalez <mariano.gonzalez@mulesoft.com>
2023-08-29 05:15:12 -0300

    W-14029065: Failure to refresh OAuth token leaves Source in inconsitent state (#2125)

--- oauth/src/test/java/org/mule/test/module/extension/oauth/authcode/OAuthSourceRefreshExtensionTestCase.java
+++ oauth/src/test/java/org/mule/test/module/extension/oauth/authcode/OAuthSourceRefreshExtensionTestCase.java
@@ -3,14 +3,21 @@
  */
 package org.mule.test.module.extension.oauth.authcode;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static org.mule.runtime.http.api.HttpConstants.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.mule.tck.probe.PollingProber.check;
 
-import org.junit.Before;
-import org.junit.Test;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 
 import org.mule.runtime.api.exception.MuleException;
+import org.mule.runtime.api.notification.ExceptionNotificationListener;
+import org.mule.runtime.api.notification.NotificationListenerRegistry;
+import org.mule.runtime.api.util.concurrent.Latch;
 import org.mule.runtime.core.api.construct.Flow;
 import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.runtime.core.api.processor.Processor;
@@ -19,7 +26,12 @@ import org.mule.test.module.extension.oauth.BaseOAuthExtensionTestCase;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import com.github.tomakehurst.wiremock.client.WireMock;
+import io.qameta.allure.Issue;
+import org.junit.Before;
+import org.junit.Test;
 
 public class OAuthSourceRefreshExtensionTestCase extends BaseOAuthExtensionTestCase {
 
@@ -27,7 +39,11 @@ public class OAuthSourceRefreshExtensionTestCase extends BaseOAuthExtensionTestC
   private static final int PROBE_TIMEOUT = 5000;
   private static final int PROBE_FREQUENCY = 500;
 
+  @Inject
+  protected NotificationListenerRegistry notificationListenerRegistry;
+
   private Flow flow;
+
 
   public static class Collector implements Processor {
 
@@ -86,6 +102,45 @@ public class OAuthSourceRefreshExtensionTestCase extends BaseOAuthExtensionTestC
     stubTokenUrl(accessTokenContent(ACCESS_TOKEN + "-refreshed"));
 
     startFlow("source");
+
+    check(PROBE_TIMEOUT, PROBE_FREQUENCY, () -> EVENTS.size() > 0);
+    wireMock.verify(postRequestedFor(urlPathEqualTo("/" + TOKEN_PATH)));
+  }
+
+  @Test
+  @Issue("W-13628406")
+  public void refreshTokenFailsOnSource() throws Exception {
+    assertRefreshTokenOnSource("source");
+  }
+
+  @Test
+  @Issue("W-13628406")
+  public void refreshTokenFailsOnPollingSource() throws Exception {
+    assertRefreshTokenOnSource("pollingSource");
+  }
+
+  private void assertRefreshTokenOnSource(String flowName) throws Exception {
+    simulateCallback();
+
+    WireMock.reset();
+    wireMock.stubFor(post(urlMatching("/" + TOKEN_PATH)).willReturn(aResponse()
+        .withStatus(INTERNAL_SERVER_ERROR.getStatusCode())
+        .withBody("no token for you!")));
+
+    Latch latch = new Latch();
+    final ExceptionNotificationListener listener = notification -> {
+      if (notification.getException().getMessage().contains("Refresh token workflow was attempted but failed")) {
+        latch.release();
+      }
+    };
+
+    notificationListenerRegistry.registerListener(listener);
+    startFlow(flowName);
+    latch.await(PROBE_TIMEOUT, MILLISECONDS);
+
+    WireMock.reset();
+    simulateCallback();
+    stubTokenUrl(accessTokenContent(ACCESS_TOKEN + "-refreshed"));
 
     check(PROBE_TIMEOUT, PROBE_FREQUENCY, () -> EVENTS.size() > 0);
     wireMock.verify(postRequestedFor(urlPathEqualTo("/" + TOKEN_PATH)));
