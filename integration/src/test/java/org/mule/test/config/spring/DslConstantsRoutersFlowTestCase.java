a63c68b993c1d93ec4b59c47fbd494122a460dd4
Martín Buchwald <martin.buchwald@mulesoft.com>
2023-10-05 01:02:03 -0300

    W-14145356: Core-components modularization: ignore integration tests for version >= 11 that use reflection until W-14226830 (#2155)

--- integration/src/test/java/org/mule/test/config/spring/DslConstantsRoutersFlowTestCase.java
+++ integration/src/test/java/org/mule/test/config/spring/DslConstantsRoutersFlowTestCase.java
@@ -6,6 +6,8 @@
  */
 package org.mule.test.config.spring;
 
+import static org.apache.commons.lang3.JavaVersion.JAVA_11;
+import static org.apache.commons.lang3.SystemUtils.isJavaVersionAtMost;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isA;
@@ -13,7 +15,9 @@ import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeThat;
 import static org.junit.rules.ExpectedException.none;
+
 import org.mule.runtime.core.api.construct.Flow;
 import org.mule.runtime.core.api.expression.ExpressionRuntimeException;
 import org.mule.runtime.core.api.processor.Processor;
@@ -39,6 +43,8 @@ public class DslConstantsRoutersFlowTestCase extends AbstractIntegrationTestCase
 
   @Test
   public void testIdempotentSecureHashReceiverRouter() throws Exception {
+    // TODO (W-14226830): remove assumeThat
+    assumeThat(isJavaVersionAtMost(JAVA_11), is(true));
     Processor router = lookupMessageProcessorFromFlow("IdempotentSecureHashReceiverRouter");
     assertThat(router.getClass().getName(),
                equalTo("org.mule.runtime.core.internal.routing.IdempotentMessageValidator"));
@@ -47,6 +53,8 @@ public class DslConstantsRoutersFlowTestCase extends AbstractIntegrationTestCase
 
   @Test
   public void testIdempotentReceiverRouter() throws Exception {
+    // TODO (W-14226830): remove assumeThat
+    assumeThat(isJavaVersionAtMost(JAVA_11), is(true));
     Processor router = lookupMessageProcessorFromFlow("IdempotentReceiverRouter");
     assertThat(router.getClass().getName(),
                equalTo("org.mule.runtime.core.internal.routing.IdempotentMessageValidator"));
