bbdd67f8e25b5fde6884c1e4b88acc49ab38622a
fsgonz <fabian.gonzalez@salesforce.com>
2023-11-28 11:20:32 -0300

    W-14547895: HeadSampling in MuleRuntime

---
+++ tracing/src/test/java/org/mule/test/components/tracing/OpenTelemetryTraceIdRatioDefaultRatioSamplerTestCase.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.components.tracing;
+
+import static org.mule.test.allure.AllureConstants.Profiling.PROFILING;
+import static org.mule.test.allure.AllureConstants.Profiling.ProfilingServiceStory.OPEN_TELEMETRY_EXPORTER;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+
+@Feature(PROFILING)
+@Story(OPEN_TELEMETRY_EXPORTER)
+public class OpenTelemetryTraceIdRatioDefaultRatioSamplerTestCase extends OpenTelemetrySamplerTestCase {
+
+  @Override
+  String getSamplerName() {
+    return "traceidratio";
+  }
+
+  @Override
+  String getSamplerArg() {
+    return null;
+  }
+}
