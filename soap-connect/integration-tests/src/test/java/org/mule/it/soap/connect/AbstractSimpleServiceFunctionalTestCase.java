f4d5f0077eb776256bcbb008ceb2441506424138
Axel Sanguinetti <asanguinetti@mulesoft.com>
2024-04-09 00:22:41 -0300

    W-13735397: Flaky SOAP IT tests (#2323)
    (cherry picked from commit 1e072532b3b7957fc808507e8b1b53cc6c6d83ca)
    
    Co-authored-by: elrodro83 <rmerino@salesforce.com>

--- soap-connect/integration-tests/src/test/java/org/mule/it/soap/connect/AbstractSimpleServiceFunctionalTestCase.java
+++ soap-connect/integration-tests/src/test/java/org/mule/it/soap/connect/AbstractSimpleServiceFunctionalTestCase.java
@@ -12,21 +12,25 @@ import org.mule.tck.junit4.rule.ExternalProcess;
 import org.mule.tck.junit4.rule.SystemProperty;
 
 import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.ClassRule;
 import org.junit.Rule;
 
 public abstract class AbstractSimpleServiceFunctionalTestCase extends MuleArtifactFunctionalTestCase {
 
-  @Rule
-  public DynamicPort port = new DynamicPort("testPort");
+  @ClassRule
+  public static DynamicPort port = new DynamicPort("testPort");
 
   // TODO MULE-16661 Use a Docker image instead of this
   // This needs to run with a plain classloader to avoid conflicting libs in different artifacts in the Mule classloader
   // hierarchy, in order to avoid a LinkageError when running in JDK 11 or higher.
-  @Rule
-  public ExternalProcess server =
-      new ExternalProcess(line -> line.contains("org.eclipse.jetty.server.ServerConnector: Started ServerConnector"),
-                          "java", "-cp", System.getProperty("soapHttpServerClasspath"), "org.mule.service.soap.server.HttpServer",
-                          "" + port.getNumber(), "org.mule.service.soap.service.Soap11Service");
+  @ClassRule
+  public static ExternalProcess server =
+      new ExternalProcess(line -> line.contains("Setting the server's publish address to be /server"),
+                          "java", "-cp",
+                          System.getProperty("soapHttpServerClasspath"),
+                          "org.mule.service.soap.server.HttpServer",
+                          "" + port.getNumber(),
+                          "org.mule.service.soap.service.Soap11Service");
 
   @Rule
   public SystemProperty address = new SystemProperty("address", "http://localhost:" + port.getNumber() + "/server");
