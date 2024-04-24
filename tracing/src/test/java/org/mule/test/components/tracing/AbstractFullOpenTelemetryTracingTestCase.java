e57420801a93e049e0f2ead806bdc2dbe45cc753
fsgonz <fabian.gonzalez@salesforce.com>
2024-04-24 10:21:59 -0300

    W-15586397: set a version to the otel image to avoid changes

--- tracing/src/test/java/org/mule/test/components/tracing/AbstractFullOpenTelemetryTracingTestCase.java
+++ tracing/src/test/java/org/mule/test/components/tracing/AbstractFullOpenTelemetryTracingTestCase.java
@@ -58,7 +58,7 @@ public abstract class AbstractFullOpenTelemetryTracingTestCase extends
     OpenTelemetryTracingTestCase implements OpenTelemetryTracingTestRunnerConfigAnnotation {
 
   private static final DockerImageName COLLECTOR_IMAGE =
-      DockerImageName.parse("ghcr.io/open-telemetry/opentelemetry-java/otel-collector");
+      DockerImageName.parse("otel/opentelemetry-collector:0.99.0");
 
   private static final Integer COLLECTOR_OTLP_GRPC_PORT = 4317;
   private static final Integer COLLECTOR_OTLP_HTTP_PORT = 4318;
