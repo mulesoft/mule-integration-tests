cd602d0f210e77916b093e9d029d1bf6e74b11d9
elrodro83 <rmerino@salesforce.com>
2024-07-02 13:38:20 -0300

    W-15509819: Validate `import` target has the same top level element as the importing file

--- integration-config-failures/src/test/java/org/mule/test/config/spring/parsers/ImportValidationTestCase.java
+++ integration-config-failures/src/test/java/org/mule/test/config/spring/parsers/ImportValidationTestCase.java
@@ -6,15 +6,19 @@
  */
 package org.mule.test.config.spring.parsers;
 
-import static org.junit.rules.ExpectedException.none;
 import static org.mule.test.allure.AllureConstants.MuleDsl.DslValidationStory.DSL_VALIDATION_STORY;
 
+import static org.junit.rules.ExpectedException.none;
+
 import org.mule.functional.junit4.AbstractConfigurationFailuresTestCase;
+import org.mule.runtime.api.meta.MuleVersion;
+import org.mule.runtime.core.api.config.DefaultMuleConfiguration;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import io.qameta.allure.Issue;
 import io.qameta.allure.Story;
 
 @Story(DSL_VALIDATION_STORY)
@@ -29,6 +33,22 @@ public class ImportValidationTestCase extends AbstractConfigurationFailuresTestC
     expectedException.expectMessage("[" + configFile + ":7]: "
         + "Could not find imported resource 'invalid_location.xml'");
     loadConfiguration(configFile);
+  }
+
+  @Test
+  @Issue("W-15509819")
+  public void failureOnImportWithDifferentArtifactType() throws Exception {
+    String configFile = "org/mule/config/spring/parsers/dsl-validation-different-artifact-type-import-config.xml";
+    expectedException.expectMessage("[" + configFile + ":7]: "
+        + "Imported resource 'org/mule/config/spring/parsers/dsl-validation-different-artifact-type-imported-config.xml' declares a 'DOMAIN', not a 'APPLICATION'");
+    loadConfiguration(configFile);
+  }
+
+  @Override
+  protected void applyConfiguration(DefaultMuleConfiguration muleConfiguration) {
+    super.applyConfiguration(muleConfiguration);
+
+    muleConfiguration.setMinMuleVersion(new MuleVersion("4.8.0"));
   }
 
 }
