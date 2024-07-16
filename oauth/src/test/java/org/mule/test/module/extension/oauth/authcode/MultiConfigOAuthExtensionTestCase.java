bd776bcb5e295c9d5c212805cac3411dde46d1e0
hyerans <hyeran.park@mulesoft.com>
2024-07-16 06:42:15 -0700

    edit test

--- oauth/src/test/java/org/mule/test/module/extension/oauth/authcode/MultiConfigOAuthExtensionTestCase.java
+++ oauth/src/test/java/org/mule/test/module/extension/oauth/authcode/MultiConfigOAuthExtensionTestCase.java
@@ -9,13 +9,15 @@ package org.mule.test.module.extension.oauth.authcode;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mule.runtime.api.store.ObjectStoreManager.BASE_PERSISTENT_OBJECT_STORE_KEY;
-import org.mule.runtime.api.store.ObjectStore;
+
+import org.mule.oauth.client.api.state.ResourceOwnerOAuthContext;
 import org.mule.tck.junit4.rule.DynamicPort;
 import org.mule.test.module.extension.oauth.BaseOAuthExtensionTestCase;
 import org.mule.test.oauth.TestOAuthConnection;
 import org.mule.test.oauth.TestOAuthConnectionState;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -52,7 +54,7 @@ public class MultiConfigOAuthExtensionTestCase extends BaseOAuthExtensionTestCas
         .run().getMessage().getPayload().getValue()).getState();
 
     assertConnectionState(connection);
-    assertOAuthStateStored(BASE_PERSISTENT_OBJECT_STORE_KEY, ownerId);
+    validateObjectStoreEntries(1);
 
     getObjectStore(BASE_PERSISTENT_OBJECT_STORE_KEY).clear();
     simulateCallback(otherCallbackPort.getNumber());
@@ -66,30 +68,32 @@ public class MultiConfigOAuthExtensionTestCase extends BaseOAuthExtensionTestCas
 
   @Test
   public void unauthorize() throws Exception {
-    startDance(callbackPort.getNumber());
-    getConnection(callbackPort.getNumber(), "getConnection");
-
-    flowRunner("unauthorize").run();
-    ObjectStore objectStore = getObjectStore(BASE_PERSISTENT_OBJECT_STORE_KEY);
-    assertThat(objectStore.retrieveAll().size(), is(1));
-
-    startDance(otherCallbackPort.getNumber());
-    getConnection(otherCallbackPort.getNumber(), "getOtherConnection");
-
-    flowRunner("unauthorizeOther").run();
-    assertThat(objectStore.retrieveAll().size(), is(0));
+    executeFlow(callbackPort.getNumber(), "getConnection", "unauthorize", 1);
+    executeFlow(otherCallbackPort.getNumber(), "getOtherConnection", "unauthorizeOther", 2);
   }
 
-  private void getConnection(int port, String flowName) throws Exception {
-    simulateCallback(port);
+  private void executeFlow(int portNumber, String getConnectionFlow, String unauthorizeFlow, int expectedEntriesSize)
+      throws Exception {
+    startDance(portNumber);
+    simulateCallback(portNumber);
 
-    TestOAuthConnectionState connection = ((TestOAuthConnection) flowRunner(flowName)
+    TestOAuthConnectionState connectionState = ((TestOAuthConnection) flowRunner(getConnectionFlow)
         .run().getMessage().getPayload().getValue()).getState();
 
-    assertConnectionState(connection);
-    assertOAuthStateStored(BASE_PERSISTENT_OBJECT_STORE_KEY, ownerId);
+    assertConnectionState(connectionState);
+
+    validateObjectStoreEntries(expectedEntriesSize);
+
+    flowRunner(unauthorizeFlow).run();
+    validateObjectStoreEntries(expectedEntriesSize);
   }
 
+  private void validateObjectStoreEntries(int expectedEntriesSize) throws Exception {
+    Map<String, ResourceOwnerOAuthContext> entries = getObjectStore(BASE_PERSISTENT_OBJECT_STORE_KEY).retrieveAll();
+    assertThat(entries.size(), is(expectedEntriesSize));
+    ResourceOwnerOAuthContext context = (ResourceOwnerOAuthContext) entries.values().toArray()[0];
+    assertThat(context.getResourceOwnerId(), is(ownerId));
+  }
 
   private void startDance(int port) throws IOException {
     simulateDanceStart(port);
