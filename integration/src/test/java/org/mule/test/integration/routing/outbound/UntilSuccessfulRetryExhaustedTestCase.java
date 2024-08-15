11117b4e64d201076df8d0ae757b9bbcb018d8f1
Ivan Fritzler <58004191+IvanAndresFritzler@users.noreply.github.com>
2024-08-15 10:31:33 -0300

    W-15643200: W-15643200: "output json --- error" fails with NullPointerException after "MULE:RETRIES_EXHAUSTED" has occurred. (#2433) (#2444)
    (cherry picked from commit 2da9005712468de09f66c95b28995a26252c6316)

--- integration/src/test/java/org/mule/test/integration/routing/outbound/UntilSuccessfulRetryExhaustedTestCase.java
+++ integration/src/test/java/org/mule/test/integration/routing/outbound/UntilSuccessfulRetryExhaustedTestCase.java
@@ -7,12 +7,13 @@
 package org.mule.test.integration.routing.outbound;
 
 import static org.mule.runtime.api.util.MuleSystemProperties.SUPPRESS_ERRORS_PROPERTY;
-import static org.mule.test.allure.AllureConstants.RoutersFeature.ROUTERS;
-import static org.mule.test.allure.AllureConstants.RoutersFeature.UntilSuccessfulStory.UNTIL_SUCCESSFUL;
+import static org.mule.test.allure.AllureConstants.ScopeFeature.SCOPE;
+import static org.mule.test.allure.AllureConstants.ScopeFeature.UntilSuccessfulStory.UNTIL_SUCCESSFUL;
 
 import static java.lang.Boolean.parseBoolean;
 import static java.util.Arrays.asList;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.lang.String.format;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.instanceOf;
@@ -20,13 +21,14 @@ import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.fail;
 
 import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.api.i18n.I18nMessageFactory;
 import org.mule.runtime.api.message.Error;
 import org.mule.runtime.api.notification.ExceptionNotificationListener;
+import org.mule.runtime.api.serialization.ObjectSerializer;
 import org.mule.runtime.api.util.concurrent.Latch;
 import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.runtime.core.api.retry.policy.RetryPolicyExhaustedException;
@@ -37,6 +39,7 @@ import org.mule.tck.junit4.rule.SystemProperty;
 import org.mule.test.AbstractIntegrationTestCase;
 import org.mule.test.runner.RunnerDelegateTo;
 
+import java.io.InputStream;
 import java.util.Collection;
 
 import javax.inject.Inject;
@@ -48,7 +51,7 @@ import org.junit.runners.Parameterized;
 import org.junit.Rule;
 import org.junit.Test;
 
-@Feature(ROUTERS)
+@Feature(SCOPE)
 @Story(UNTIL_SUCCESSFUL)
 @RunnerDelegateTo(Parameterized.class)
 public class UntilSuccessfulRetryExhaustedTestCase extends AbstractIntegrationTestCase {
@@ -58,6 +61,11 @@ public class UntilSuccessfulRetryExhaustedTestCase extends AbstractIntegrationTe
 
   @Inject
   private TestQueueManager queueManager;
+
+  @Inject
+  private ObjectSerializer defaultSerializer;
+
+  private final boolean isSuppressErrors;
 
   @Override
   protected String getConfigFile() {
@@ -71,6 +79,7 @@ public class UntilSuccessfulRetryExhaustedTestCase extends AbstractIntegrationTe
 
   public UntilSuccessfulRetryExhaustedTestCase(String suppressErrors) {
     this.suppressErrors = new SystemProperty(SUPPRESS_ERRORS_PROPERTY, suppressErrors);
+    this.isSuppressErrors = parseBoolean(suppressErrors);
   }
 
   @Test
@@ -97,7 +106,7 @@ public class UntilSuccessfulRetryExhaustedTestCase extends AbstractIntegrationTe
 
   @Test
   public void onRetryExhaustedCausedByConnectionExceptionErrorTypeMustBeRetryExhausted() throws Exception {
-    if (isSupressErrors()) {
+    if (isSuppressErrors()) {
       flowRunner("retryExhaustedCausedByConnectivityError").withPayload("message")
           .runExpectingException(ErrorTypeMatcher.errorType(MuleErrors.RETRY_EXHAUSTED));
     } else {
@@ -106,14 +115,14 @@ public class UntilSuccessfulRetryExhaustedTestCase extends AbstractIntegrationTe
     }
   }
 
-  private boolean isSupressErrors() {
+  private boolean isSuppressErrors() {
     return parseBoolean(suppressErrors.getValue());
   }
 
   @Test
   @Issue("MULE-18041")
   public void retryExhaustedCausedByConnectionExceptionLogCheck() throws Exception {
-    if (isSupressErrors()) {
+    if (isSuppressErrors()) {
       flowRunner("retryExhaustedCausedByConnectivityErrorWithSuppressionLogCheck").withPayload("message")
           .run();
     } else {
@@ -125,7 +134,7 @@ public class UntilSuccessfulRetryExhaustedTestCase extends AbstractIntegrationTe
   @Test
   @Issue("MULE-18041")
   public void retryExhaustedCausedByNonConnectionExceptionLogCheck() throws Exception {
-    if (isSupressErrors()) {
+    if (isSuppressErrors()) {
       flowRunner("retryExhaustedCausedByNonConnectivityErrorWithSuppressionLogCheck").withPayload("message")
           .run();
     } else {
@@ -137,7 +146,7 @@ public class UntilSuccessfulRetryExhaustedTestCase extends AbstractIntegrationTe
   @Test
   @Issue("MULE-18041")
   public void retryExhaustedCausedByMuleRuntimeExceptionLogCheck() throws Exception {
-    if (isSupressErrors()) {
+    if (isSuppressErrors()) {
       flowRunner("retryExhaustedCausedByMuleRuntimeErrorWithSuppressionLogCheck").withPayload("message")
           .run();
     } else {
@@ -150,7 +159,7 @@ public class UntilSuccessfulRetryExhaustedTestCase extends AbstractIntegrationTe
   @Issue("MULE-18562")
   public void retryExhaustedUnsuppressedErrorTypeHandling() throws Exception {
     CoreEvent event = flowRunner("retryExhaustedUnsuppressedErrorTypeHandling").withPayload("message").run();
-    if (isSupressErrors()) {
+    if (isSuppressErrors()) {
       assertThat(event.getMessage().getPayload().getValue(), is("retry-exhausted-handled"));
     } else {
       assertThat(event.getMessage().getPayload().getValue(), is("security-handled"));
@@ -176,10 +185,37 @@ public class UntilSuccessfulRetryExhaustedTestCase extends AbstractIntegrationTe
     assertThat(error.getFailingComponent(), containsString("retryExhaustedErrorWithSuppressionsCheck/processors/0"));
     assertThat(error.getErrorMessage(), nullValue());
     assertThat(error.getChildErrors(), empty());
-    if (isSupressErrors()) {
+    if (isSuppressErrors()) {
       assertThat(error.getErrorType().toString(), equalTo("MULE:RETRY_EXHAUSTED"));
     } else {
       assertThat(error.getErrorType().toString(), equalTo("MULE:SECURITY"));
+    }
+  }
+
+  @Test
+  @Issue("W-15643200")
+  public void retryExhaustedErrorSerializationCheck() throws Exception {
+    flowRunner("retryExhaustedErrorSerializationCheck").withPayload("message").run();
+    Error error = (Error) queueManager.read("dlq", RECEIVE_TIMEOUT, MILLISECONDS).getMessage().getPayload().getValue();
+    // Serialize the error
+    byte[] errorSerialization = defaultSerializer.getInternalProtocol().serialize(error);
+    // Deserialize the error
+    Error errorDeserialization = defaultSerializer.getInternalProtocol().deserialize(errorSerialization);
+    assertThat(errorDeserialization.getCause(), instanceOf(RetryPolicyExhaustedException.class));
+    assertThat(errorDeserialization.getDescription(), equalTo("Mule runtime error"));
+  }
+
+  @Test
+  @Issue("W-15643200")
+  public void retryExhaustedLegacyErrorSerializationCheck() throws Exception {
+    // Deserialize the legacy error.
+    try (InputStream errorSerializationInputStream =
+        getClass()
+            .getResourceAsStream(format("UntilSuccessfulRetryExhaustedTestCase_retryExhaustedErrorSerializationCheck%s.bin",
+                                        isSuppressErrors ? "_suppressedTrue" : "_suppressedFalse"))) {
+      Error deserializedError = defaultSerializer.getInternalProtocol().deserialize(errorSerializationInputStream);
+      assertThat(deserializedError.getCause(), instanceOf(RetryPolicyExhaustedException.class));
+      assertThat(deserializedError.getDescription(), equalTo("Mule runtime error"));
     }
   }
 
